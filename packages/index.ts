38b7f29e7a962b5fb65a7e8f30ac12fd2475a16c
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2022-08-16 22:17:03 +0900

    Fix: literal guarded

--- packages/index.ts
+++ packages/index.ts
@@ -1,9 +1,14 @@
-import * as components  from "./components";
+import * as components from "./components";
 import _Vue from "vue";
+
+type ComponentKey = keyof typeof components;
+const isLiteral = (component: string): component is ComponentKey =>
+  Object.keys(components).includes(component);
 
 const install = (Vue: typeof _Vue) => {
   for (const component in components) {
-    // @ts-ignore
+    if (!isLiteral(component)) return;
+
     Vue.component(component, components[component]);
   }
 };
