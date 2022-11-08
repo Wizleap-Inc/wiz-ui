23f280799c053b7f3fe608aae3e3db61da19bf84
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2022-11-08 18:04:06 +0900

    feat: wizleap iconをcustom themeとして登録

---
+++ .storybook/wizTheme.cjs
@@ -0,0 +1,11 @@
+import { create } from "@storybook/theming";
+
+export default create({
+  base: "light",
+
+  brandTitle: "Wizleap storybook",
+  brandUrl: "https://wizleap.co.jp",
+  brandImage:
+    "https://wizleap.co.jp/wp-content/themes/wizleap/assets/images/common/img-logo.png",
+  brandTarget: "_self",
+});
