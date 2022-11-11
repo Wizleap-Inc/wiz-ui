b06ac8c0fd62d7e87746ce1e16afd4193e9d5c68
ichi-h <85932406+ichi-h@users.noreply.github.com>
2022-11-11 22:15:53 +0900

    Fix: arrow-rightに命名変更

--- packages/wiz-ui/src/components/atoms/anchor/anchor.stories.ts
+++ packages/wiz-ui/src/components/atoms/anchor/anchor.stories.ts
@@ -2,7 +2,7 @@ import { StoryFn } from "@storybook/vue";
 
 import { COLOR_MAP_ACCESSORS } from "../../../constants/styles/color";
 import { FONT_SIZE_ACCESSORS } from "../../../constants/styles/fontSize";
-import WizIArrowRight from "../../icons/arrowRight.vue";
+import WizIArrowRight from "../../icons/arrow-right.vue";
 
 import WizAnchor from "./anchor.vue";
 
