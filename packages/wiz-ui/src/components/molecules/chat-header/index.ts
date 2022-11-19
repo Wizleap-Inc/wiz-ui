8dae4cb08ff0c23429da2f65678912e0674ce010
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2022-11-20 01:33:07 +0900

    refactor: 使わなかったchat-headerを削除

--- packages/wiz-ui/src/components/molecules/chat-header/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as WizChatHeader } from "./chat-header.vue";
--- packages/wiz-ui/src/components/molecules/index.ts
+++ packages/wiz-ui/src/components/molecules/index.ts
@@ -2,5 +2,4 @@ export * from "./message-box";
 export * from "./tables";
 export * from "./chat-item";
 export * from "./chat-form";
-export * from "./chat-header";
 export * from "./card";
