889d6f726b72a16329430243cb4edd08214d8790
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2022-11-30 17:17:41 +0900

    feat: WizHeaderを準備

---
+++ packages/wiz-ui/src/components/custom/header/index.ts
@@ -0,0 +1,1 @@
+export { default as WizHeader } from "./header.vue";
--- packages/wiz-ui/src/constants/component/name.ts
+++ packages/wiz-ui/src/constants/component/name.ts
@@ -2,4 +2,5 @@ export const ComponentName = {
   Text: "WizText",
   Tab: "WizTab",
   TabPane: "WizTabPane",
+  Header: "WizHeader",
 } as const;
