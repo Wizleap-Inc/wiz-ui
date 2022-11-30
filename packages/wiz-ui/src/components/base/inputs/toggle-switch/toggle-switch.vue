78cc0da726f375733308f8df11bb767d13117f1f
Tanizawa Kenta <kt10025102@gmail.com>
2022-12-01 01:03:26 +0900

    feat:wiz-toggle-switchの作成

---
+++ packages/wiz-ui/src/components/base/inputs/toggle-switch/toggle-switch.vue
@@ -0,0 +1,85 @@
+<template>
+  <label class="wiz-toggle-switch">
+    <input
+      class="wiz-toggle-switch__input"
+      type="checkbox"
+      v-model="switchValue"
+    />
+    <span class="wiz-toggle-switch__slider"></span>
+  </label>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+import { THEME } from "@/constants/styles";
+
+interface Props {
+  value: boolean;
+}
+
+const props = defineProps<Props>();
+
+interface Emit {
+  (e: "input", value: boolean): void;
+}
+
+const emit = defineEmits<Emit>();
+
+const switchValue = computed({
+  get: () => props.value,
+  set: (value) => emit("input", value),
+});
+
+const colorGreen800 = THEME.color.green["800"];
+const colorWhite800 = THEME.color.white["800"];
+const spacingXl = THEME.spacing.xl;
+const spacingMax = THEME.spacing.max;
+</script>
+
+<style lang="scss" scoped>
+.wiz-toggle-switch {
+  position: relative;
+  display: inline-block;
+  width: 42px;
+  height: 20px;
+
+  &__input {
+    opacity: 0;
+    width: 0;
+    height: 0;
+
+    &:checked + .wiz-toggle-switch__slider:before {
+      -webkit-transform: translateX(20px);
+      -ms-transform: translateX(20px);
+      transform: translateX(20px);
+    }
+  }
+
+  &__slider {
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: v-bind(colorWhite800);
+    border: 1px solid v-bind(colorGreen800);
+    border-radius: v-bind(spacingXl);
+    box-sizing: border-box;
+
+    &:before {
+      position: absolute;
+      content: "";
+      height: 13.33px;
+      width: 13.33px;
+      top: 2.33px;
+      left: 3px;
+      background-color: v-bind(colorGreen800);
+      border-radius: v-bind(spacingMax);
+      -webkit-transition: 0.4s;
+      transition: 0.4s;
+    }
+  }
+}
+</style>
