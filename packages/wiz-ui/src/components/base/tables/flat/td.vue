ce6d00d1afffa155aebee5d8611900950a28398b
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2022-12-06 19:16:25 +0900

    feat: FlatTableを実装

---
+++ packages/wiz-ui/src/components/base/tables/flat/td.vue
@@ -0,0 +1,50 @@
+<template>
+  <td
+    class="wiz-flat-table__td"
+    :class="{
+      'wiz-flat-table__td--left': align === 'left',
+      'wiz-flat-table__td--center': align === 'center',
+      'wiz-flat-table__td--right': align === 'right',
+    }"
+  >
+    <slot />
+  </td>
+</template>
+
+<script setup lang="ts">
+import { THEME } from "@/constants";
+
+interface Props {
+  align?: "left" | "center" | "right";
+}
+
+withDefaults(defineProps<Props>(), {
+  align: "center",
+});
+
+const spacingSm = THEME.spacing.sm;
+const colorGray300 = THEME.color.gray["300"];
+const colorGray700 = THEME.color.gray["700"];
+const colorWhite800 = THEME.color.white["800"];
+</script>
+
+<style lang="scss" scoped>
+.wiz-flat-table__td {
+  padding: v-bind(spacingSm);
+  color: v-bind(colorGray700);
+  background: v-bind(colorWhite800);
+  border: 1px solid v-bind(colorGray300);
+
+  &--left {
+    text-align: left;
+  }
+
+  &--center {
+    text-align: center;
+  }
+
+  &--right {
+    text-align: right;
+  }
+}
+</style>
---
+++ packages/wiz-ui/src/components/base/tables/flat/th.vue
@@ -0,0 +1,56 @@
+<template>
+  <th
+    class="wiz-flat-table__th"
+    :class="{
+      'wiz-flat-table__th--col': scope === 'col',
+      'wiz-flat-table__th--row': scope === 'row',
+    }"
+  >
+    <slot />
+  </th>
+</template>
+
+<script setup lang="ts">
+import { THEME } from "@/constants";
+
+interface Props {
+  width?: string;
+  scope?: "col" | "row";
+}
+
+withDefaults(defineProps<Props>(), {
+  width: "auto",
+  scope: "col",
+});
+
+const colorWhite800 = THEME.color.white["800"];
+const colorGreen800 = THEME.color.green["800"];
+const colorGray300 = THEME.color.gray["300"];
+const colorGray400 = THEME.color.gray["400"];
+const colorGray700 = THEME.color.gray["700"];
+const fontSizeSm = THEME.fontSize.sm;
+const spacingSm = THEME.spacing.sm;
+</script>
+
+<style lang="scss" scoped>
+.wiz-flat-table__th {
+  width: v-bind(width);
+  font-size: v-bind(fontSizeSm);
+  font-weight: bold;
+  padding: v-bind(spacingSm);
+
+  &--col {
+    color: v-bind(colorWhite800);
+    background: v-bind(colorGreen800);
+    border: 1px solid v-bind(colorGray300);
+    text-align: center;
+  }
+
+  &--row {
+    color: v-bind(colorGray700);
+    background: v-bind(colorGray300);
+    border: 1px solid v-bind(colorGray400);
+    text-align: left;
+  }
+}
+</style>
