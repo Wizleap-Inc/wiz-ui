d88f0762a7c5ef87f68a61d0fa4f2331e6f1cbb2
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2022-12-09 04:24:27 +0900

    feat: ExampleAppにlayout実装を集約

--- examples/vue2/src/App.vue
+++ examples/vue2/src/App.vue
@@ -1,115 +1,16 @@
 <template>
   <WizProvider>
-    <WizCustomHeader
-      title="Wiz UI Playground"
-      avatarSrc="https://avatars.githubusercontent.com/u/10214025?v=4"
-      :tabs="notificationTabs"
-      :notifications="notificationItems"
-      sticky
-    />
+    <Header />
     <WizHStack>
-      <WizNavContainer sticky>
-        <WizNavItem
-          v-for="(item, i) in navigationItems"
-          :key="i"
-          :icon="item.icon"
-          :label="item.label"
-          :to="item.to"
-          :active="item.active"
-        />
-      </WizNavContainer>
+      <Navigation />
       <router-view />
     </WizHStack>
   </WizProvider>
 </template>
 
 <script setup lang="ts">
-import {
-  WizNavContainer,
-  WizNavItem,
-  WizIDashboard,
-  WizIAssignment,
-  WizIBusinessCenter,
-  WizIHelp,
-  WizProvider,
-  WizCustomHeader,
-  WizHStack,
-} from "@wizleap-inc/wiz-ui";
-import { NotificationTabItem } from "@wizleap-inc/wiz-ui/dist/components/custom/header/types";
-import { NotificationItem } from "@wizleap-inc/wiz-ui/dist/components/custom/notification/types";
-import { computed } from "vue";
-import { useRoute } from "vue-router/composables";
+import { WizProvider, WizHStack } from "@wizleap-inc/wiz-ui";
 
-const route = useRoute();
-const currentPath = computed(() => route.path);
-
-const navigationItems = computed(() => [
-  {
-    icon: WizIDashboard,
-    label: "Home",
-    to: "/",
-    active: currentPath.value === "/",
-  },
-  {
-    icon: WizIAssignment,
-    label: "About",
-    to: "/about",
-    active: currentPath.value === "/about",
-  },
-  {
-    icon: WizIBusinessCenter,
-    label: "Contact",
-    to: "/contact",
-    active: currentPath.value === "/contact",
-  },
-  {
-    icon: WizIHelp,
-    label: "Help",
-    to: "/help",
-    active: currentPath.value === "/help",
-  },
-]);
-
-const notificationTabs: NotificationTabItem[] = [
-  {
-    label: "タブ1",
-    name: "tab1",
-    notificationCount: 3,
-    variant: "primary",
-  },
-  {
-    label: "タブ2",
-    name: "tab2",
-    notificationCount: 0,
-    variant: "secondary",
-  },
-];
-
-const notificationItems: NotificationItem[] = Array.from(
-  { length: 14 },
-  (_, i) => ({
-    id: `${i}`,
-    title: "ほげほげほげほげほげほげほげほげほげほげほげほげほげほげ",
-    timestamp: new Date(Date.now() - Math.random() * 1000 * 60 * 60 * 24 * 365),
-    tabName: ["tab1", "tab2"][i % 2],
-    read: i % 3 === 0,
-    tableInfo: [
-      {
-        title: "情報1",
-        content: "データ1",
-      },
-      {
-        title: "情報2",
-        content: "データ2",
-      },
-    ],
-  })
-);
+import Header from "./components/header/index.vue";
+import Navigation from "./components/navigation/index.vue";
 </script>
-
-<style>
-body {
-  margin: 0;
-  background-color: #f5f5f5;
-}
-</style>
---
+++ examples/vue2/src/components/navigation/footer-info.vue
@@ -0,0 +1,38 @@
+<template>
+  <WizVStack>
+    <WizAnchor
+      to="https://wizleap.co.jp"
+      :icon="WizIOpenInNew"
+      iconPosition="right"
+      color="gray.700"
+      fontSize="sm"
+      openInNewTab
+    >
+      会社概要
+    </WizAnchor>
+    <WizAnchor
+      to="https://wizleap.co.jp/services/"
+      :icon="WizIOpenInNew"
+      iconPosition="right"
+      color="gray.700"
+      fontSize="sm"
+      openInNewTab
+    >
+      サービス
+    </WizAnchor>
+    <WizAnchor
+      to="https://wizleap.co.jp/news/"
+      :icon="WizIOpenInNew"
+      iconPosition="right"
+      color="gray.700"
+      fontSize="sm"
+      openInNewTab
+    >
+      ニュース
+    </WizAnchor>
+  </WizVStack>
+</template>
+
+<script setup lang="ts">
+import { WizAnchor, WizVStack, WizIOpenInNew } from "@wizleap-inc/wiz-ui";
+</script>
