6c9a224552b5f9b72c078161fbda0fd3bdfdcba9
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2022-12-11 16:13:40 +0900

    feat: Unstyled Tableを整備

---
+++ packages/wiz-ui/src/components/base/tables/unstyled/table.vue
@@ -0,0 +1,33 @@
+<template>
+  <table class="wiz-unstyled-table">
+    <slot />
+  </table>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+
+import { ComponentName } from "@/constants/component/name";
+import { SpacingKeys } from "@/types/styles/spacing";
+import { getSpacingCSS } from "@/utils/styles/spacing";
+
+defineOptions({
+  name: ComponentName.UnstyledTable,
+});
+
+interface Props {
+  space?: SpacingKeys;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  space: "no",
+});
+
+const computedSpace = computed(() => getSpacingCSS(props.space));
+</script>
+
+<style lang="scss" scoped>
+.wiz-unstyled-table {
+  border-spacing: v-bind(computedSpace);
+}
+</style>
