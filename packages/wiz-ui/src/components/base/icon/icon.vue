c837c268b6f3c34d2d7436247c89cbcc774ff6cd
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2022-12-16 04:30:10 +0900

    refactor: iconのスタイルをvanilla extract化

--- packages/wiz-ui/src/components/base/icon/icon.vue
+++ packages/wiz-ui/src/components/base/icon/icon.vue
@@ -1,22 +1,31 @@
 <template>
-  <div class="wiz-icon">
-    <component :is="icon" />
-    <div class="wiz-icon__badge" v-if="badge">
-      <span class="wiz-icon__badge__text">{{ badge }}</span>
+  <div :class="iconStyle">
+    <component :is="icon" :class="[fontSizeStyle[size], fillStyle[color]]" />
+    <div :class="[iconBadgeStyle, backgroundStyle[badgeBgColor]]" v-if="badge">
+      <span :class="[iconBadgeTextStyle, colorStyle[badgeColor]]">{{
+        badge
+      }}</span>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import {
-  THEME,
   ComponentName,
   ColorKeys,
-  getColorCss,
   FontSizeKeys,
-  getFontSizeCss,
 } from "@wizleap-inc/wiz-ui-constants";
-import { computed } from "vue";
+import {
+  iconStyle,
+  iconBadgeStyle,
+  iconBadgeTextStyle,
+} from "@wizleap-inc/wiz-ui-styles/bases/icon.css";
+import {
+  backgroundStyle,
+  colorStyle,
+  fillStyle,
+  fontSizeStyle,
+} from "@wizleap-inc/wiz-ui-styles/commons";
 
 import type { TIcon } from "@/components/icons";
 
@@ -32,52 +41,10 @@ interface Props {
   icon: TIcon;
 }
 
-const props = withDefaults(defineProps<Props>(), {
+withDefaults(defineProps<Props>(), {
   size: "xl2",
+  color: "gray.700",
   badgeColor: "white.800",
   badgeBgColor: "red.800",
 });
-
-const computedSize = computed(() => getFontSizeCss(props.size));
-const computedColor = computed(() =>
-  props.color ? getColorCss(props.color) : "currentColor"
-);
-const computedBadgeColor = computed(() => getColorCss(props.badgeColor));
-const computedBadgeBgColor = computed(() => getColorCss(props.badgeBgColor));
-
-const spacingMax = THEME.spacing.max;
-const fontSizeXl2 = THEME.fontSize.xl2;
-const fontSizeXs2 = THEME.fontSize.xs2;
 </script>
-
-<style lang="scss" scoped>
-.wiz-icon {
-  line-height: 1;
-  position: relative;
-  width: fit-content;
-  height: fit-content;
-
-  & > svg {
-    font-size: v-bind(computedSize);
-    fill: v-bind(computedColor);
-  }
-
-  &__badge {
-    position: absolute;
-    top: -40%;
-    right: -40%;
-    width: v-bind(fontSizeXl2);
-    height: v-bind(fontSizeXl2);
-    background-color: v-bind(computedBadgeBgColor);
-    border-radius: v-bind(spacingMax);
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-    &__text {
-      font-size: v-bind(fontSizeXs2);
-      color: v-bind(computedBadgeColor);
-    }
-  }
-}
-</style>
