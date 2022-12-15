71a08d937d19ae580d0232e5ae50ac806850f540
kawamurasorachi <sorakawa0218@gmail.com>
2022-12-15 15:10:12 +0900

    anchorのスタイルをvanilla extract化

--- packages/styles/index.ts
+++ packages/styles/index.ts
@@ -1,4 +1,1 @@
-import { createTheme } from "@vanilla-extract/css";
-import { THEME } from "@wizleap-inc/wiz-ui-constants";
-
-export const [themeClass, vars] = createTheme(THEME);
+export * from "./theme.css";
