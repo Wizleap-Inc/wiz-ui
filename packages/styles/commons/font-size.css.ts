ef14d57984ff5cb64a8232d0a03e520598d826c1
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2022-12-27 10:09:19 +0900

    refactor: font-sizeからimportantを削除

--- packages/styles/commons/font-size.css.ts
+++ packages/styles/commons/font-size.css.ts
@@ -2,5 +2,5 @@ import { styleVariants } from "@vanilla-extract/css";
 import { FONT_SIZE_MAP } from "@wizleap-inc/wiz-ui-constants";
 
 export const fontSizeStyle = styleVariants(FONT_SIZE_MAP, (fontSize) => ({
-  fontSize: `${fontSize} !important`,
+  fontSize,
 }));
--- packages/wiz-ui/src/components/base/heading/heading.vue
+++ packages/wiz-ui/src/components/base/heading/heading.vue
@@ -2,9 +2,9 @@
   <component
     :is="headingHTML"
     :class="[
-      headingStyle[level],
-      fontSize && fontSizeStyle[fontSize],
-      color && colorStyle[color],
+      headingStyle,
+      fontSizeStyle[computedFontSize],
+      colorStyle[computedColor],
     ]"
   >
     <slot />
@@ -31,8 +31,43 @@ interface Props {
   fontSize?: FontSizeKeys;
 }
 
+const DEFAULT_FONT_SIZE: Record<
+  Exclude<Props["level"], undefined>,
+  FontSizeKeys
+> = {
+  1: "xl2",
+  2: "xl",
+  3: "md",
+  4: "sm",
+  5: "xs",
+  6: "xs2",
+} as const;
+
+const DEFAULT_COLOR: Record<Exclude<Props["level"], undefined>, ColorKeys> = {
+  1: "gray.900",
+  2: "gray.800",
+  3: "gray.700",
+  4: "gray.700",
+  5: "gray.700",
+  6: "gray.700",
+} as const;
+
 const props = withDefaults(defineProps<Props>(), {
   level: 1,
+});
+
+const computedFontSize = computed(() => {
+  if (props.fontSize) {
+    return props.fontSize;
+  }
+  return DEFAULT_FONT_SIZE[props.level];
+});
+
+const computedColor = computed(() => {
+  if (props.color) {
+    return props.color;
+  }
+  return DEFAULT_COLOR[props.level];
 });
 
 const headingHTML = computed(() => `h${props.level}`);
