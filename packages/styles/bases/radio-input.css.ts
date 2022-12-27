df402bf83d87c1d7504e62a55cd73518fbd433df
ichi-h <85932406+ichi-h@users.noreply.github.com>
2022-12-28 00:37:59 +0900

    feat: radioのvanilla-extract化

---
+++ packages/styles/bases/radio-input.css.ts
@@ -0,0 +1,69 @@
+import { style, styleVariants } from "@vanilla-extract/css";
+import { THEME } from "@wizleap-inc/wiz-ui-constants";
+
+const borderWidth = "1px";
+
+export const radioStyle = style({
+  width: "fit-content",
+});
+
+export const radioInputStyle = style({
+  position: "absolute",
+  opacity: 0,
+  cursor: "pointer",
+});
+
+export const radioLabelStyle = style({
+  display: "flex",
+  alignItems: "center",
+  fontSize: THEME.fontSize.sm,
+  gap: THEME.spacing.sm,
+  ":before": {
+    content: "",
+    background: THEME.color.white["800"],
+    border: `${borderWidth} solid ${THEME.color.gray["400"]}`,
+    borderRadius: THEME.spacing.max,
+    width: THEME.spacing.md,
+    height: THEME.spacing.md,
+    boxSizing: "border-box",
+    display: "inline-block",
+  },
+});
+
+export const radioLabelCheckedStyle = style({
+  position: "relative",
+  ":before": {
+    border: `${borderWidth} solid ${THEME.color.green["800"]}`,
+  },
+  ":after": {
+    position: "absolute",
+    content: "",
+    width: THEME.spacing.xs,
+    height: THEME.spacing.xs,
+    left: THEME.spacing.xs2,
+    background: THEME.color.green["800"],
+    borderRadius: THEME.spacing.max,
+  },
+});
+
+export const radioLabelDisabledStyle = style({
+  opacity: 0.5,
+});
+
+export const radioLabelColorStyle = styleVariants({
+  default: {
+    color: THEME.color.gray["600"],
+  },
+  checked: {
+    color: THEME.color.green["800"],
+  },
+});
+
+export const radioLabelCursorStyle = styleVariants({
+  default: {
+    cursor: "pointer",
+  },
+  disabled: {
+    cursor: "not-allowed",
+  },
+});
--- packages/wiz-ui/src/components/base/inputs/radio/types.ts
+++ packages/wiz-ui/src/components/base/inputs/radio/types.ts
@@ -1,4 +1,4 @@
 export interface RadioOption {
   label: string;
-  value: string;
+  value: number;
 }
