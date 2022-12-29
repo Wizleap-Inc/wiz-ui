032fb4bb7fa00a8e69d084690987cb4c901d97b0
okakyo <0622okakyo@gmail.com>
2022-12-30 08:51:53 +0900

    feat: add descriptions

--- packages/wiz-ui/src/components/base/menu/menu-item.stories.ts
+++ packages/wiz-ui/src/components/base/menu/menu-item.stories.ts
@@ -27,15 +27,61 @@ export const Default = Template.bind({});
 Default.args = {
   label: "テスト",
 };
+Default.parameters = {
+  docs: {
+    description: {
+      story:
+        "サブメニューのコンポーネントです。このコンポーネントでは、`label` パラメータが必須です。",
+    },
+    source: {
+      code: `
+<template>
+  <WizMenuItem label="テスト"/>
+</template>
+      `,
+    },
+  },
+};
 
 export const Active = Template.bind({});
 Active.args = {
   label: "テスト",
   active: true,
 };
+Active.parameters = {
+  docs: {
+    description: {
+      story: `
+Active を設定することで、常にアクティブ化することもできます。
+      `,
+    },
+    source: {
+      code: `
+<template>
+  <WizMenuItem label="テスト" active/>
+</template>
+      `,
+    },
+  },
+};
 
 export const Disabled = Template.bind({});
 Disabled.args = {
   label: "テスト",
   clickable: false,
+};
+Disabled.parameters = {
+  docs: {
+    description: {
+      story:
+        "`clickable = false`  とすることで、クリックできないように設定することもできます。default は `true`",
+    },
+    source: {
+      code: `
+<template>
+  <WizMenuItem label="テスト" clickable="false"/>
+</template>
+      `,
+    },
+  },
 };
