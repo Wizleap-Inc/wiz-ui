3f6f0a81cbbdaadba9b2886361f1dfeb9d48b5bb
sor4chi <sorakawa0218@gmail.com>
2022-12-29 23:37:28 +0900

    refactor: card-tableをvanilla extract式のスタイルに変更

--- packages/wiz-ui/src/components/base/tables/card/th.vue
+++ packages/wiz-ui/src/components/base/tables/card/th.vue
@@ -1,11 +1,12 @@
 <template>
-  <th class="wiz-card-table__th">
+  <th :class="cardThStyle" :style="{ width }">
     <slot />
   </th>
 </template>
 
 <script setup lang="ts">
-import { THEME, ComponentName } from "@wizleap-inc/wiz-ui-constants";
+import { ComponentName } from "@wizleap-inc/wiz-ui-constants";
+import { cardThStyle } from "@wizleap-inc/wiz-ui-styles/bases/card-table.css";
 
 defineOptions({
   name: ComponentName.CardTh,
@@ -18,16 +19,4 @@ interface Props {
 withDefaults(defineProps<Props>(), {
   width: "auto",
 });
-
-const colorGray500 = THEME.color.gray["500"];
-const fontSizeXs2 = THEME.fontSize.xs2;
 </script>
-
-<style lang="scss" scoped>
-.wiz-card-table__th {
-  color: v-bind(colorGray500);
-  width: v-bind(width);
-  font-size: v-bind(fontSizeXs2);
-  font-weight: normal;
-}
-</style>
