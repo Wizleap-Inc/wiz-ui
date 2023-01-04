841a65999f8c439308eb872822a4aae6e1afe35c
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2023-01-04 13:08:18 +0900

    feat: wiz-ui-nextの環境追加

---
+++ packages/wiz-ui-next/vite.config.ts
@@ -0,0 +1,38 @@
+import path from "path";
+
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import Vue from "@vitejs/plugin-vue";
+import DefineOptions from "unplugin-vue-define-options/vite";
+import { defineConfig } from "vite";
+import Dts from "vite-plugin-dts";
+
+export default defineConfig({
+  plugins: [
+    Vue(),
+    Dts({
+      entryRoot: path.resolve(__dirname, "src"),
+    }),
+    DefineOptions(),
+    vanillaExtractPlugin(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "Wiz-UI",
+      fileName: (format) => `wiz-ui.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+});
