e046d6a34b8b3b4b54f55d7da70a61616ddb3be9
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2023-01-04 23:38:05 +0900

    feat: WizSelectBoxのVue3対応

---
+++ packages/wiz-ui-next/src/components/base/inputs/selectbox/selectbox.vue
@@ -0,0 +1,128 @@
+<template>
+  <WizPopupContainer v-model="openSelectBox" :expand="expand">
+    <div
+      :class="[
+        selectBoxStyle,
+        openSelectBox && selectBoxActiveStyle,
+        disabled && selectBoxDisabledStyle,
+        selectBoxCursorStyle[selectBoxCursor],
+      ]"
+      :style="{ width: computedWidth }"
+    >
+      <div
+        :class="[
+          selectBoxInnerBoxStyle,
+          !!modelValue && selectBoxInnerBoxSelectedStyle,
+        ]"
+        @click="toggleSelectBox"
+      >
+        <WizHStack align="center" justify="between" height="100%">
+          <span v-if="!modelValue">{{ placeholder }}</span>
+          <span
+            v-for="(option, key) in options"
+            v-show="option.value === modelValue"
+            :key="'selected' + key"
+            :class="selectBoxInnerBoxSelectedValueStyle"
+          >
+            {{ option.label }}
+          </span>
+          <WizIExpandLess
+            v-if="openSelectBox"
+            :class="selectBoxInnerBoxLessStyle"
+          />
+          <WizIExpandMore
+            v-else-if="!openSelectBox"
+            :class="selectBoxInnerBoxMoreStyle"
+          />
+        </WizHStack>
+      </div>
+    </div>
+    <WizPopup layer="popover">
+      <div
+        :class="selectBoxSelectorStyle"
+        :style="{ 'min-width': `calc(${width} + ${BORDER_WIDTH} * 2)` }"
+      >
+        <WizVStack gap="xs2">
+          <div
+            :class="selectBoxSelectorOptionStyle"
+            v-for="(option, key) in options"
+            :key="'option' + key"
+            @click="onSelect(option.value)"
+          >
+            {{ option.label }}
+          </div>
+        </WizVStack>
+      </div>
+    </WizPopup>
+  </WizPopupContainer>
+</template>
+
+<script setup lang="ts">
+import { ComponentName } from "@wizleap-inc/wiz-ui-constants";
+import {
+  BORDER_WIDTH,
+  selectBoxStyle,
+  selectBoxActiveStyle,
+  selectBoxDisabledStyle,
+  selectBoxCursorStyle,
+  selectBoxInnerBoxStyle,
+  selectBoxInnerBoxSelectedValueStyle,
+  selectBoxInnerBoxLessStyle,
+  selectBoxInnerBoxMoreStyle,
+  selectBoxInnerBoxSelectedStyle,
+  selectBoxSelectorStyle,
+  selectBoxSelectorOptionStyle,
+} from "@wizleap-inc/wiz-ui-styles/bases/selectbox-input.css";
+import { ref, computed } from "vue";
+
+import { WizPopupContainer, WizPopup } from "@/components";
+import { WizIExpandLess, WizIExpandMore } from "@/components/icons";
+
+import { WizHStack, WizVStack } from "../../stack";
+
+import { Option } from "./types";
+
+defineOptions({
+  name: ComponentName.SelectBox,
+});
+
+interface Props {
+  options: Option[];
+  modelValue: string;
+  placeholder?: string;
+  width?: string;
+  disabled?: boolean;
+  expand?: boolean;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  modelValue: "",
+  placeholder: "選択してください",
+  width: "10rem",
+  disabled: false,
+});
+
+const openSelectBox = ref(false);
+const toggleSelectBox = () => {
+  if (props.disabled) {
+    return;
+  }
+  openSelectBox.value = !openSelectBox.value;
+};
+
+interface Emit {
+  (e: "update:modelValue", value: string): void;
+}
+const emit = defineEmits<Emit>();
+
+const onSelect = (value: string) => {
+  toggleSelectBox();
+  emit("update:modelValue", value);
+};
+
+const selectBoxCursor = computed(() =>
+  props.disabled ? "disabled" : "default"
+);
+
+const computedWidth = computed(() => (props.expand ? "100%" : props.width));
+</script>
