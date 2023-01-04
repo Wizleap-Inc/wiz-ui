b0afafe353bddee3f57bd7d67f9db541c95953f9
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2023-01-04 17:50:15 +0900

    feat: WizTagのVue3対応

---
+++ packages/wiz-ui-next/src/components/base/tag/tag.vue
@@ -0,0 +1,53 @@
+<template>
+  <span
+    :class="[
+      tagStlye,
+      colorStyle[color],
+      fontSizeStyle[fontSize],
+      fontWeightStyle[fontWeight],
+      backgroundStyle[backgroundColor],
+    ]"
+  >
+    <WizIcon v-if="icon" :icon="icon" :color="color" />
+    {{ label }}
+  </span>
+</template>
+
+<script setup lang="ts">
+import {
+  ComponentName,
+  ColorKeys,
+  FontSizeKeys,
+  FontWeightKeys,
+} from "@wizleap-inc/wiz-ui-constants";
+import { tagStlye } from "@wizleap-inc/wiz-ui-styles/bases/tag.css";
+import {
+  backgroundStyle,
+  colorStyle,
+  fontSizeStyle,
+  fontWeightStyle,
+} from "@wizleap-inc/wiz-ui-styles/commons";
+
+import { WizIcon } from "@/components/base";
+import type { TIcon } from "@/components/icons";
+
+defineOptions({
+  name: ComponentName.Tag,
+});
+
+interface Props {
+  color?: ColorKeys;
+  fontSize?: FontSizeKeys;
+  fontWeight?: FontWeightKeys;
+  backgroundColor?: ColorKeys;
+  icon?: TIcon;
+  label: string;
+}
+
+withDefaults(defineProps<Props>(), {
+  color: "white.800",
+  backgroundColor: "blue.800",
+  fontSize: "md",
+  fontWeight: "normal",
+});
+</script>
