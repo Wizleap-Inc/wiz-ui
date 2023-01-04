1079ecc7a9860d125a5b7953fe6d987f72d20e4d
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2023-01-04 17:08:06 +0900

    feat: icon componentを対応

---
+++ packages/wiz-ui-next/src/components/icons/icons.stories.ts
@@ -0,0 +1,30 @@
+import { StoryFn } from "@storybook/vue";
+import { THEME } from "@wizleap-inc/wiz-ui-constants";
+
+import * as Icons from ".";
+
+export default {
+  title: "Icons",
+};
+
+const OverviewTemplate: StoryFn = (_, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { ...Icons },
+  setup() {
+    const displayIcons = Object.entries(Icons).map(([name, component]) => ({
+      name,
+      comp: component,
+    }));
+    return { displayIcons };
+  },
+  template: `
+    <div style="display: flex; flex-wrap: wrap; gap: 1rem;">
+      <div style="display: flex; flex-direction: column; align-items: center;" v-for="{name, comp} in displayIcons" :key="name">
+        <span style="color: ${THEME.color.gray["700"]}">{{ name }}</span>
+        <component :is="comp" style="font-size: 4rem; fill: ${THEME.color.gray["700"]}" />
+      </div>
+    </div>
+  `,
+});
+
+export const Overview = OverviewTemplate.bind({});
---
+++ packages/wiz-ui-next/src/components/icons/tab.vue
@@ -0,0 +1,20 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="1em"
+    width="1em"
+    viewBox="0 0 24 24"
+  >
+    <path
+      d="M4.625 19q-.7 0-1.162-.462Q3 18.075 3 17.375V6.625q0-.7.463-1.162Q3.925 5 4.625 5h14.75q.7 0 1.163.463.462.462.462 1.162v10.75q0 .7-.462 1.163-.463.462-1.163.462ZM13 8.375V6H4.625q-.275 0-.45.175T4 6.625v10.75q0 .275.175.45t.45.175h14.75q.275 0 .45-.175t.175-.45V10h-5.375q-.675 0-1.15-.475Q13 9.05 13 8.375Zm-9 9V6h.625q-.275 0-.45.175T4 6.625v10.75q0 .275.175.45t.45.175H4v-.625Z"
+    />
+  </svg>
+</template>
+
+<script setup lang="ts">
+import { ComponentName } from "@wizleap-inc/wiz-ui-constants";
+
+defineOptions({
+  name: ComponentName.ITab,
+});
+</script>
