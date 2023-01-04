61e8969e74f06eda7a4760fdfed9baa921af73be
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2023-01-04 17:01:16 +0900

    fix: shims型がVue2.7のものになっていたので3に更新

--- examples/vue3/src/shims.d.ts
+++ examples/vue3/src/shims.d.ts
@@ -1,5 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/ban-types */
 /* eslint-disable init-declarations */
+
 /// <reference types="vite/client" />
 declare module "*.vue" {
   import { DefineComponent } from "vue";
