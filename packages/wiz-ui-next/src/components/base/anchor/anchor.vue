0f6105d4fd0b335c6efd1a7d30d2c0ac28b0a1f5
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2023-01-05 14:56:54 +0900

    fix: WizAnchorのVue3対応

---
+++ packages/wiz-ui-next/src/components/base/anchor/anchor.vue
@@ -0,0 +1,85 @@
+<template>
+  <component
+    :class="[
+      anchorStyle,
+      colorStyle[color],
+      fontSizeStyle[fontSize],
+      fontWeightStyle[fontWeight],
+      nowrap && anchorNoWrapStyle,
+    ]"
+    :is="isRouterLink ? 'router-link' : 'a'"
+    :to="isRouterLink ? to : undefined"
+    :href="isRouterLink ? undefined : to"
+    :target="openInNewTab ? '_blank' : undefined"
+    :rel="openInNewTab ? 'noopener noreferrer' : undefined"
+  >
+    <WizIcon
+      :icon="icon"
+      v-if="icon && iconPosition === 'left'"
+      :size="getRelativeFontSize(fontSize, 3)"
+      :color="color"
+    />
+    <slot />
+    <WizIcon
+      :icon="icon"
+      v-if="icon && iconPosition === 'right'"
+      :size="getRelativeFontSize(fontSize, 3)"
+      :color="color"
+    />
+  </component>
+</template>
+
+<script setup lang="ts">
+import {
+  ComponentName,
+  ColorKeys,
+  FontSizeKeys,
+  getRelativeFontSize,
+  FontWeightKeys,
+} from "@wizleap-inc/wiz-ui-constants";
+import {
+  anchorStyle,
+  anchorNoWrapStyle,
+} from "@wizleap-inc/wiz-ui-styles/bases/anchor.css";
+import {
+  colorStyle,
+  fontSizeStyle,
+  fontWeightStyle,
+} from "@wizleap-inc/wiz-ui-styles/commons";
+import { computed, withDefaults } from "vue";
+import { RouterLinkProps } from "vue-router/types/router";
+
+import { WizIcon, TIcon } from "@/components";
+
+interface Props {
+  to: RouterLinkProps["to"];
+  color?: ColorKeys;
+  fontSize?: FontSizeKeys;
+  fontWeight?: FontWeightKeys;
+  icon?: TIcon;
+  iconPosition?: "left" | "right";
+  openInNewTab?: boolean;
+  nowrap?: boolean;
+}
+
+defineOptions({
+  name: ComponentName.Anchor,
+});
+
+const props = withDefaults(defineProps<Props>(), {
+  color: "blue.800",
+  fontSize: "md",
+  fontWeight: "normal",
+  iconPosition: "left",
+});
+
+const isRouterLink = computed(() => {
+  if (props.to === undefined) return false;
+  // propsのtoがobjectだった時点でLocation遷移なので、RouterLinkを使う
+  if (typeof props.to === "object") return true;
+  // propsのtoがhttpから始まってる時点で外部リンクなので、aタグを使う
+  if (props.to.startsWith("http")) return false;
+  // propsのtargetが_blankだったら新規タブで読み込まないといけないので、aタグ
+  return props.openInNewTab === false;
+});
+</script>
