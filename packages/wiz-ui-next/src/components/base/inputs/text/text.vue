14f5d5f22cf656c82c66b10c434b41acec0d5578
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2023-01-05 22:33:09 +0900

    feat: text inputを拡張、errorの赤枠を表現できるように

--- packages/wiz-ui-next/src/components/base/inputs/text/text.vue
+++ packages/wiz-ui-next/src/components/base/inputs/text/text.vue
@@ -6,19 +6,23 @@
     :disabled="disabled"
     :expand="expand"
     :width="width"
+    :error="isError"
     type="text"
   />
 </template>
 
 <script setup lang="ts">
 import { ComponentName } from "@wizleap-inc/wiz-ui-constants";
-import { computed } from "vue";
+import { computed, inject } from "vue";
+
+import { formControlKey } from "@/hooks/use-form-control-provider";
 
 import { PrivateBaseInput } from "../base";
 
 defineOptions({
   name: ComponentName.TextInput,
 });
+
 interface Props {
   modelValue: string;
   name: string;
@@ -40,4 +44,8 @@ const value = computed({
   get: () => props.modelValue,
   set: (value) => emit("update:modelValue", value),
 });
+
+// Form Control
+const form = inject(formControlKey);
+const isError = computed(() => (form ? form.isError.value : false));
 </script>
