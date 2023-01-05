560a5cd3d778c37e82d24ff8eaaabd2d2854188d
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2023-01-05 23:46:49 +0900

    feat: 全入力系コンポーネントのFormGroupStoryを作成

--- packages/wiz-ui-next/src/components/custom/form/group.stories.ts
+++ packages/wiz-ui-next/src/components/custom/form/group.stories.ts
@@ -2,7 +2,19 @@ import { Meta, StoryFn } from "@storybook/vue3";
 import { SPACING_ACCESSORS } from "@wizleap-inc/wiz-ui-constants";
 import { ref } from "vue";
 
-import { WizTextInput } from "@/components";
+import {
+  WizTextInput,
+  WizPasswordInput,
+  WizSelectBox,
+  WizCheckBox,
+  WizRadio,
+  WizTextArea,
+  WizDatePicker,
+  WizTimePicker,
+} from "@/components";
+import { CheckBoxOption } from "@/components/base/inputs/checkbox/types";
+import { RadioOption } from "@/components/base/inputs/radio/types";
+import { SelectBoxOption } from "@/components/base/inputs/selectbox/types";
 
 import { WizFormGroup, WizFormControl } from ".";
 
@@ -114,3 +126,152 @@ Gap.parameters = {
     },
   },
 };
+
+const CHOICES = [
+  {
+    label: "選択肢1",
+    value: 1,
+  },
+  {
+    label: "選択肢2",
+    value: 2,
+  },
+];
+
+export const AllInput: StoryFn<typeof WizFormControl> = () => ({
+  components: {
+    WizFormGroup,
+    WizFormControl,
+    WizTextInput,
+    WizPasswordInput,
+    WizSelectBox,
+    WizCheckBox,
+    WizRadio,
+    WizTextArea,
+    WizDatePicker,
+    WizTimePicker,
+  },
+  setup() {
+    const textInput = ref("");
+    const passwordInput = ref("");
+    const textareaInput = ref("");
+    const SELECT_OPTIONS: SelectBoxOption[] = CHOICES;
+    const selectInput = ref<string | null>(null);
+    const CHECKBOX_OPTIONS: CheckBoxOption[] = CHOICES;
+    const checkboxInput = ref<number[]>([]);
+    const RADIO_OPTIONS: RadioOption[] = CHOICES;
+    const radioInput = ref<number | null>(null);
+    const datePickerInput = ref(new Date(2021, 0, 1));
+    const timePickerInput = ref("");
+    return {
+      textInput,
+      passwordInput,
+      textareaInput,
+      selectInput,
+      SELECT_OPTIONS,
+      checkboxInput,
+      CHECKBOX_OPTIONS,
+      radioInput,
+      RADIO_OPTIONS,
+      datePickerInput,
+      timePickerInput,
+    };
+  },
+  template: `
+    <WizFormGroup label-width="10rem" gap="md">
+      <WizFormControl label="WizTextInput">
+        <WizTextInput v-model="textInput" name="textInput" placeholder="入力してください" />
+      </WizFormControl>
+      <WizFormControl label="WizPasswordInput">
+        <WizPasswordInput v-model="passwordInput" name="passwordInput" placeholder="入力してください" />
+      </WizFormControl>
+      <WizFormControl label="WizSelectBox">
+        <WizSelectBox v-model="selectInput" name="selectInput" placeholder="選択してください" :options="SELECT_OPTIONS" />
+      </WizFormControl>
+      <WizFormControl label="WizCheckBox">
+        <WizCheckBox v-model="checkboxInput" name="checkboxInput" :options="CHECKBOX_OPTIONS" />
+      </WizFormControl>
+      <WizFormControl label="WizRadio">
+        <WizRadio v-model="radioInput" name="radioInput" :options="RADIO_OPTIONS" />
+      </WizFormControl>
+      <WizFormControl label="WizDatePicker">
+        <WizDatePicker v-model="datePickerInput" name="datePickerInput"/>
+      </WizFormControl>
+      <WizFormControl label="WizTimePicker">
+        <WizTimePicker v-model="timePickerInput" name="timePickerInput"/>
+      </WizFormControl>
+      <WizFormControl label="WizTextArea">
+        <WizTextArea v-model="textareaInput" name="textareaInput" placeholder="入力してください" />
+      </WizFormControl>
+    </WizFormGroup>
+  `,
+});
+
+export const AllInputError: StoryFn<typeof WizFormControl> = () => ({
+  components: {
+    WizFormGroup,
+    WizFormControl,
+    WizTextInput,
+    WizPasswordInput,
+    WizSelectBox,
+    WizCheckBox,
+    WizRadio,
+    WizTextArea,
+    WizDatePicker,
+    WizTimePicker,
+  },
+  setup() {
+    const textInput = ref("");
+    const passwordInput = ref("");
+    const textareaInput = ref("");
+    const SELECT_OPTIONS: SelectBoxOption[] = CHOICES;
+    const selectInput = ref<string | null>(null);
+    const CHECKBOX_OPTIONS: CheckBoxOption[] = CHOICES;
+    const checkboxInput = ref<number[]>([]);
+    const RADIO_OPTIONS: RadioOption[] = CHOICES;
+    const radioInput = ref<number | null>(null);
+    const datePickerInput = ref(new Date(2021, 0, 1));
+    const timePickerInput = ref("");
+    return {
+      textInput,
+      passwordInput,
+      textareaInput,
+      selectInput,
+      SELECT_OPTIONS,
+      checkboxInput,
+      CHECKBOX_OPTIONS,
+      radioInput,
+      RADIO_OPTIONS,
+      datePickerInput,
+      timePickerInput,
+    };
+  },
+  template: `
+    <WizFormGroup label-width="10rem" gap="md">
+      <WizFormControl label="WizTextInput" error="エラーが発生しました">
+        <WizTextInput v-model="textInput" name="textInput" placeholder="入力してください" />
+      </WizFormControl>
+      <WizFormControl label="WizPasswordInput" error="エラーが発生しました">
+        <WizPasswordInput v-model="passwordInput" name="passwordInput" placeholder="入力してください" />
+      </WizFormControl>
+      <WizFormControl label="WizSelectBox" error="エラーが発生しました">
+        <WizSelectBox v-model="selectInput" name="selectInput" placeholder="選択してください" :options="SELECT_OPTIONS" />
+      </WizFormControl>
+      <WizFormControl label="WizCheckBox" error="エラーが発生しました">
+        <WizCheckBox v-model="checkboxInput" name="checkboxInput" :options="CHECKBOX_OPTIONS" />
+      </WizFormControl>
+      <WizFormControl label="WizRadio" error="エラーが発生しました">
+        <WizRadio v-model="radioInput" name="radioInput" :options="RADIO_OPTIONS" />
+      </WizFormControl>
+      <WizFormControl label="WizDatePicker" error="エラーが発生しました">
+        <WizDatePicker v-model="datePickerInput" name="datePickerInput" />
+      </WizFormControl>
+      <WizFormControl label="WizTimePicker" error="エラーが発生しました">
+        <WizTimePicker v-model="timePickerInput" name="timePickerInput" />
+      </WizFormControl>
+      <WizFormControl label="WizTextArea" error="エラーが発生しました">
+        <WizTextArea v-model="textareaInput" name="textareaInput" placeholder="入力してください" />
+      </WizFormControl>
+    </WizFormGroup>
+  `,
+});
