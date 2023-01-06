4e6ca9783429ffdbd940061df0faf262f8cf6c52
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2023-01-06 17:14:59 +0900

    feat: Vue2にBaseInputのerror拡張をバックポート

--- packages/wiz-ui/src/components/base/inputs/base/base.vue
+++ packages/wiz-ui/src/components/base/inputs/base/base.vue
@@ -1,12 +1,15 @@
 <template>
   <input
-    :class="[baseInputStyle, disabled && baseInputDisabledStyle]"
+    :class="[
+      baseInputStyle,
+      disabled && baseInputDisabledStyle,
+      inputBorderStyle[state],
+    ]"
     :style="{ width: computedWidth }"
     :placeholder="placeholder"
     :name="name"
     :disabled="disabled"
-    :value="value"
-    @input="onInput"
+    v-model="textValue"
     :type="type"
   />
 </template>
@@ -17,7 +20,8 @@ import {
   baseInputStyle,
   baseInputDisabledStyle,
 } from "@wizleap-inc/wiz-ui-styles/bases/base-input.css";
-import { computed } from "vue";
+import { inputBorderStyle } from "@wizleap-inc/wiz-ui-styles/commons";
+import { computed, ref } from "vue";
 
 defineOptions({
   name: ComponentName.BaseInput,
@@ -31,6 +35,7 @@ interface Props {
   expand?: boolean;
   type: "text" | "password";
   width?: string;
+  error?: boolean;
 }
 
 const props = withDefaults(defineProps<Props>(), {
@@ -43,10 +48,18 @@ interface Emit {
 
 const emit = defineEmits<Emit>();
 
-const onInput = (e: Event) => {
-  const target = e.target as HTMLInputElement;
-  emit("input", target.value);
-};
+const textValue = computed({
+  get: () => props.value,
+  set: (value) => emit("input", value),
+});
+
+const hasFocus = ref(false);
 
 const computedWidth = computed(() => (props.expand ? "100%" : props.width));
+
+const state = computed(() => {
+  if (props.error) return "error";
+  if (hasFocus.value) return "active";
+  return "default";
+});
 </script>
