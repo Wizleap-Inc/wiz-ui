63739f693beacabda9589b8a412fc3fc564c439c
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2023-01-07 14:54:45 +0900

    feat: WizSkeletonを作成

---
+++ packages/wiz-ui-next/src/components/base/skeleton/skeleton.vue
@@ -0,0 +1,39 @@
+<template>
+  <div
+    :class="isLoading && [skeletonStyle, normalSkeletonStyle]"
+    :style="{ width, height }"
+  >
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ComponentName } from "@wizleap-inc/wiz-ui-constants";
+import {
+  skeletonStyle,
+  normalSkeletonStyle,
+} from "@wizleap-inc/wiz-ui-styles/bases/skeleton.css";
+import { PropType } from "vue";
+
+defineOptions({
+  name: ComponentName.Skeleton,
+});
+
+defineProps({
+  width: {
+    type: String as PropType<string>,
+    default: "100%",
+    required: false,
+  },
+  height: {
+    type: String as PropType<string>,
+    default: "100%",
+    required: false,
+  },
+  isLoading: {
+    type: Boolean as PropType<boolean>,
+    default: true,
+    required: false,
+  },
+});
+</script>
