5ef965ebe7256c7d304a7870dad4bad63de066a4
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2023-01-07 14:54:55 +0900

    feat: WizSkeletonTextを作成

---
+++ packages/wiz-ui/src/components/base/skeleton/skeleton-text.vue
@@ -0,0 +1,64 @@
+<template>
+  <WizVStack v-if="isLoading" :gap="gap">
+    <div v-for="line in lines" :key="line">
+      <div
+        :class="[
+          skeletonStyle,
+          normalSkeletonStyle,
+          textSkeletonStyle[judgeOrder(line)],
+          fontSizeAsHeightStyle[fontSize],
+        ]"
+      />
+    </div>
+  </WizVStack>
+  <slot v-else />
+</template>
+
+<script setup lang="ts">
+import {
+  ComponentName,
+  FontSizeKeys,
+  SpacingKeys,
+} from "@wizleap-inc/wiz-ui-constants";
+import {
+  skeletonStyle,
+  normalSkeletonStyle,
+  textSkeletonStyle,
+} from "@wizleap-inc/wiz-ui-styles/bases/skeleton.css";
+import { fontSizeAsHeightStyle } from "@wizleap-inc/wiz-ui-styles/commons";
+import { PropType } from "vue";
+
+import { WizVStack } from "@/components";
+
+defineOptions({
+  name: ComponentName.SkeletonText,
+});
+
+const props = defineProps({
+  fontSize: {
+    type: String as PropType<FontSizeKeys>,
+    default: "lg",
+    required: false,
+  },
+  gap: {
+    type: String as PropType<SpacingKeys>,
+    default: "sm",
+    required: false,
+  },
+  lines: {
+    type: Number as PropType<number>,
+    default: 1,
+    required: false,
+  },
+  isLoading: {
+    type: Boolean as PropType<boolean>,
+    default: true,
+    required: false,
+  },
+});
+
+const judgeOrder = (line: number) => {
+  if (props.lines === 1) return "noLast";
+  return line === props.lines ? "last" : "noLast";
+};
+</script>
