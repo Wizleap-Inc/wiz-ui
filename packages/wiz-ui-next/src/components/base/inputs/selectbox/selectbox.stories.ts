46c8568f88fce72f512e3b177b90f2b407eaa82b
okakyo <0622okakyo@gmail.com>
2023-01-22 20:56:27 +0900

    feat: add selectbox input docs

--- packages/wiz-ui-next/src/components/base/inputs/selectbox/selectbox.stories.ts
+++ packages/wiz-ui-next/src/components/base/inputs/selectbox/selectbox.stories.ts
@@ -50,11 +50,42 @@ export const Default = Template.bind({});
 Default.args = {
   options: _getDummyOptions("test", 3),
 };
+Default.parameters = {
+  docs: {
+    source: {
+      code: `
+<template>
+  <div>
+    <p>入力値：{{ value }}</p>
+    <WizRadio options="[]" v-model="value" @input="input"/>
+  </div>
+</template>
+      `,
+    },
+  },
+};
 
 export const Disabled = Template.bind({});
 Disabled.args = {
   options: [],
   disabled: true,
+};
+Disabled.parameters = {
+  docs: {
+    description: {
+      story: "`disabled` に指定することもできます。",
+    },
+    source: {
+      code: `
+<template>
+  <div>
+    <p>入力値：{{ value }}</p>
+    <WizRadio disabled options="[]" v-model="value" @input="input"/>
+  </div>
+</template>
+      `,
+    },
+  },
 };
 
 const MultiTemplate: StoryFn<typeof WizSelectBox> = (args) => ({
@@ -70,10 +101,53 @@ const MultiTemplate: StoryFn<typeof WizSelectBox> = (args) => ({
     </WizHStack>
   `,
 });
+Disabled.parameters = {
+  docs: {
+    description: {
+      story: "`disabled` に指定することもできます。",
+    },
+    source: {
+      code: `
+<template>
+  <div>
+    <p>入力値：{{ value }}</p>
+    <WizRadio disabled options="[]" v-model="value" @input="input"/>
+  </div>
+</template>
+      `,
+    },
+  },
+};
 
 export const LongLabel = MultiTemplate.bind({});
 LongLabel.args = {
   options: _getDummyOptions("ThisIsTooLongLabelThisIsTooLongLabel", 3),
+};
+
+LongLabel.parameters = {
+  docs: {
+    description: {
+      story: "Label が長文になった場合は以下の通りになります。",
+    },
+    source: {
+      code: `
+<script lang="ts" setup>
+const options = [
+  { label: "ThisIsTooLongLabelThisIsTooLongLabel1", value: 1},
+  { label: "ThisIsTooLongLabelThisIsTooLongLabel2", value: 2 },
+  { label: "ThisIsTooLongLabelThisIsTooLongLabel3", value: 3 },
+]
+</script>
+
+<template>
+    <WizHStack>
+      <WizSelectBox :options="options" v-model="value" @input="input"/>
+      <WizSelectBox :options="options"  v-model="value" @input="input"/>
+    </WizHStack>
+</template>
+      `,
+    },
+  },
 };
 
 export const Expand = Template.bind({});
@@ -81,13 +155,98 @@ Expand.args = {
   options: _getDummyOptions("test", 1),
   expand: true,
 };
+Expand.parameters = {
+  docs: {
+    description: {
+      story: "コンポーネントを幅いっぱいに広げるように指定することができます。",
+    },
+    source: {
+      code: `
+<script lang="ts" setup>
+  const options = [
+    { label: "test1", value: 1},
+  ]
+</script>
 
+<template>
+    <WizHStack>
+      <WizSelectBox :options="options" expand v-model="value" @input="input"/>
+      <WizSelectBox :options="options" expand v-model="value" @input="input"/>
+    </WizHStack>
+</template>
+      `,
+    },
+  },
+};
 export const ManyOptions = Template.bind({});
 ManyOptions.args = {
   options: _getDummyOptions("test", 15),
 };
 
+ManyOptions.parameters = {
+  docs: {
+    description: {
+      story: "コンポーネントを幅いっぱいに広げるように指定することができます。",
+    },
+    source: {
+      code: `
+<script lang="ts" setup>
+  const options = [
+    { label: "test1", value: 1},
+    { label: "test2", value: 2},
+    { label: "test3", value: 3},
+    { label: "test4", value: 4},
+    { label: "test5", value: 5},
+    { label: "test6", value: 6},
+    { label: "test7", value: 7},
+    { label: "test8", value: 8},
+    { label: "test9", value: 9},
+    { label: "test10", value: 10},
+    { label: "test11", value: 11},
+    { label: "test12", value: 12},
+    { label: "test13", value: 13},
+    { label: "test14", value: 14},
+    { label: "test15", value: 15},
+  ]
+</script>
+
+<template>
+    <WizHStack>
+      <WizSelectBox :options="options" expand v-model="value" @input="input"/>
+      <WizSelectBox :options="options" expand v-model="value" @input="input"/>
+    </WizHStack>
+</template>
+      `,
+    },
+  },
+};
+
 export const ExtraLabel = Template.bind({});
 ExtraLabel.args = {
   options: _getDummyOptions("test", 3, "(10)"),
+};
+ExtraLabel.parameters = {
+  docs: {
+    description: {
+      story: "追加でラベルを追加をすることができます。",
+    },
+    source: {
+      code: `
+
+<script lang="ts" setup>
+  const options = [
+    { label: "test1", value: 1, exLabel: "(10)"},
+    { label: "test2", value: 2, exLabel: "(10)"},
+    { label: "test3", value: 3, exLabel: "(10)"},
+  ];
+</script>
+<template>
+    <WizHStack>
+      <WizSelectBox :options="options" v-model="value" @input="input"/>
+      <WizSelectBox :options="options" v-model="value" @input="input"/>
+    </WizHStack>
+</template>
+      `,
+    },
+  },
 };
