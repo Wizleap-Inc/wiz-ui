a9286a0ab375721dabfe104dabf2f8643d901dfe
okakyo <0622okakyo@gmail.com>
2023-01-22 14:59:28 +0900

    feat: add icon docs

--- packages/wiz-ui-next/src/components/base/icon/icon.stories.ts
+++ packages/wiz-ui-next/src/components/base/icon/icon.stories.ts
@@ -37,6 +37,17 @@ export const Default = Template.bind({});
 Default.args = {
   icon: WizINotification,
 };
+Default.parameters = {
+  docs: {
+    source: {
+      code: `
+<template>
+  <WizIcon :icon="WizNotification" />
+</template>
+      `,
+    },
+  },
+};
 
 export const Size: StoryFn<typeof WizIcon> = (args) => ({
   components: { WizIcon },
@@ -54,6 +65,26 @@ export const Size: StoryFn<typeof WizIcon> = (args) => ({
     </div>
   `,
 });
+Size.parameters = {
+  docs: {
+    description: {
+      story:
+        "Icon のサイズを指定することができます。選択肢はVariablesのFontSizeを参照してください。",
+    },
+    source: {
+      code: `
+<template>
+  <div style="display: grid; grid-template-columns: max-content max-content; grid-gap: 1rem; align-items: center;">
+    <template v-for="size in FONT_SIZE_ACCESSORS">
+      <div>Size = {{ size }}</div>
+      <WizIcon :icon="WizINotification" :size="size" />
+    </template>
+  </div>
+</template>
+      `,
+    },
+  },
+};
 
 export const Color: StoryFn<typeof WizIcon> = (args) => ({
   components: { WizIcon },
@@ -71,6 +102,26 @@ export const Color: StoryFn<typeof WizIcon> = (args) => ({
     </div>
   `,
 });
+Color.parameters = {
+  docs: {
+    description: {
+      story:
+        "Icon の色を指定することができます。選択肢はVariablesのColorKeysを参照してください。",
+    },
+    source: {
+      code: `
+<template>
+  <div style="display: grid; grid-template-columns: max-content max-content; grid-gap: 1rem; align-items: center;">
+    <template v-for="color in COLOR_MAP_ACCESSORS">
+      <div>Color = {{ color }}</div>
+      <WizIcon :icon="WizINotification" :color="color" />
+    </template>
+  </div>
+</template>
+      `,
+    },
+  },
+};
 
 export const Icon: StoryFn<typeof WizIcon> = (args) => ({
   components: { WizIcon },
@@ -87,3 +138,22 @@ export const Icon: StoryFn<typeof WizIcon> = (args) => ({
     </div>
   `,
 });
+Icon.parameters = {
+  docs: {
+    description: {
+      story: "Icon は次のように指定します。",
+    },
+    source: {
+      code: `
+<template>
+  <div style="display: grid; grid-template-columns: max-content max-content; grid-gap: 1rem; align-items: center;">
+    <template v-for="icon in Object.values(Icons)">
+      <div>Icon = {{ icon.name }}</div>
+      <WizIcon :icon="icon" />
+    </template>
+  </div>
+</template>
+      `,
+    },
+  },
+};
--- packages/wiz-ui/src/components/base/icon/icon.stories.ts
+++ packages/wiz-ui/src/components/base/icon/icon.stories.ts
@@ -37,6 +37,17 @@ export const Default = Template.bind({});
 Default.args = {
   icon: WizINotification,
 };
+Default.parameters = {
+  docs: {
+    source: {
+      code: `
+<template>
+  <WizIcon :icon="WizNotification" />
+</template>
+      `,
+    },
+  },
+};
 
 export const Size: StoryFn = () => ({
   components: { WizIcon },
@@ -53,6 +64,26 @@ export const Size: StoryFn = () => ({
     </div>
   `,
 });
+Size.parameters = {
+  docs: {
+    description: {
+      story:
+        "Icon のサイズを指定することができます。選択肢はVariablesのFontSizeを参照してください。",
+    },
+    source: {
+      code: `
+<template>
+  <div style="display: grid; grid-template-columns: max-content max-content; grid-gap: 1rem; align-items: center;">
+    <template v-for="size in FONT_SIZE_ACCESSORS">
+      <div>Size = {{ size }}</div>
+      <WizIcon :icon="WizINotification" :size="size" />
+    </template>
+  </div>
+</template>
+      `,
+    },
+  },
+};
 
 export const Color: StoryFn = () => ({
   components: { WizIcon },
@@ -69,6 +100,26 @@ export const Color: StoryFn = () => ({
     </div>
   `,
 });
+Color.parameters = {
+  docs: {
+    description: {
+      story:
+        "Icon の色を指定することができます。選択肢はVariablesのColorKeysを参照してください。",
+    },
+    source: {
+      code: `
+<template>
+  <div style="display: grid; grid-template-columns: max-content max-content; grid-gap: 1rem; align-items: center;">
+    <template v-for="color in COLOR_MAP_ACCESSORS">
+      <div>Color = {{ color }}</div>
+      <WizIcon :icon="WizINotification" :color="color" />
+    </template>
+  </div>
+</template>
+      `,
+    },
+  },
+};
 
 export const Icon: StoryFn = () => ({
   components: { WizIcon },
@@ -84,3 +135,22 @@ export const Icon: StoryFn = () => ({
     </div>
   `,
 });
+Icon.parameters = {
+  docs: {
+    description: {
+      story: "Icon は次のように指定します。",
+    },
+    source: {
+      code: `
+<template>
+  <div style="display: grid; grid-template-columns: max-content max-content; grid-gap: 1rem; align-items: center;">
+    <template v-for="icon in Object.values(Icons)">
+      <div>Icon = {{ icon.name }}</div>
+      <WizIcon :icon="icon" />
+    </template>
+  </div>
+</template>
+      `,
+    },
+  },
+};
