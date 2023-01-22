5a00c00e352afa19864e38e12b810a3fa6ccfe93
okakyo <0622okakyo@gmail.com>
2023-01-22 15:30:15 +0900

    feat: add checkbox input docs

--- packages/wiz-ui/src/components/base/inputs/checkbox/checkbox.stories.ts
+++ packages/wiz-ui/src/components/base/inputs/checkbox/checkbox.stories.ts
@@ -52,6 +52,31 @@ Default.args = {
     { label: "test4", value: 4 },
   ],
 };
+Default.parameters = {
+  docs: {
+    description: {
+      story: "入力を `disabled` にすることもできます。",
+    },
+    source: {
+      code: `
+<script lang="ts" setup>
+  const options = [
+    { label: "test1", value: 1 },
+    { label: "test2", value: 2 },
+    { label: "test3", value: 3 },
+    { label: "test4", value: 4 },
+  ]
+</script>
+<template>
+  <div>
+    <p>入力値：{{ value }}</p>
+    <WizCheckBox :options ="options" v-model="value" @input="args.input"/>
+  </div>
+</template>
+      `,
+    },
+  },
+};
 
 export const AllDisabled = Template.bind({});
 AllDisabled.args = {
@@ -63,6 +88,31 @@ AllDisabled.args = {
   ],
   disabled: true,
 };
+AllDisabled.parameters = {
+  docs: {
+    description: {
+      story: "入力を `disabled` にすることもできます。",
+    },
+    source: {
+      code: `
+<script lang="ts" setup>
+  const options = [
+    { label: "test1", value: 1 },
+    { label: "test2", value: 2 },
+    { label: "test3", value: 3 },
+    { label: "test4", value: 4 },
+  ]
+</script>
+<template>
+  <div>
+    <p>入力値：{{ value }}</p>
+    <WizCheckBox disabled :options ="options" v-model="value" @input="args.input"/>
+  </div>
+</template>
+      `,
+    },
+  },
+};
 
 export const SpotDisabled = Template.bind({});
 SpotDisabled.args = {
@@ -72,6 +122,31 @@ SpotDisabled.args = {
     { label: "test3", value: 3 },
     { label: "test4", value: 4, disabled: true },
   ],
+};
+SpotDisabled.parameters = {
+  docs: {
+    description: {
+      story: "選択肢の一部を disabled にすることもできます。",
+    },
+    source: {
+      code: `
+<script lang="ts" setup>
+  const options = [
+    { label: "test1", value: 1 },
+    { label: "test2", value: 2, disabled: true },
+    { label: "test3", value: 3 },
+    { label: "test4", value: 4, disabled: true },
+  ]
+</script>
+<template>
+  <div>
+    <p>入力値：{{ value }}</p>
+    <WizCheckBox :options ="options" v-model="value" @input="args.input"/>
+  </div>
+</template>
+      `,
+    },
+  },
 };
 
 export const Vertical = Template.bind({});
@@ -84,6 +159,32 @@ Vertical.args = {
   ],
   direction: "vertical",
 };
+Vertical.parameters = {
+  docs: {
+    description: {
+      story:
+        "選択肢を出す方向を変えることができます。 選択肢は `vertical`, `horizontal` から選択できます。 default は `horizontal` です。",
+    },
+    source: {
+      code: `
+<script lang="ts" setup>
+  const options = [
+    { label: "test1", value: 1 },
+    { label: "test2", value: 2 },
+    { label: "test3", value: 3 },
+    { label: "test4", value: 4 },
+  ]
+</script>
+<template>
+  <div>
+    <p>入力値：{{ value }}</p>
+    <WizCheckBox :options ="options" direction="vertical" v-model="value" @input="args.input"/>
+  </div>
+</template>
+      `,
+    },
+  },
+};
 
 export const Gap = Template.bind({});
 Gap.args = {
@@ -94,4 +195,30 @@ Gap.args = {
     { label: "test4", value: 4 },
   ],
   gap: "sm",
+};
+Gap.parameters = {
+  docs: {
+    description: {
+      story:
+        "選択肢に `gap` を指定して余白を設定することができます。選択肢はSpacingKeysの中から選択できます。default 値は `xl` です。",
+    },
+    source: {
+      code: `
+<script lang="ts" setup>
+  const options = [
+    { label: "test1", value: 1 },
+    { label: "test2", value: 2 },
+    { label: "test3", value: 3 },
+    { label: "test4", value: 4 },
+  ]
+</script>
+<template>
+  <div>
+    <p>入力値：{{ value }}</p>
+    <WizCheckBox  :options ="options" gap="sm" v-model="value" @input="args.input"/>
+  </div>
+</template>
+      `,
+    },
+  },
 };
