5613e9b72d035c38a04851332d32e366e6174307
okakyo <0622okakyo@gmail.com>
2023-01-28 01:31:21 +0900

    feat: add progress bar docs

--- packages/wiz-ui/src/components/base/progress-bar/progress-bar.stories.ts
+++ packages/wiz-ui/src/components/base/progress-bar/progress-bar.stories.ts
@@ -46,6 +46,29 @@ export const Default = Template.bind({});
 Default.args = {
   contents: baseContents,
 };
+Default.parameters = {
+  docs: {
+    source: {
+      code: `
+<script lang="ts" setup>
+const const baseContents = [
+  { status: "done" },
+  { status: "active" },
+  { status: "inactive" },
+  { status: "none" },
+  { status: "pending" },
+  { status: "dead" },
+];
+</script>
+<template>
+  <div style="padding: 100px;">
+    <WizProgressBar :contents="baseContents" />
+  </div>
+</template>
+      `,
+    },
+  },
+};
 
 export const WithValue = Template.bind({});
 WithValue.args = {
@@ -53,6 +76,32 @@ WithValue.args = {
     ...content,
     value: index + 1,
   })),
+};
+WithValue.parameters = {
+  docs: {
+    description: {
+      story: "`value` を指定することで Point 上に値を表示することができます。",
+    },
+    source: {
+      code: `
+<script lang="ts" setup>
+const const baseContents = [
+  { status: "done", value="1" },
+  { status: "active",value="2" },
+  { status: "inactive",value="3" },
+  { status: "none", value="4"},
+  { status: "pending", value="5"},
+  { status: "dead",value="6" },
+];
+</script>
+<template>
+  <div style="padding: 100px;">
+    <WizProgressBar :contents="baseContents" />
+  </div>
+</template>
+      `,
+    },
+  },
 };
 
 export const WithProgress = Template.bind({});
@@ -62,6 +111,33 @@ WithProgress.args = {
     progress: [1, 3, 4].includes(index),
   })),
 };
+WithProgress.parameters = {
+  docs: {
+    description: {
+      story:
+        "`progress` を指定することで、指定した Bar をアクティブ化することができます。",
+    },
+    source: {
+      code: `
+<script lang="ts" setup>
+const const baseContents = [
+  { status: "done", progres=true },
+  { status: "active",  },
+  { status: "inactive", progress=true },
+  { status: "none", progress=true},
+  { status: "pending" },
+  { status: "dead" },
+];
+</script>
+<template>
+  <div style="padding: 100px;">
+    <WizProgressBar :contents="baseContents" />
+  </div>
+</template>
+      `,
+    },
+  },
+};
 
 export const WithTooltip = Template.bind({});
 WithTooltip.args = {
@@ -69,6 +145,33 @@ WithTooltip.args = {
     ...content,
     tooltip: `Tooltip ${index + 1}`,
   })),
+};
+WithTooltip.parameters = {
+  docs: {
+    description: {
+      story:
+        "`tooltip` を指定することで、ホバー時に ポップアップ を表示することができます。",
+    },
+    source: {
+      code: `
+<script lang="ts" setup>
+const const baseContents = [
+  { status: "done", tooltip="Tooltip 1" },
+  { status: "active", tooltip="Tooltip 2" },
+  { status: "inactive", tooltip="Tooltip 3" },
+  { status: "none", tooltip="Tooltip 4" },
+  { status: "pending", tooltip="Tooltip 5"},
+  { status: "dead", tooltip="Tooltip 6" },
+];
+</script>
+<template>
+  <div style="padding: 100px;">
+    <WizProgressBar :contents="baseContents" />
+  </div>
+</template>
+      `,
+    },
+  },
 };
 
 export const WithLabel = Template.bind({});
@@ -78,6 +181,33 @@ WithLabel.args = {
     label: `Label ${index + 1}`,
   })),
 };
+WithLabel.parameters = {
+  docs: {
+    description: {
+      story:
+        "`label` を指定することで、Progress の下側に ラベル を表示することができます。",
+    },
+    source: {
+      code: `
+<script lang="ts" setup>
+const const baseContents = [
+  { status: "done", label="Label 1" },
+  { status: "active", label="Label 2" },
+  { status: "inactive", label="Label 3" },
+  { status: "none", label="Label 4" },
+  { status: "pending", label="Label 5"},
+  { status: "dead", label="Label 6" },
+];
+</script>
+<template>
+  <div style="padding: 100px;">
+    <WizProgressBar :contents="baseContents" />
+  </div>
+</template>
+      `,
+    },
+  },
+};
 
 export const WithAnnotation = Template.bind({});
 WithAnnotation.args = {
@@ -85,6 +215,33 @@ WithAnnotation.args = {
     ...content,
     annotation: `Annotation ${index + 1}`,
   })),
+};
+WithAnnotation.parameters = {
+  docs: {
+    description: {
+      story:
+        "`annnotation` を指定することで、Progress の上側に ラベル を表示することができます。`label` と異なり、赤文字で強調された文字が表示されます。 ",
+    },
+    source: {
+      code: `
+<script lang="ts" setup>
+const const baseContents = [
+  { status: "done", annotation="Annotation 1" },
+  { status: "active", annotation="Annotation 2" },
+  { status: "inactive", annotation="Annotation 3" },
+  { status: "none", annotation="Annotation 4"},
+  { status: "pending", annotation="Annotation 5"},
+  { status: "dead", annotation="Annotation 6" },
+];
+</script>
+<template>
+  <div style="padding: 100px;">
+    <WizProgressBar :contents="baseContents" />
+  </div>
+</template>
+      `,
+    },
+  },
 };
 
 export const WithAll = Template.bind({});
@@ -97,4 +254,27 @@ WithAll.args = {
     label: `Label ${index + 1}`,
     annotation: `Annotation ${index + 1}`,
   })),
+};
+WithAll.parameters = {
+  docs: {
+    source: {
+      code: `
+<script lang="ts" setup>
+const const baseContents = [
+  { status: "done", value="1",progress=true, tooltip="Tooltip 1", label="Label 1", annotation="Annotation 1" },
+  { status: "active",value="2", tooltip="Tooltip 2", label="Label 2", annotation="Annotation 2" },
+  { status: "inactive",value="3",progress=true, tooltip="Tooltip 3", label="Label 3", annotation="Annotation 3" },
+  { status: "none", value="4",progress=true, tooltip="Tooltip 4", label="Label 4", annotation="Annotation 4"},
+  { status: "pending", value="5", tooltip="Tooltip 5", label="Label 5", annotation="Annotation 5"},
+  { status: "dead",value="6", tooltip="Tooltip 6", label="Label 6", annotation="Annotation 6" },
+];
+</script>
+<template>
+  <div style="padding: 100px;">
+    <WizProgressBar :contents="baseContents" />
+  </div>
+</template>
+      `,
+    },
+  },
 };
