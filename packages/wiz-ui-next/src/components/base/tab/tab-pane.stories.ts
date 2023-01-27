0ec642785e483a008b2950422bf985e928dc704b
okakyo <0622okakyo@gmail.com>
2023-01-28 02:21:16 +0900

    feat: add tab pane docs

--- packages/wiz-ui-next/src/components/base/tab/tab-pane.stories.ts
+++ packages/wiz-ui-next/src/components/base/tab/tab-pane.stories.ts
@@ -39,11 +39,37 @@ export const Default = Template.bind({});
 Default.args = {
   label: "ほげほげ",
 };
+Default.parameters = {
+  docs: {
+    source: {
+      code: `
+<template>
+  <TabPane label="ほげほげ" @click="click" />
+</template>
+      `,
+    },
+  },
+};
 
 export const Active = Template.bind({});
 Active.args = {
   label: "ほげほげ",
   active: true,
+};
+Active.parameters = {
+  docs: {
+    description: {
+      story:
+        "Active を設定することで、常にアクティブ化することもできます。 default 値は `false` です。",
+    },
+    source: {
+      code: `
+<template>
+  <TabPane label="ほげほげ" active @click="click" />
+</template>
+      `,
+    },
+  },
 };
 
 export const Disabled = Template.bind({});
@@ -51,15 +77,58 @@ Disabled.args = {
   label: "ほげほげ",
   disabled: true,
 };
+Disabled.parameters = {
+  docs: {
+    description: {
+      story: "Disabled に指定することができます。",
+    },
+    source: {
+      code: `
+<template>
+  <TabPane label="ほげほげ" disabled @click="click" />
+</template>
+      `,
+    },
+  },
+};
 
 export const NotificationCount = Template.bind({});
 NotificationCount.args = {
   label: "ほげほげ",
   notificationCount: 10,
 };
+NotificationCount.parameters = {
+  docs: {
+    description: {
+      story:
+        "`notificationCount` を指定することで、Tab に Notification を付与することができます。",
+    },
+    source: {
+      code: `
+<template>
+  <TabPane label="ほげほげ" notificationCount="10" @click="click" />
+</template>
+      `,
+    },
+  },
+};
 
 export const Width = Template.bind({});
 Width.args = {
   label: "ほげほげ",
   width: "100px",
+};
+Width.parameters = {
+  docs: {
+    description: {
+      story: "`width` を指定することで、幅を指定することができます。",
+    },
+    source: {
+      code: `
+<template>
+  <TabPane label="ほげほげ" width="100px" @click="click" />
+</template>
+      `,
+    },
+  },
 };
