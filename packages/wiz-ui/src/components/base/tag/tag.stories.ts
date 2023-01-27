b8c66bead2fbf2f3da004f9214c430d9a3df7b73
okakyo <0622okakyo@gmail.com>
2023-01-28 03:15:01 +0900

    feat: add tag docs

--- packages/wiz-ui/src/components/base/tag/tag.stories.ts
+++ packages/wiz-ui/src/components/base/tag/tag.stories.ts
@@ -40,11 +40,36 @@ export const Default = Template.bind({});
 Default.args = {
   label: "タグ",
 };
+Default.parameters = {
+  docs: {
+    source: {
+      code: `
+<template>
+  <WizTag label="タグ" />
+</template>
+      `,
+    },
+  },
+};
 
 export const Bold = Template.bind({});
 Bold.args = {
   label: "タグ",
   fontWeight: "bold",
+};
+Bold.parameters = {
+  docs: {
+    description: {
+      story: "Bold を指定することで、文字を強調することができます。",
+    },
+    source: {
+      code: `
+<template>
+  <WizTag label="タグ" fontWeight="bold" />
+</template>
+      `,
+    },
+  },
 };
 
 export const WithIcon = Template.bind({});
@@ -52,11 +77,40 @@ WithIcon.args = {
   label: "タグ",
   icon: WizIClose,
 };
+WithIcon.parameters = {
+  docs: {
+    description: {
+      story:
+        "アイコンを指定することができます。`WizI`というPrefixのコンポーネントがアイコンなのでそれを渡してください。選択肢はIconsを参照してください。",
+    },
+    source: {
+      code: `
+<template>
+  <WizTag label="タグ" :icon="WizIClose" />
+</template>
+      `,
+    },
+  },
+};
 
 export const Width = Template.bind({});
 Width.args = {
   label: "タグ",
   width: "100px",
+};
+Width.parameters = {
+  docs: {
+    description: {
+      story: "`width` を指定することで、幅を指定することができます。",
+    },
+    source: {
+      code: `
+<template>
+  <WizTag label="タグ" width="100px" />
+</template>
+      `,
+    },
+  },
 };
 
 export const Variant: StoryFn = (_, { argTypes }) => ({
@@ -76,3 +130,27 @@ export const Variant: StoryFn = (_, { argTypes }) => ({
     </div>
   `,
 });
+Variant.parameters = {
+  docs: {
+    description: {
+      story:
+        "`variant` により、IconButton の色を決められたテーマ色に変更することができます。`info`, `error`, `success`, `mono`, から選択できます。 default は`info` です。",
+    },
+  },
+  source: {
+    code: `
+<template>
+  <div style="display: grid; grid-template-columns: max-content max-content; grid-gap: 1rem; align-items: center;">
+    <WizTag label="必須" variant="info" :icon="WizIClose" />
+    <div>variant="info"</div>
+    <WizTag label="自動失注" variant="error" :icon="WizIClose" />
+    <div>variant="error"</div>
+    <WizTag label="申請完了" variant="success" :icon="WizIClose" />
+    <div>variant="success"</div>
+    <WizTag label="11/11(月)" variant="mono" :icon="WizIClose" />
+    <div>variant="mono"</div>
+  </div>
+</template>
+      `,
+  },
+};
