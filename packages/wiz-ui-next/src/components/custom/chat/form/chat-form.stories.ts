222b0466a706a5bb71413940dfe920666c6def5b
okakyo <0622okakyo@gmail.com>
2023-01-31 22:10:25 +0900

    feat: add chat form doc

--- packages/wiz-ui-next/src/components/custom/chat/form/chat-form.stories.ts
+++ packages/wiz-ui-next/src/components/custom/chat/form/chat-form.stories.ts
@@ -42,13 +42,46 @@ const Template: StoryFn<typeof WizChatForm> = (args) => ({
 
 export const Default = Template.bind({});
 Default.args = {};
+Default.parameters = {
+  docs: {
+    source: {
+      code: `
+<template>
+  <WizChatForm v-model="value" @update:modelValue="args.input" @submit="args.submit" />
+</template>
+      `,
+    },
+  },
+};
 
 export const Placeholder = Template.bind({});
 Placeholder.args = {
   placeholder: "入力してください",
 };
+Placeholder.parameters = {
+  docs: {
+    source: {
+      code: `
+<template>
+  <WizChatForm v-model="value" placeholder="入力してください" @update:modelValue="args.input" @submit="args.submit" />
+</template>
+      `,
+    },
+  },
+};
 
 export const Disabled = Template.bind({});
 Disabled.args = {
   disabled: true,
+};
+Disabled.parameters = {
+  docs: {
+    source: {
+      code: `
+<template>
+  <WizChatForm v-model="value" disabled @update:modelValue="args.input" @submit="args.submit" />
+</template>
+      `,
+    },
+  },
 };
