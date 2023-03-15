c3e82f9a4bb46ea2c560cd47560900f146613183
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-03-15 14:58:54 +0900

    docs(accordion): storybookを更新

--- packages/wiz-ui-next/src/components/base/accordion/accordion.stories.ts
+++ packages/wiz-ui-next/src/components/base/accordion/accordion.stories.ts
@@ -5,14 +5,56 @@ import WizAccordion from "./accordion.vue";
 export default {
   title: "Base/Accordion",
   component: WizAccordion,
-  argTypes: {},
+  argTypes: {
+    backgroundColor: {
+      control: { type: "select" },
+      options: ["white", "gray"],
+    },
+  },
 } as Meta<typeof WizAccordion>;
 
 const Template: StoryFn<typeof WizAccordion> = (args) => ({
   setup: () => ({ args }),
   components: { WizAccordion },
-  template: `<WizAccordion v-bind="args">折りたたまれている部分です．</WizAccordion>`,
+  template: `
+  <WizAccordion v-bind="args"> 
+    <p>折りたたまれている部分です．</p>
+    <p>折りたたまれている部分です．</p>
+    <p>折りたたまれている部分です．</p>
+  </WizAccordion>
+  `,
 });
 
 export const Default = Template.bind({});
-Default.args = {};
+Default.args = {
+  backgroundColor: "gray",
+};
+
+const MultipleTemplate: StoryFn<typeof WizAccordion> = (args) => ({
+  setup: () => ({ args }),
+  components: { WizAccordion },
+  template: `
+  <WizAccordion v-bind="args"> 
+    <p>折りたたまれている部分です．</p>
+    <p>折りたたまれている部分です．</p>
+    <p>折りたたまれている部分です．</p>
+  </WizAccordion>
+  
+  <WizAccordion v-bind="args"> 
+    <p>折りたたまれている部分です．</p>
+    <p>折りたたまれている部分です．</p>
+    <p>折りたたまれている部分です．</p>
+  </WizAccordion>
+  
+  <WizAccordion v-bind="args"> 
+    <p>折りたたまれている部分です．</p>
+    <p>折りたたまれている部分です．</p>
+    <p>折りたたまれている部分です．</p>
+  </WizAccordion>
+  `,
+});
+
+export const Multiple = MultipleTemplate.bind({});
+Multiple.args = {
+  backgroundColor: "gray",
+};
--- packages/wiz-ui/src/components/base/accordion/accordion.stories.ts
+++ packages/wiz-ui/src/components/base/accordion/accordion.stories.ts
@@ -5,15 +5,58 @@ import WizAccordion from "./accordion.vue";
 export default {
   title: "Base/Accordion",
   component: WizAccordion,
-  argTypes: {},
+  argTypes: {
+    backgroundColor: {
+      control: { type: "select" },
+      options: ["white", "gray"],
+    },
+  },
 } as Meta<typeof WizAccordion>;
 
 const Template: StoryFn<typeof WizAccordion> = (_, { argTypes }) => ({
   props: Object.keys(argTypes),
   components: { WizAccordion },
   setup: () => ({}),
-  template: `<WizAccordion v-bind="$props">折りたたまれている部分です．</WizAccordion>`,
+  template: `
+  <WizAccordion v-bind="$props">
+    <p>折りたたまれている部分です．</p>
+    <p>折りたたまれている部分です．</p>
+    <p>折りたたまれている部分です．</p>
+  </WizAccordion>
+  `,
 });
 
 export const Default = Template.bind({});
-Default.args = {};
+Default.args = {
+  backgroundColor: "gray",
+};
+
+const MultipleTemplate: StoryFn<typeof WizAccordion> = (_, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { WizAccordion },
+  setup: () => ({}),
+  template: `
+  <div>
+  <WizAccordion v-bind="$props">
+    <p>折りたたまれている部分です．</p>
+    <p>折りたたまれている部分です．</p>
+    <p>折りたたまれている部分です．</p>
+  </WizAccordion>
+  <WizAccordion v-bind="$props">
+    <p>折りたたまれている部分です．</p>
+    <p>折りたたまれている部分です．</p>
+    <p>折りたたまれている部分です．</p>
+  </WizAccordion>
+  <WizAccordion v-bind="$props">
+    <p>折りたたまれている部分です．</p>
+    <p>折りたたまれている部分です．</p>
+    <p>折りたたまれている部分です．</p>
+  </WizAccordion>
+</div>
+  `,
+});
+
+export const Multiple = MultipleTemplate.bind({});
+Multiple.args = {
+  backgroundColor: "gray",
+};
