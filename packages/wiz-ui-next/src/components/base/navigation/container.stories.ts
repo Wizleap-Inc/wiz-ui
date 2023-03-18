e8420e5fad35299352e68568fe724eb049d830f2
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-03-18 22:46:14 +0900

    docs(nav-container): popup表示時のパターンを追加

--- packages/wiz-ui-next/src/components/base/navigation/container.stories.ts
+++ packages/wiz-ui-next/src/components/base/navigation/container.stories.ts
@@ -267,3 +267,85 @@ export const PopupSticky: StoryFn = (_, { argTypes }) => ({
   </div>
   `,
 });
+
+export const Disabled: StoryFn = (_, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { WizNavContainer, WizNavItem },
+  setup() {
+    const lockingPopup = ref(false);
+    const setLock = (isLock: boolean) => {
+      lockingPopup.value = isLock;
+    };
+    const createButton = (n: number): ButtonGroupItem => ({
+      kind: "button",
+      option: {
+        label: `label ${n}`,
+        value: n,
+        onClick: () => {
+          console.log(`clicked ${n}`);
+        },
+      },
+    });
+
+    return {
+      WizIDashboard,
+      WizIAssignment,
+      WizIBusinessCenter,
+      WizIHelp,
+      lockingPopup,
+      setLock,
+      createButton,
+    };
+  },
+  template: `
+  <div style="display: flex; height: 100vh;">
+    <WizNavContainer sticky>
+      <WizNavItem :disabled="true"  :lockingPopup="lockingPopup" @setLock="setLock" :buttons="[createButton(0)]" :icon="WizIDashboard" label="Home" to="/" :active="false" />
+      <WizNavItem :disabled="true"  :lockingPopup="lockingPopup" @setLock="setLock" :buttons="[createButton(1), createButton(2)]" :icon="WizIAssignment" label="Page1" to="/page1" :active="false" />
+      <WizNavItem :disabled="true"  :lockingPopup="lockingPopup" @setLock="setLock" :buttons="[createButton(3), createButton(4)]" :icon="WizIBusinessCenter" label="Page2" to="/page2" :active="false" />
+      <WizNavItem :disabled="true"  :lockingPopup="lockingPopup" @setLock="setLock" :buttons="[createButton(5), createButton(6)]" :icon="WizIHelp" label="Page3" to="/page3" :active="false" />
+    </WizNavContainer>
+  </div>
+  `,
+});
+
+export const PopupAndTooltip: StoryFn = (_, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { WizNavContainer, WizNavItem },
+  setup() {
+    const lockingPopup = ref(false);
+    const setLock = (isLock: boolean) => {
+      lockingPopup.value = isLock;
+    };
+    const createButton = (n: number): ButtonGroupItem => ({
+      kind: "button",
+      option: {
+        label: `label ${n}`,
+        value: n,
+        onClick: () => {
+          console.log(`clicked ${n}`);
+        },
+      },
+    });
+
+    return {
+      WizIDashboard,
+      WizIAssignment,
+      WizIBusinessCenter,
+      WizIHelp,
+      lockingPopup,
+      setLock,
+      createButton,
+    };
+  },
+  template: `
+  <div style="display: flex; height: 100vh;">
+    <WizNavContainer sticky>
+      <WizNavItem tooltipText="これはヒント1です。"  :lockingPopup="lockingPopup" @setLock="setLock" :buttons="[createButton(0)]" :icon="WizIDashboard" label="Home" to="/" :active="false" />
+      <WizNavItem tooltipText="これはヒント2です。"  :lockingPopup="lockingPopup" @setLock="setLock" :buttons="[createButton(1), createButton(2)]" :icon="WizIAssignment" label="Page1" to="/page1" :active="false" />
+      <WizNavItem tooltipText="これはヒント3です。"  :lockingPopup="lockingPopup" @setLock="setLock" :buttons="[createButton(3), createButton(4)]" :icon="WizIBusinessCenter" label="Page2" to="/page2" :active="false" />
+      <WizNavItem tooltipText="これはヒント4です。"  :lockingPopup="lockingPopup" @setLock="setLock" :buttons="[createButton(5), createButton(6)]" :icon="WizIHelp" label="Page3" to="/page3" :active="false" />
+    </WizNavContainer>
+  </div>
+  `,
+});
