994eba7d9fa4ca15e3ba6cc22004ad9720d71d4f
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-03-19 19:14:31 +0900

    feat(accordion): paddingオプションを追加

--- packages/wiz-ui/src/components/base/accordion/accordion.vue
+++ packages/wiz-ui/src/components/base/accordion/accordion.vue
@@ -1,10 +1,12 @@
 <template>
   <WizVStack :width="width" :class="AccordionDetailsStyle[backgroundColor]">
-    <slot
-      ref="contentRef"
-      :class="AccordionContentStyle"
+    <div
       v-if="isOpen && !expandDown"
-    />
+      ref="contentRef"
+      :class="[AccordionContentStyle, ...computedPadding]"
+    >
+      <slot />
+    </div>
     <div :class="AccordionSummaryStyle" @click="onClick">
       <WizHStack
         align="center"
@@ -31,16 +33,18 @@
         />
       </WizHStack>
     </div>
-    <slot
-      ref="contentRef"
-      :class="AccordionContentStyle"
+    <div
       v-if="isOpen && expandDown"
-    />
+      ref="contentRef"
+      :class="[AccordionContentStyle, ...computedPadding]"
+    >
+      <slot />
+    </div>
   </WizVStack>
 </template>
 
 <script setup lang="ts">
-import { ColorKeys } from "@wizleap-inc/wiz-ui-constants";
+import { ColorKeys, SpacingKeys } from "@wizleap-inc/wiz-ui-constants";
 import {
   AccordionMessageVariantStyle,
   AccordionDetailsStyle,
@@ -49,6 +53,15 @@ import {
   openSpin,
   closeSpin,
 } from "@wizleap-inc/wiz-ui-styles/bases/accordion.css";
+import {
+  paddingStyle,
+  paddingXStyle,
+  paddingYStyle,
+  paddingTopStyle,
+  paddingRightStyle,
+  paddingBottomStyle,
+  paddingLeftStyle,
+} from "@wizleap-inc/wiz-ui-styles/commons";
 import { ref, computed, PropType } from "vue";
 
 import { WizHStack, WizIcon, WizVStack } from "@/components";
@@ -84,6 +97,13 @@ const props = defineProps({
     required: false,
     default: true,
   },
+  p: { type: String as PropType<SpacingKeys>, required: false },
+  pt: { type: String as PropType<SpacingKeys>, required: false },
+  pr: { type: String as PropType<SpacingKeys>, required: false },
+  pb: { type: String as PropType<SpacingKeys>, required: false },
+  pl: { type: String as PropType<SpacingKeys>, required: false },
+  px: { type: String as PropType<SpacingKeys>, required: false },
+  py: { type: String as PropType<SpacingKeys>, required: false },
 });
 
 interface Emit {
@@ -104,4 +124,14 @@ const onClick = (event: MouseEvent) => {
   emit("toggle");
   event.preventDefault();
 };
+
+const computedPadding = computed(() => [
+  props.px && paddingXStyle[props.px],
+  props.py && paddingYStyle[props.py],
+  !props.px && !props.py && props.p && paddingStyle[props.p],
+  props.pt && paddingTopStyle[props.pt],
+  props.pr && paddingRightStyle[props.pr],
+  props.pb && paddingBottomStyle[props.pb],
+  props.pl && paddingLeftStyle[props.pl],
+]);
 </script>
