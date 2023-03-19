fe7fdc90b638f482c17fa5d670a9a5a6daa62bb7
ichi-h <85932406+ichi-h@users.noreply.github.com>
2023-03-20 07:39:47 +0900

    refactor(accordion): 不要なアニメーションのプロパティを削除

--- packages/wiz-ui/src/components/base/accordion/animation-configuration.ts
+++ packages/wiz-ui/src/components/base/accordion/animation-configuration.ts
@@ -6,21 +6,17 @@ export const ANIMATION_CONFIGURATION = {
 export const closingAnimationKeyframes = (content: HTMLElement) => [
   {
     height: content.offsetHeight + "px",
-    opacity: 1,
   },
   {
     height: 0,
-    opacity: 1,
   },
 ];
 
 export const openingAnimationKeyframes = (content: HTMLElement) => [
   {
     height: 0,
-    opacity: 1,
   },
   {
     height: content.offsetHeight + "px",
-    opacity: 1,
   },
 ];
