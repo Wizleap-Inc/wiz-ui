b858e814f939ed4199f9ab5ae69727eaeed56e24
ichi-h <85932406+ichi-h@users.noreply.github.com>
2023-03-23 17:02:43 +0900

    docs(popup): 中間表示対応にともなうstoryの変更
    - Playground2の追加（1画面で全方向を確認できるstoryを追加）

--- packages/wiz-ui-next/src/components/base/popup/popup.stories.ts
+++ packages/wiz-ui-next/src/components/base/popup/popup.stories.ts
@@ -25,7 +25,20 @@ export default {
     },
     direction: {
       control: { type: "select" },
-      options: ["tl", "tr", "bl", "br", "lt", "lb", "rt", "rb"],
+      options: [
+        "tl",
+        "tr",
+        "tc",
+        "bl",
+        "br",
+        "bc",
+        "rt",
+        "rb",
+        "rc",
+        "lt",
+        "lb",
+        "lc",
+      ],
     },
     closeOnBlur: {
       control: { type: "boolean" },
@@ -54,12 +67,16 @@ export const Direction: StoryFn<typeof WizPopup> = (args) => ({
       main: "direction",
       patterns: [
         { direction: "bl" },
+        { direction: "bc" },
         { direction: "br" },
         { direction: "tl" },
+        { direction: "tc" },
         { direction: "tr" },
         { direction: "lt" },
+        { direction: "lc" },
         { direction: "lb" },
         { direction: "rt" },
+        { direction: "rc" },
         { direction: "rb" },
       ],
     };
@@ -158,6 +175,95 @@ export const Playground: StoryFn<typeof WizPopup> = (args) => ({
     </div>
     <input type="range" min="0" :max="''+maxX" v-model="x" />
     <input type="range" min="0" :max="''+maxY" v-model="y" />
+  </div>
+  `,
+});
+
+export const Playground2: StoryFn<typeof WizPopup> = (args) => ({
+  components: { WizPopup, WizPopupContainer, WizTextButton, WizHStack },
+  setup() {
+    const isOpen = ref(true);
+    const toggle = () => (isOpen.value = !isOpen.value);
+    const close = () => (isOpen.value = false);
+    const absolutePositions = [
+      {
+        top: "0px",
+        left: "0px",
+        bottom: "initial",
+        right: "initial",
+        transform: "initial",
+      },
+      {
+        top: "0px",
+        left: "initial",
+        bottom: "initial",
+        right: "0px",
+        transform: "initial",
+      },
+      {
+        top: "initial",
+        left: "0px",
+        bottom: "0px",
+        right: "initial",
+        transform: "initial",
+      },
+      {
+        top: "initial",
+        left: "initial",
+        bottom: "0px",
+        right: "0px",
+        transform: "initial",
+      },
+      {
+        top: "50%",
+        left: "50%",
+        bottom: "initial",
+        right: "initial",
+        transform: "translate(-50%, -50%)",
+      },
+      {
+        top: "50%",
+        left: "0px",
+        bottom: "initial",
+        right: "initial",
+        transform: "translate(0%, -50%)",
+      },
+      {
+        top: "50%",
+        left: "initial",
+        bottom: "initial",
+        right: "0px",
+        transform: "translate(0%, -50%)",
+      },
+      {
+        top: "0px",
+        left: "50%",
+        bottom: "initial",
+        right: "initial",
+        transform: "translate(-50%, 0%)",
+      },
+      {
+        top: "initial",
+        left: "50%",
+        bottom: "0px",
+        right: "initial",
+        transform: "translate(-50%, 0%)",
+      },
+    ];
+    return { isOpen, toggle, close, absolutePositions, args };
+  },
+  template: `
+  <div style="height: 100vh; width: 100vw; position: relative; overflow">
+    <div v-for="(pos, key) in absolutePositions" :key="key" :style="{ position: 'absolute', ...pos }">
+      <wiz-popup-container>
+        <wiz-text-button @click="toggle">Toggle</wiz-text-button>
+        <wiz-popup :direction="args.direction" :gap="args.gap" :closeOnBlur="args.closeOnBlur" :isOpen="isOpen" @onClose="close">
+          <div style="padding: 16px; background-color: white; border-radius: 4px;">
+            <span>Popup content</span>
+          </div>
+        </wiz-popup>
+      </wiz-popup-container>
+    </div>
   </div>
   `,
 });
--- packages/wiz-ui/src/components/base/popup/popup.stories.ts
+++ packages/wiz-ui/src/components/base/popup/popup.stories.ts
@@ -25,7 +25,20 @@ export default {
     },
     direction: {
       control: { type: "select" },
-      options: ["tl", "tr", "bl", "br", "lt", "lb", "rt", "rb"],
+      options: [
+        "tl",
+        "tr",
+        "tc",
+        "bl",
+        "br",
+        "bc",
+        "rt",
+        "rb",
+        "rc",
+        "lt",
+        "lb",
+        "lc",
+      ],
     },
     closeOnBlur: {
       control: { type: "boolean" },
@@ -110,12 +123,16 @@ Direction.args = {
   main: "direction",
   patterns: [
     { direction: "bl" },
+    { direction: "bc" },
     { direction: "br" },
     { direction: "tl" },
+    { direction: "tc" },
     { direction: "tr" },
     { direction: "lt" },
+    { direction: "lc" },
     { direction: "lb" },
     { direction: "rt" },
+    { direction: "rc" },
     { direction: "rb" },
   ],
 };
@@ -139,36 +156,98 @@ Gap.args = {
 
 export const Playground = Template.bind({});
 
-export const MultiplePlayground: StoryFn = (_, { argTypes }) => ({
-  components: { WizPopup, WizTextButton, WizPopupContainer },
+export const Playground2: StoryFn<typeof WizPopup> = (_, { argTypes }) => ({
   props: Object.keys(argTypes),
+  components: { WizPopup, WizPopupContainer, WizTextButton, WizHStack },
   setup() {
-    const isOpen1 = ref(false);
-    const isOpen2 = ref(false);
-    const toggle1 = () => {
-      isOpen1.value = !isOpen1.value;
-    };
-    const toggle2 = () => (isOpen2.value = !isOpen2.value);
-    return { isOpen1, isOpen2, toggle1, toggle2 };
+    const isOpen = ref(true);
+    const toggle = () => (isOpen.value = !isOpen.value);
+    const close = () => (isOpen.value = false);
+    const absolutePositions = [
+      {
+        top: "0px",
+        left: "0px",
+        bottom: "initial",
+        right: "initial",
+        transform: "initial",
+      },
+      {
+        top: "0px",
+        left: "initial",
+        bottom: "initial",
+        right: "0px",
+        transform: "initial",
+      },
+      {
+        top: "initial",
+        left: "0px",
+        bottom: "0px",
+        right: "initial",
+        transform: "initial",
+      },
+      {
+        top: "initial",
+        left: "initial",
+        bottom: "0px",
+        right: "0px",
+        transform: "initial",
+      },
+      {
+        top: "50%",
+        left: "50%",
+        bottom: "initial",
+        right: "initial",
+        transform: "translate(-50%, -50%)",
+      },
+      {
+        top: "50%",
+        left: "0px",
+        bottom: "initial",
+        right: "initial",
+        transform: "translate(0%, -50%)",
+      },
+      {
+        top: "50%",
+        left: "initial",
+        bottom: "initial",
+        right: "0px",
+        transform: "translate(0%, -50%)",
+      },
+      {
+        top: "0px",
+        left: "50%",
+        bottom: "initial",
+        right: "initial",
+        transform: "translate(-50%, 0%)",
+      },
+      {
+        top: "initial",
+        left: "50%",
+        bottom: "0px",
+        right: "initial",
+        transform: "translate(-50%, 0%)",
+      },
+    ];
+    return { isOpen, toggle, close, absolutePositions };
   },
   template: `
-    <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; gap: 10rem;">
+  <div style="height: 100vh; width: 100vw; position: relative; overflow">
+    <div v-for="(pos, key) in absolutePositions" :key="key" :style="{ position: 'absolute', ...pos }">
       <wiz-popup-container>
-        <wiz-text-button @click="toggle1">Toggle Popup 1</wiz-text-button>
-        <wiz-popup :isOpen="isOpen1" @onClose="isOpen1 = false">
+        <wiz-text-button @click="toggle">Toggle</wiz-text-button>
+        <wiz-popup :direction="$props.direction" :gap="$props.gap" :closeOnBlur="$props.closeOnBlur" :isOpen="isOpen" @onClose="close">
           <div style="padding: 16px; background-color: white; border-radius: 4px;">
-            <p>This is a popup content 1</p>
+            <span>Popup content</span>
           </div>
         </wiz-popup>
       </wiz-popup-container>
-      <wiz-popup-container>
-        <wiz-text-button @click="toggle2">Toggle Popup 2</wiz-text-button>
-        <wiz-popup :isOpen="isOpen2" @onClose="isOpen2 = false">
-          <div style="padding: 16px; background-color: white; border-radius: 4px;">
-            <p>This is a popup content 2</p>
+          </div>
           </div>
         </wiz-popup>
       </wiz-popup-container>
     </div>
+        </wiz-popup>
+      </wiz-popup-container>
+  </div>
   `,
 });
