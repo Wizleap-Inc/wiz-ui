e5ffc06021644daa64a8f04a063929958c036a98
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-03-27 19:41:25 +0900

    refactor(icons): インデント修正等

--- packages/icons/scripts/build.js
+++ packages/icons/scripts/build.js
@@ -20,21 +20,22 @@ const getSVGFiles = (dir) =>
       svg: fs.readFileSync(path.join(dir, file), "utf-8"),
     }));
 
-// Vueコンポーネントを生成
-const createIconVueFile = (svg, component) => `
-<template>
-${svg}
-</template>
-
-<script setup lang="ts">
-import { ComponentName } from "@wizleap-inc/wiz-ui-constants";
-
-defineOptions({
-  name: ComponentName.I${component},
-});
-</script>
-`;
-
+// Iconコンポーネントをフレームワークにあわせて生成
+const createIcon = {
+  vue: (svg, component) => `
+    <template>
+    ${svg}
+    </template>
+    
+    <script setup lang="ts">
+    import { ComponentName } from "@wizleap-inc/wiz-ui-constants";
+    
+    defineOptions({
+      name: ComponentName.I${component},
+    });
+    </script>
+  `,
+};
 const SVG_DIR = path.join(__dirname, "../assets/");
 const CONSTANTS_DIR = path.join(__dirname, "../../constants/");
 const ICON_DIRS = [
@@ -47,17 +48,17 @@ const components = getSVGFiles(SVG_DIR).map((file) => {
   return {
     fileName: file.fileName,
     name: name,
-    vueFile: createIconVueFile(file.svg, name),
+    vueFile: createIcon.vue(file.svg, name),
   };
 });
 
 // constants/component/icon-name.tsを生成する
 const iconComponentName = `
-export const IconComponentName = {
-  ${components
-    .map((component) => `I${component.name}: "WizI${component.name}"`)
-    .join(",")}
-}`;
+  export const IconComponentName = {
+    ${components
+      .map((component) => `I${component.name}: "WizI${component.name}"`)
+      .join(",")}
+  }`;
 fs.writeFileSync(
   path.join(CONSTANTS_DIR, "component/icon-name.ts"),
   iconComponentName
@@ -72,12 +73,12 @@ const indexFile = `
     )
     .join("")}
 
-export type TIcon = 
-  ${components.map((component) => `| typeof WizI${component.name}`).join("")};
+  export type TIcon = 
+    ${components.map((component) => `| typeof WizI${component.name}`).join("")};
 
-export {
-  ${components.map((component) => `WizI${component.name}`).join(",")}
-};`;
+  export {
+    ${components.map((component) => `WizI${component.name}`).join(",")}
+  };`;
 
 ICON_DIRS.forEach((dir) => {
   rimraf.sync(`${dir}/**/*.vue`); // src/icons/*.vueをクリアする
