481b3c3fd33959a22967f632c9149a132aa4c1fa
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-03-28 13:49:13 +0900

    docs(icon-button): aria-labelを付与

--- packages/wiz-ui-next/src/components/base/buttons/icon/icon.stories.ts
+++ packages/wiz-ui-next/src/components/base/buttons/icon/icon.stories.ts
@@ -22,22 +22,25 @@ export const Default: StoryFn<typeof WizIconButton> = (args) => ({
   components: { WizIconButton },
   setup: () => ({ args, WizISend }),
   template: `
-    <div><WizIconButton :icon="WizISend" @click="args.onClick('Default')" /></div>
+    <div><WizIconButton :label="args.label" :icon="WizISend" @click="args.onClick('Default')" /></div>
   `,
 });
+Default.args = {
+  label: "send",
+};
 
 export const Variant: StoryFn<typeof WizIconButton> = (args) => ({
   components: { WizIconButton },
   setup: () => ({ args, WizISend }),
   template: `
     <div style="display: grid; grid-template-columns: max-content max-content; grid-gap: 1rem; align-items: center;">
-      <div><WizIconButton variant="primary" :icon="WizISend" @click="args.onClick('primary')" /></div>
+      <div><WizIconButton label="primary" variant="primary" :icon="WizISend" @click="args.onClick('primary')" /></div>
       <div>variant = primary</div>
-      <div><WizIconButton variant="sub" :icon="WizISend" @click="args.onClick('sub')" /></div>
+      <div><WizIconButton label="sub" variant="sub" :icon="WizISend" @click="args.onClick('sub')" /></div>
       <div>variant = sub</div>
-      <div><WizIconButton variant="transparent" :icon="WizISend" @click="args.onClick('transparent')" /></div>
+      <div><WizIconButton label="transparent" variant="transparent" :icon="WizISend" @click="args.onClick('transparent')" /></div>
       <div>variant = transparent</div>
-      <div><WizIconButton variant="link" :icon="WizISend" @click="args.onClick('link')" /></div>
+      <div><WizIconButton label="link" variant="link" :icon="WizISend" @click="args.onClick('link')" /></div>
       <div>variant = link</div>
     </div>
   `,
@@ -53,13 +56,13 @@ Variant.parameters = {
       code: `
 <template>
   <div style="display: grid; grid-template-columns: max-content max-content; grid-gap: 1rem; align-items: center;">
-    <div><WizIconButton variant="primary" :icon="WizISend" @click="args.onClick('primary')" /></div>
+    <div><WizIconButton label="primary" variant="primary" :icon="WizISend" @click="args.onClick('primary')" /></div>
     <div>variant = primary</div>
-    <div><WizIconButton variant="sub" :icon="WizISend" @click="args.onClick('sub')" /></div>
+    <div><WizIconButton label="sub" variant="sub" :icon="WizISend" @click="args.onClick('sub')" /></div>
     <div>variant = sub</div>
-    <div><WizIconButton variant="transparent" :icon="WizISend" @click="args.onClick('transparent')" /></div>
+    <div><WizIconButton label="transparent" variant="transparent" :icon="WizISend" @click="args.onClick('transparent')" /></div>
     <div>variant = transparent</div>
-    <div><WizIconButton variant="link" :icon="WizISend" @click="args.onClick('link')" /></div>
+    <div><WizIconButton label="link" variant="link" :icon="WizISend" @click="args.onClick('link')" /></div>
     <div>variant = link</div>
   </div>
 </template>
@@ -73,11 +76,11 @@ export const Size: StoryFn<typeof WizIconButton> = (args) => ({
   setup: () => ({ args, WizISend }),
   template: `
     <div style="display: grid; grid-template-columns: max-content max-content; grid-gap: 1rem; align-items: center;">
-      <div><WizIconButton size="sm" :icon="WizISend" @click="args.onClick('sm')" /></div>
+      <div><WizIconButton label="sm" size="sm" :icon="WizISend" @click="args.onClick('sm')" /></div>
       <div>size = sm</div>
-      <div><WizIconButton size="md" :icon="WizISend" @click="args.onClick('md')" /></div>
+      <div><WizIconButton label="md" size="md" :icon="WizISend" @click="args.onClick('md')" /></div>
       <div>size = md</div>
-      <div><WizIconButton size="lg" :icon="WizISend" @click="args.onClick('lg')" /></div>
+      <div><WizIconButton label="lg" size="lg" :icon="WizISend" @click="args.onClick('lg')" /></div>
       <div>size = lg</div>
     </div>
   `,
@@ -93,11 +96,11 @@ Size.parameters = {
       code: `
 <template>
   <div style="display: grid; grid-template-columns: max-content max-content; grid-gap: 1rem; align-items: center;">
-    <div><WizIconButton size="sm" :icon="WizISend" @click="args.onClick('sm')" /></div>
+    <div><WizIconButton label="sm" size="sm" :icon="WizISend" @click="args.onClick('sm')" /></div>
     <div>size = sm</div>
-    <div><WizIconButton size="md" :icon="WizISend" @click="args.onClick('md')" /></div>
+    <div><WizIconButton label="md" size="md" :icon="WizISend" @click="args.onClick('md')" /></div>
     <div>size = md</div>
-    <div><WizIconButton size="lg" :icon="WizISend" @click="args.onClick('lg')" /></div>
+    <div><WizIconButton label="lg" size="lg" :icon="WizISend" @click="args.onClick('lg')" /></div>
     <div>size = lg</div>
   </div>
 </template>
@@ -111,13 +114,13 @@ export const Disabled: StoryFn<typeof WizIconButton> = (args) => ({
   setup: () => ({ args, WizISend }),
   template: `
     <div style="display: grid; grid-template-columns: max-content max-content; grid-gap: 1rem; align-items: center;">
-      <div><WizIconButton disabled :icon="WizISend" @click="args.onClick('primary disabled')" /></div>
+      <div><WizIconButton disabled label="send" :icon="WizISend" @click="args.onClick('primary disabled')" /></div>
       <div>disabled = true, variant = primary</div>
-      <div><WizIconButton disabled variant="sub" :icon="WizISend" @click="args.onClick('sub disabled')" /></div>
+      <div><WizIconButton disabled label="sub" variant="sub" :icon="WizISend" @click="args.onClick('sub disabled')" /></div>
       <div>disabled = true, variant = sub</div>
-      <div><WizIconButton disabled variant="transparent" :icon="WizISend" @click="args.onClick('transparent disabled')" /></div>
+      <div><WizIconButton disabled label="transparent" variant="transparent" :icon="WizISend" @click="args.onClick('transparent disabled')" /></div>
       <div>disabled = true, variant = transparent</div>
-      <div><WizIconButton disabled variant="link" :icon="WizISend" @click="args.onClick('link disabled')" /></div>
+      <div><WizIconButton disabled label="link" variant="link" :icon="WizISend" @click="args.onClick('link disabled')" /></div>
       <div>disabled = true, variant = link</div>
     </div>
   `,
@@ -133,13 +136,13 @@ Disabled.parameters = {
       code: `
 <template>
   <div style="display: grid; grid-template-columns: max-content max-content; grid-gap: 1rem; align-items: center;">
-    <div><WizIconButton disabled :icon="WizISend" @click="args.onClick('primary disabled')" /></div>
+    <div><WizIconButton disabled label="send" :icon="WizISend" @click="args.onClick('primary disabled')" /></div>
     <div>disabled = true, variant = primary</div>
-    <div><WizIconButton disabled variant="sub" :icon="WizISend" @click="args.onClick('sub disabled')" /></div>
+    <div><WizIconButton disabled label="sub" variant="sub" :icon="WizISend" @click="args.onClick('sub disabled')" /></div>
     <div>disabled = true, variant = sub</div>
-    <div><WizIconButton disabled variant="transparent" :icon="WizISend" @click="args.onClick('transparent disabled')" /></div>
+    <div><WizIconButton disabled label="transparent" variant="transparent" :icon="WizISend" @click="args.onClick('transparent disabled')" /></div>
     <div>disabled = true, variant = transparent</div>
-    <div><WizIconButton disabled variant="link" :icon="WizISend" @click="args.onClick('link disabled')" /></div>
+    <div><WizIconButton disabled label="link" variant="link" :icon="WizISend" @click="args.onClick('link disabled')" /></div>
     <div>disabled = true, variant = link</div>
   </div>
 </template>
