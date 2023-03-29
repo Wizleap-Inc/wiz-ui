f0095f26d3f9a4e49b01f374cc41e3128f8b80e1
sor4chi <sorakawa0218@gmail.com>
2023-03-29 20:11:09 +0900

    feat(wiz-ui-react): setting typescript, vite, storybook

---
+++ packages/wiz-ui-react/.storybook/main.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const { vanillaExtractPlugin } = require("@vanilla-extract/vite-plugin");
+const { mergeConfig } = require("vite");
+module.exports = {
+  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx|mdx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
+    "storycap",
+  ],
+  framework: "@storybook/react-vite",
+  features: {
+    interactionsDebugger: true,
+  },
+  core: {
+    builder: "@storybook/builder-vite",
+  },
+  viteFinal: async (config) => {
+    return mergeConfig(config, {
+      plugins: [vanillaExtractPlugin()],
+      alias: {
+        "@": path.resolve(__dirname, "../src"),
+      },
+      base: "./",
+    });
+  },
+  staticDirs: [
+    {
+      from: "./assets",
+      to: "/public",
+    },
+  ],
+  docs: {
+    docsPage: "automatic",
+    defaultName: "Docs",
+    autodocs: true,
+  },
+};
