f8b91d88bdff0a0de8f94f1a0447560494a3e2fb
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-04-05 23:44:44 +0900

    feat(text): WizTextのReact対応

---
+++ packages/wiz-ui-react/src/components/base/text/components/text.tsx
@@ -0,0 +1,96 @@
+import {
+  ColorKeys,
+  FontSizeKeys,
+  WhiteSpaceKeys,
+} from "@wizleap-inc/wiz-ui-constants";
+import * as styles from "@wizleap-inc/wiz-ui-styles/bases/text.css";
+import {
+  lineHeightStyle,
+  fontSizeStyle,
+  colorStyle,
+  whiteSpaceStyle,
+} from "@wizleap-inc/wiz-ui-styles/commons";
+import { ReactNode, memo } from "react";
+
+export type Props = {
+  as?: "p" | "span" | "label";
+  htmlFor?: string;
+  color?: ColorKeys;
+  fontSize?: FontSizeKeys;
+  lineHeight?: FontSizeKeys;
+  bold?: boolean;
+  maxLines?: number;
+  whiteSpace?: WhiteSpaceKeys;
+  dummyValue?: string;
+  breakAll?: boolean;
+  textAlign?: "start" | "end" | "left" | "right" | "center";
+  lineThrough?: boolean;
+  children?: ReactNode;
+};
+const _Text = ({
+  as = "p",
+  htmlFor,
+  color = "gray.900",
+  fontSize = "md",
+  lineHeight,
+  bold,
+  maxLines,
+  whiteSpace = "normal",
+  dummyValue,
+  breakAll,
+  textAlign = "start",
+  lineThrough = false,
+  ...props
+}: Props) => {
+  const overflowStyles = (() => {
+    if (!maxLines) return {};
+    return {
+      overflow: "hidden",
+      display: "-webkit-box",
+      webkitBoxOrient: "vertical",
+      WebkitLineClamp: maxLines,
+    };
+  })();
+  const Component = (props: { children: ReactNode }) => {
+    switch (as) {
+      case "p": {
+        return <p>{props.children}</p>;
+      }
+      case "label": {
+        return <label htmlFor={htmlFor}>{props.children}</label>;
+      }
+      case "span": {
+        return <span>{props.children}</span>;
+      }
+    }
+  };
+  return (
+    <div
+      className={`
+        ${styles.textStyle}
+        ${styles.textFontWeightStyle[bold ? "bold" : "default"]}
+        ${styles.textAlignStyle[textAlign]}      
+        ${(maxLines || breakAll) && styles.textWordBreakStyle}      
+        ${
+          lineHeight
+            ? lineHeightStyle[lineHeight]
+            : styles.textDefaultLineHeightStyle
+        }      
+        ${fontSizeStyle[fontSize]}        
+        ${colorStyle[color]}        
+        ${whiteSpaceStyle[whiteSpace]}        
+        ${lineThrough && styles.textLineThroughStyle}`}
+      style={overflowStyles}
+    >
+      <Component>
+        {dummyValue ? (
+          <span className={styles.textDummyStyle}>{dummyValue}</span>
+        ) : (
+          props.children
+        )}
+      </Component>
+    </div>
+  );
+};
+
+export const WizText = memo(_Text);
