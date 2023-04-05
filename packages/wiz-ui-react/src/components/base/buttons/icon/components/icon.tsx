46e5e6383460d78f747c97e58412c1336daf61e4
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-04-05 16:15:29 +0900

    feat(icon): WizIconを追加

---
+++ packages/wiz-ui-react/src/components/base/buttons/icon/components/icon.tsx
@@ -0,0 +1,27 @@
+import { ColorKeys, FontSizeKeys } from "@wizleap-inc/wiz-ui-constants";
+import * as styles from "@wizleap-inc/wiz-ui-styles/bases/icon.css";
+import { fillStyle, fontSizeStyle } from "@wizleap-inc/wiz-ui-styles/commons";
+import { memo } from "react";
+
+import { TIcon } from "@/components/icons";
+
+type Props = {
+  icon: TIcon;
+  color?: ColorKeys | "inherit";
+  size?: FontSizeKeys;
+};
+
+const _Icon = ({ color = "gray.700", size = "xl2", ...props }: Props) => {
+  const colorStyle =
+    color === "inherit" ? styles.iconDefaultStyle : fillStyle[color];
+  const IconComponent = props.icon;
+  return (
+    <div className={`${styles.iconStyle} ${styles.iconSizeStyle[size]}`}>
+      <div className={`${fontSizeStyle[size]} ${colorStyle}`}>
+        <IconComponent />
+      </div>
+    </div>
+  );
+};
+
+export const WizIcon = memo(_Icon);
