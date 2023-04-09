31204cf7ce6a64b0e350df744eb41cbebf5bb12c
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-04-10 01:09:11 +0900

    docs(panel-switch): storybookを追加

---
+++ packages/wiz-ui-react/src/components/base/inputs/panel-switch/stories/panel-switch.stories.tsx
@@ -0,0 +1,36 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { useState } from "react";
+
+import { WizPanelSwitch } from "../components";
+const meta: Meta<typeof WizPanelSwitch> = {
+  title: "Base/Input/PanelSwitch",
+  component: WizPanelSwitch,
+  argTypes: {
+    onClick: {
+      action: "input",
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof WizPanelSwitch>;
+
+export const Default: Story = {
+  args: {
+    items: [
+      { label: "test1", value: 1 },
+      { label: "test2", value: 2 },
+      { label: "test3", value: 3 },
+      { label: "test4", value: 4 },
+    ],
+  },
+  render: (args) => {
+    const [value, setValue] = useState<number | null>(null);
+    return (
+      <div>
+        <p>入力値：{value}</p>
+        <WizPanelSwitch {...args} value={value} onClick={setValue} />
+      </div>
+    );
+  },
+};
