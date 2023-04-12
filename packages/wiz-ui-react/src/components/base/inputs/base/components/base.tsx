77070bf7cb40799228c819f7c06bc7a700178deb
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-04-13 00:22:48 +0900

    feat(base-input): forwardRefで実装

--- packages/wiz-ui-react/src/components/base/inputs/base/components/base.tsx
+++ packages/wiz-ui-react/src/components/base/inputs/base/components/base.tsx
@@ -1,9 +1,18 @@
 import * as styles from "@wizleap-inc/wiz-ui-styles/bases/base-input.css";
 import { inputBorderStyle } from "@wizleap-inc/wiz-ui-styles/commons";
 import { clsx } from "clsx";
-import { memo, useMemo, useState } from "react";
+import {
+  ForwardedRef,
+  InputHTMLAttributes,
+  forwardRef,
+  memo,
+  useMemo,
+  useState,
+} from "react";
 
-type Props = {
+type InputProps = InputHTMLAttributes<HTMLInputElement>;
+
+type Props = InputProps & {
   id?: string;
   value?: string;
   placeholder?: string;
@@ -13,48 +22,59 @@ type Props = {
   width?: string;
   error?: boolean;
   spaceType?: "left" | "right" | "both" | "none";
-  onChange: (value: string) => void;
 };
 
-const _PrivateBaseInput = ({
-  id,
-  value,
-  placeholder,
-  disabled,
-  expand,
-  type,
-  width = "10rem",
-  error,
-  spaceType = "none",
-  ...props
-}: Props) => {
-  const [hasFocus, setHasFocus] = useState(false);
+const _PrivateBaseInput = forwardRef(
+  (
+    {
+      id,
+      value,
+      placeholder,
+      disabled,
+      expand,
+      type,
+      width = "10rem",
+      error,
+      spaceType = "none",
+      ...props
+    }: Props,
+    ref: ForwardedRef<HTMLInputElement>
+  ) => {
+    const [hasFocus, setHasFocus] = useState(false);
 
-  const state = useMemo(() => {
-    if (error) return "error";
-    if (hasFocus) return "active";
-    return "default";
-  }, [hasFocus, error]);
+    const state = useMemo(() => {
+      if (error) return "error";
+      if (hasFocus) return "active";
+      return "default";
+    }, [hasFocus, error]);
 
-  return (
-    <input
-      className={clsx(
-        styles.baseInputStyle,
-        styles.baseInputPaddingStyle[spaceType],
-        disabled && styles.baseInputDisabledStyle,
-        inputBorderStyle[state]
-      )}
-      style={{ width: expand ? "100%" : width }}
-      placeholder={placeholder}
-      disabled={disabled}
-      type={type}
-      id={id}
-      onFocus={() => setHasFocus(true)}
-      onBlur={() => setHasFocus(false)}
-      onChange={(e) => props.onChange(e.target.value)}
-      value={value}
-    />
-  );
-};
+    return (
+      <input
+        ref={ref}
+        className={clsx(
+          styles.baseInputStyle,
+          styles.baseInputPaddingStyle[spaceType],
+          disabled && styles.baseInputDisabledStyle,
+          inputBorderStyle[state]
+        )}
+        style={{ width: expand ? "100%" : width }}
+        placeholder={placeholder}
+        disabled={disabled}
+        type={type}
+        id={id}
+        onFocus={(e) => {
+          setHasFocus(true);
+          props.onFocus?.(e);
+        }}
+        onBlur={(e) => {
+          setHasFocus(false);
+          props.onBlur?.(e);
+        }}
+        onChange={props.onChange}
+        value={value}
+      />
+    );
+  }
+);
 
 export const PrivateBaseInput = memo(_PrivateBaseInput);
