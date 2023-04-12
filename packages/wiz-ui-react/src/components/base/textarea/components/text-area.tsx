41d520bde791a0b378a2c434ff9af458b73ab0a4
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-04-13 00:10:40 +0900

    feat(text-area): forwardRefで実装

--- packages/wiz-ui-react/src/components/base/textarea/components/text-area.tsx
+++ packages/wiz-ui-react/src/components/base/textarea/components/text-area.tsx
@@ -1,55 +1,66 @@
 import * as styles from "@wizleap-inc/wiz-ui-styles/bases/text-area.css";
 import { inputBorderStyle } from "@wizleap-inc/wiz-ui-styles/commons";
 import { clsx } from "clsx";
-import { memo, useMemo, useState } from "react";
+import {
+  ForwardedRef,
+  TextareaHTMLAttributes,
+  forwardRef,
+  memo,
+  useMemo,
+  useState,
+} from "react";
 
 import { useFormControl } from "@/hooks/use-form-control-provider";
 
-export type Props = {
+type TextAreaProps = TextareaHTMLAttributes<HTMLTextAreaElement>;
+
+type Props = TextAreaProps & {
   id?: string;
   value?: string;
   placeholder?: string;
   disabled?: boolean;
   expand?: boolean;
   rows?: number;
-  onChange: (value: string) => void;
 };
 
-const _TextArea = ({
-  id,
-  value,
-  placeholder,
-  disabled,
-  expand,
-  rows = 3,
-  ...props
-}: Props) => {
-  const { isError } = useFormControl();
-  const [hasFocus, setHasFocus] = useState(false);
-  const state = useMemo(() => {
-    if (isError) return "error";
-    if (hasFocus) return "active";
-    return "default";
-  }, [isError, hasFocus]);
-
-  return (
-    <textarea
-      value={value}
-      placeholder={placeholder}
-      id={id}
-      disabled={disabled}
-      rows={rows}
-      className={clsx(
-        styles.textAreaStyle,
-        styles.textAreaVariantStyle[disabled ? "disabled" : "default"],
-        expand && styles.textAreaExpandStyle,
-        inputBorderStyle[state]
-      )}
-      onFocus={() => setHasFocus(true)}
-      onBlur={() => setHasFocus(false)}
-      onChange={(e) => props.onChange(e.target.value)}
-    />
-  );
-};
+const _TextArea = forwardRef(
+  (
+    { id, value, placeholder, disabled, expand, rows = 3, ...props }: Props,
+    ref: ForwardedRef<HTMLTextAreaElement>
+  ) => {
+    const { isError } = useFormControl();
+    const [hasFocus, setHasFocus] = useState(false);
+    const state = useMemo(() => {
+      if (isError) return "error";
+      if (hasFocus) return "active";
+      return "default";
+    }, [isError, hasFocus]);
+    return (
+      <textarea
+        ref={ref}
+        value={value}
+        placeholder={placeholder}
+        id={id}
+        disabled={disabled}
+        rows={rows}
+        className={clsx(
+          styles.textAreaStyle,
+          styles.textAreaVariantStyle[disabled ? "disabled" : "default"],
+          expand && styles.textAreaExpandStyle,
+          inputBorderStyle[state]
+        )}
+        onFocus={(e) => {
+          setHasFocus(true);
+          props.onFocus?.(e);
+        }}
+        onBlur={(e) => {
+          setHasFocus(false);
+          props.onBlur?.(e);
+        }}
+        onChange={(e) => props.onChange?.(e)}
+      />
+    );
+  }
+);
 
 export const WizTextArea = memo(_TextArea);
