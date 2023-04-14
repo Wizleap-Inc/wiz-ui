3ea5896d8c32c2e871cd3ea834085295745a4dbb
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-04-15 00:57:39 +0900

    docs(popup-react): パターンを追加

--- packages/wiz-ui-react/src/components/base/popup/stories/popup.stories.tsx
+++ packages/wiz-ui-react/src/components/base/popup/stories/popup.stories.tsx
@@ -1,4 +1,6 @@
 import { Meta, StoryObj } from "@storybook/react";
+import { SpacingKeys } from "@wizleap-inc/wiz-ui-constants";
+import { useEffect, useState } from "react";
 
 import { WizProvider } from "@/components/manager";
 
@@ -54,7 +56,6 @@ export const Default: Story = {
       >
         {patterns.map((pattern, i) => (
           <div key={i}>
-            <div>aaa</div>
             <WizPopup {...args}>
               <WizPopupTrigger>
                 <button>Click me {pattern.direction}</button>
@@ -74,7 +75,238 @@ export const Default: Story = {
                 </div>
               </WizPopupContent>
             </WizPopup>
-            <div>bbbb</div>
+          </div>
+        ))}
+      </div>
+    );
+  },
+  args: {},
+};
+
+export const Gap: Story = {
+  render: (args) => {
+    const patterns: { direction: Direction; gap: SpacingKeys }[] = [
+      { direction: "rt", gap: "no" },
+      { direction: "rt", gap: "xs2" },
+      { direction: "rt", gap: "xs" },
+      { direction: "rt", gap: "sm" },
+      { direction: "rt", gap: "md" },
+      { direction: "rt", gap: "lg" },
+      { direction: "rt", gap: "xl" },
+      { direction: "rt", gap: "xl2" },
+      { direction: "rt", gap: "xl3" },
+      { direction: "rt", gap: "xl4" },
+    ];
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "2.5rem",
+        }}
+      >
+        {patterns.map((pattern, i) => (
+          <div key={i}>
+            <WizPopup {...args}>
+              <WizPopupTrigger>
+                <button>Click me {pattern.direction}</button>
+              </WizPopupTrigger>
+              <WizPopupContent {...pattern}>
+                <div
+                  style={{
+                    padding: "16px",
+                    backgroundColor: "white",
+                    borderRadius: "4px",
+                  }}
+                >
+                  <p>This is a popup content</p>
+                  <button>
+                    <WizPopupClose>close </WizPopupClose>
+                  </button>
+                </div>
+              </WizPopupContent>
+            </WizPopup>
+          </div>
+        ))}
+      </div>
+    );
+  },
+  args: {},
+};
+
+export const Playground: Story = {
+  render: (args) => {
+    const [x, setX] = useState(0);
+    const [y, setY] = useState(0);
+    const [maxX, setMaxX] = useState(0);
+    const [maxY, setMaxY] = useState(0);
+    const sbRoot = document.getElementById("storybook-root");
+    if (!sbRoot) return <></>;
+    const updateMax = () => {
+      setMaxX(sbRoot.clientWidth);
+      setMaxY(sbRoot.clientHeight);
+    };
+    useEffect(() => {
+      updateMax();
+      window.addEventListener("resize", updateMax);
+    }, []);
+    return (
+      <div
+        style={{
+          height: "100vh",
+          width: "100vw",
+          display: "relative",
+        }}
+      >
+        <div
+          style={{
+            position: "absolute",
+            top: y + "px",
+            left: x + "px",
+          }}
+        >
+          <WizPopup {...args}>
+            <WizPopupTrigger>
+              <button>Toggle</button>
+            </WizPopupTrigger>
+            <WizPopupContent>
+              <div
+                style={{
+                  padding: "16px",
+                  backgroundColor: "white",
+                  borderRadius: "4px",
+                }}
+              >
+                <p>This is a popup content</p>
+                <button>
+                  <WizPopupClose>close </WizPopupClose>
+                </button>
+              </div>
+            </WizPopupContent>
+          </WizPopup>
+        </div>
+        <input
+          type="range"
+          min={0}
+          max={maxX}
+          onChange={(e) => setX(parseInt(e.target.value))}
+        />
+        <input
+          type="range"
+          min={0}
+          max={maxY}
+          onChange={(e) => setY(parseInt(e.target.value))}
+        />
+      </div>
+    );
+  },
+  args: {},
+};
+
+export const Playground2: Story = {
+  render: (args) => {
+    const absolutePositions = [
+      {
+        top: "0px",
+        left: "0px",
+        bottom: "initial",
+        right: "initial",
+        transform: "initial",
+      },
+      {
+        top: "0px",
+        left: "initial",
+        bottom: "initial",
+        right: "0px",
+        transform: "initial",
+      },
+      {
+        top: "initial",
+        left: "0px",
+        bottom: "0px",
+        right: "initial",
+        transform: "initial",
+      },
+      {
+        top: "initial",
+        left: "initial",
+        bottom: "0px",
+        right: "0px",
+        transform: "initial",
+      },
+      {
+        top: "50%",
+        left: "50%",
+        bottom: "initial",
+        right: "initial",
+        transform: "translate(-50%, -50%)",
+      },
+      {
+        top: "50%",
+        left: "0px",
+        bottom: "initial",
+        right: "initial",
+        transform: "translate(0%, -50%)",
+      },
+      {
+        top: "50%",
+        left: "initial",
+        bottom: "initial",
+        right: "0px",
+        transform: "translate(0%, -50%)",
+      },
+      {
+        top: "0px",
+        left: "50%",
+        bottom: "initial",
+        right: "initial",
+        transform: "translate(-50%, 0%)",
+      },
+      {
+        top: "initial",
+        left: "50%",
+        bottom: "0px",
+        right: "initial",
+        transform: "translate(-50%, 0%)",
+      },
+    ];
+    return (
+      <div
+        style={{
+          height: "100vh",
+          width: "100vw",
+          position: "relative",
+        }}
+      >
+        {absolutePositions.map((pattern, i) => (
+          <div
+            key={i}
+            style={{
+              position: "absolute",
+              ...pattern,
+            }}
+          >
+            <WizPopup {...args}>
+              <WizPopupTrigger>
+                <button>Toggle</button>
+              </WizPopupTrigger>
+              <WizPopupContent {...pattern}>
+                <div
+                  style={{
+                    padding: "16px",
+                    backgroundColor: "white",
+                    borderRadius: "4px",
+                  }}
+                >
+                  <p>This is a popup content</p>
+                  <button>
+                    <WizPopupClose>close </WizPopupClose>
+                  </button>
+                </div>
+              </WizPopupContent>
+            </WizPopup>
           </div>
         ))}
       </div>
