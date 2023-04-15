d291fbab4bd1ee70b673404995bd756c161deedd
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-04-15 18:22:00 +0900

    feat(anchor): forwardedRef

--- packages/wiz-ui-react/src/components/base/anchor/components/anchor.tsx
+++ packages/wiz-ui-react/src/components/base/anchor/components/anchor.tsx
@@ -1,5 +1,6 @@
 import {
   ColorKeys,
+  ComponentName,
   FontSizeKeys,
   FontWeightKeys,
   getRelativeFontSize,
@@ -11,7 +12,7 @@ import {
   fontWeightStyle,
 } from "@wizleap-inc/wiz-ui-styles/commons";
 import clsx from "clsx";
-import { ReactNode, memo, useMemo } from "react";
+import { ForwardedRef, ReactNode, forwardRef, memo, useMemo } from "react";
 import { Link } from "react-router-dom";
 
 import { TIcon, WizIcon } from "@/components";
@@ -28,73 +29,82 @@ type Props = {
   children: ReactNode;
 };
 
-const _Anchor = ({
-  to,
-  color = "blue.800",
-  fontSize = "md",
-  fontWeight = "normal",
-  icon,
-  iconPosition = "left",
-  openInNewTab = false,
-  nowrap = false,
-  ...props
-}: Props) => {
-  const isRouterLink = useMemo(() => {
-    if (openInNewTab) return false;
-    return !to.startsWith("http");
-  }, []);
+const _Anchor = forwardRef(
+  (
+    {
+      to,
+      color = "blue.800",
+      fontSize = "md",
+      fontWeight = "normal",
+      icon,
+      iconPosition = "left",
+      openInNewTab = false,
+      nowrap = false,
+      ...props
+    }: Props,
+    ref: ForwardedRef<HTMLAnchorElement>
+  ) => {
+    const isRouterLink = useMemo(() => {
+      if (openInNewTab) return false;
+      return !to.startsWith("http");
+    }, []);
 
-  const AnchorContent = ({ children }: { children: ReactNode }) => (
-    <>
-      {icon && iconPosition === "left" && (
-        <WizIcon
-          icon={icon}
-          size={getRelativeFontSize(fontSize, 3)}
-          color={color}
-        />
-      )}
-      {children}
-      {icon && iconPosition === "right" && (
-        <WizIcon
-          icon={icon}
-          size={getRelativeFontSize(fontSize, 3)}
-          color={color}
-        />
-      )}
-    </>
-  );
+    const AnchorContent = ({ children }: { children: ReactNode }) => (
+      <>
+        {icon && iconPosition === "left" && (
+          <WizIcon
+            icon={icon}
+            size={getRelativeFontSize(fontSize, 3)}
+            color={color}
+          />
+        )}
+        {children}
+        {icon && iconPosition === "right" && (
+          <WizIcon
+            icon={icon}
+            size={getRelativeFontSize(fontSize, 3)}
+            color={color}
+          />
+        )}
+      </>
+    );
 
-  const anchorStyle = clsx([
-    styles.anchorStyle,
-    colorStyle[color],
-    fontSizeStyle[fontSize],
-    fontWeightStyle[fontWeight],
-    nowrap && styles.anchorNoWrapStyle,
-  ]);
+    const anchorStyle = clsx([
+      styles.anchorStyle,
+      colorStyle[color],
+      fontSizeStyle[fontSize],
+      fontWeightStyle[fontWeight],
+      nowrap && styles.anchorNoWrapStyle,
+    ]);
 
-  return (
-    <div>
-      {isRouterLink ? (
-        <Link
-          className={anchorStyle}
-          to={to}
-          target={openInNewTab ? "_blank" : undefined}
-          rel={openInNewTab ? "noopener noreferrer" : undefined}
-        >
-          <AnchorContent>{props.children}</AnchorContent>
-        </Link>
-      ) : (
-        <a
-          className={anchorStyle}
-          href={to}
-          target={openInNewTab ? "_blank" : undefined}
-          rel={openInNewTab ? "noopener noreferrer" : undefined}
-        >
-          <AnchorContent>{props.children}</AnchorContent>
-        </a>
-      )}
-    </div>
-  );
-};
+    return (
+      <div>
+        {isRouterLink ? (
+          <Link
+            ref={ref}
+            className={anchorStyle}
+            to={to}
+            target={openInNewTab ? "_blank" : undefined}
+            rel={openInNewTab ? "noopener noreferrer" : undefined}
+          >
+            <AnchorContent>{props.children}</AnchorContent>
+          </Link>
+        ) : (
+          <a
+            ref={ref}
+            className={anchorStyle}
+            href={to}
+            target={openInNewTab ? "_blank" : undefined}
+            rel={openInNewTab ? "noopener noreferrer" : undefined}
+          >
+            <AnchorContent>{props.children}</AnchorContent>
+          </a>
+        )}
+      </div>
+    );
+  }
+);
+
+_Anchor.displayName = ComponentName.Anchor;
 
 export const WizAnchor = memo(_Anchor);
