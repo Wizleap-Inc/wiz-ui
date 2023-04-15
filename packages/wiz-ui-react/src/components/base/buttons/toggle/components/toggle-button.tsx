8a8f10d1f8f103c2ff9f3f7c34590f7359552169
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-04-15 17:51:06 +0900

    feat(toggle-button): forwardRef実装

--- packages/wiz-ui-react/src/components/base/buttons/toggle/components/toggle-button.tsx
+++ packages/wiz-ui-react/src/components/base/buttons/toggle/components/toggle-button.tsx
@@ -1,7 +1,14 @@
+import { ComponentName } from "@wizleap-inc/wiz-ui-constants";
 import * as styles from "@wizleap-inc/wiz-ui-styles/bases/toggle-button.css";
 import { gapStyle } from "@wizleap-inc/wiz-ui-styles/commons";
 import clsx from "clsx";
-import { ComponentProps, ReactNode, memo } from "react";
+import {
+  ComponentProps,
+  ForwardedRef,
+  ReactNode,
+  forwardRef,
+  memo,
+} from "react";
 
 import { TIcon, WizIcon } from "@/components";
 
@@ -15,43 +22,51 @@ type Props = ComponentProps<"button"> & {
   children?: ReactNode;
 };
 
-const _ToggleButton = ({
-  inActiveIcon,
-  activeIcon,
-  disabled = false,
-  rounded = true,
-  size = "md",
-  isActive,
-  ...props
-}: Props) => {
-  const iconSize = (() => {
-    if (size === "sm") return "xl";
-    if (size === "md") return "xl2";
-    if (size === "lg") return "xl3";
-    return undefined;
-  })();
-  return (
-    <button
-      className={clsx(
-        styles.toggleButtonStyle,
-        styles.toggleButtonSizeStyle[size],
-        isActive && styles.toggleButtonActiveStyle,
-        disabled && styles.toggleButtonDisabledStyle,
-        rounded && styles.toggleButtonRoundedStyle
-      )}
-      disabled={disabled}
-      {...props}
-    >
-      <div className={clsx(gapStyle["xs2"], styles.toggleButtonStackStyle)}>
-        <WizIcon
-          icon={isActive ? activeIcon : inActiveIcon}
-          color={"green.800"}
-          size={iconSize}
-        />
-        {props.children}
-      </div>
-    </button>
-  );
-};
+const _ToggleButton = forwardRef(
+  (
+    {
+      inActiveIcon,
+      activeIcon,
+      disabled = false,
+      rounded = true,
+      size = "md",
+      isActive,
+      ...props
+    }: Props,
+    ref: ForwardedRef<HTMLButtonElement>
+  ) => {
+    const iconSize = (() => {
+      if (size === "sm") return "xl";
+      if (size === "md") return "xl2";
+      if (size === "lg") return "xl3";
+      return undefined;
+    })();
+    return (
+      <button
+        ref={ref}
+        className={clsx(
+          styles.toggleButtonStyle,
+          styles.toggleButtonSizeStyle[size],
+          isActive && styles.toggleButtonActiveStyle,
+          disabled && styles.toggleButtonDisabledStyle,
+          rounded && styles.toggleButtonRoundedStyle
+        )}
+        disabled={disabled}
+        {...props}
+      >
+        <div className={clsx(gapStyle["xs2"], styles.toggleButtonStackStyle)}>
+          <WizIcon
+            icon={isActive ? activeIcon : inActiveIcon}
+            color={"green.800"}
+            size={iconSize}
+          />
+          {props.children}
+        </div>
+      </button>
+    );
+  }
+);
+
+_ToggleButton.displayName = ComponentName.ToggleButton;
 
 export const WizToggleButton = memo(_ToggleButton);
