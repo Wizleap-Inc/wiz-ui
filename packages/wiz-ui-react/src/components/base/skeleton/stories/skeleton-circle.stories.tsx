943bb9cba3514b92077a9fae862e2b5fb9f1f9f2
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-04-16 12:40:11 +0900

    docs(skeleton-circle): add react stories

---
+++ packages/wiz-ui-react/src/components/base/skeleton/stories/skeleton-circle.stories.tsx
@@ -0,0 +1,61 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import { WizSkeletonCircle } from "../components";
+
+const meta: Meta<typeof WizSkeletonCircle> = {
+  title: "Base/Skeleton/Circle",
+  component: WizSkeletonCircle,
+  argTypes: {
+    size: {
+      control: { type: "text" },
+    },
+    isLoading: {
+      control: { type: "boolean" },
+    },
+  },
+  parameters: {
+    docs: {
+      description: {
+        component: `
+### WizSkeletonCircle
+
+円形のスケルトンを表示するコンポーネントです。主に（というかほぼ）アバターのスケルトンに使用します。
+`,
+      },
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof WizSkeletonCircle>;
+
+export const Default: Story = {
+  args: {
+    size: "2.5rem",
+    isLoading: true,
+  },
+  render: (args) => (
+    <WizSkeletonCircle {...args}>
+      {/* <WizAvatar src="./public/images/avatar-1.png" alt="avatar" /> */}
+    </WizSkeletonCircle>
+  ),
+};
+
+export const Size: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: `Circleの直径を指定することができます。`,
+      },
+    },
+  },
+  args: {
+    size: "5rem",
+    isLoading: true,
+  },
+  render: (args) => (
+    <WizSkeletonCircle {...args}>
+      {/* <WizAvatar src="./public/images/avatar-1.png" alt="avatar" /> */}
+    </WizSkeletonCircle>
+  ),
+};
