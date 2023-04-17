af51767b8b762aed2ea3492b72c9d250c0219e5d
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-04-18 00:27:50 +0900

    fix(popup): 回り込みロジックを修正

--- packages/wiz-ui-react/src/components/base/popup/components/popup-content.tsx
+++ packages/wiz-ui-react/src/components/base/popup/components/popup-content.tsx
@@ -22,18 +22,17 @@ import { Direction } from "../types/direction";
 
 import { PopupContext } from "./popup-context";
 
+type PopupPlacementStyle = {
+  bottom?: number;
+  left?: number;
+  right?: number;
+  top?: number;
+  transform?: string;
+};
+
 const getPlacementStyle: Record<
   Direction,
-  (
-    rect: DOMRect,
-    gap?: string
-  ) => {
-    bottom?: number;
-    left?: number;
-    right?: number;
-    top?: number;
-    transform?: string;
-  }
+  (rect: DOMRect, gap?: string) => PopupPlacementStyle
 > = {
   tl: (rect: DOMRect, gap?: string) => ({
     top: rect.y + window.scrollY,
@@ -97,14 +96,13 @@ const getPlacementStyle: Record<
   }),
 };
 
-const shift: Record<
+const adjustDirection: Record<
   Direction,
   (body: DOMRect, content: DOMRect, trigger: DOMRect) => Direction
 > = {
   bl: (body: DOMRect, content: DOMRect, trigger: DOMRect) => {
     const y =
       body.height < trigger.y + trigger.height + window.scrollY ? "t" : "b";
-
     const x =
       body.width < trigger.x + content.width + window.scrollX ? "r" : "l";
     return `${y}${x}` as Direction;
@@ -242,6 +240,8 @@ type Props = {
   gap?: SpacingKeys;
   shadow?: boolean;
   animation?: boolean;
+  /** Popupの表示領域を指定します。指定した場合、回り込みロジックによりPopupは指定した要素の内側に表示されます。 */
+  bodyElement?: HTMLElement;
   children: ReactNode;
 } & ComponentProps<"div">;
 
@@ -260,26 +260,24 @@ export const WizPopupContent = ({
   const contentRef = useRef<HTMLDivElement>(null);
   if (closeOnBlur) useClickOutside(contentRef, ctx.closePopup, ctx.triggerRef);
 
-  const placementStyle = (() => {
-    const bodyRect = document
-      .getElementById("storybook-root")
-      ?.getBoundingClientRect(); // document.body.getBoundingClientRect();
-    const contentRect = contentRef.current?.getBoundingClientRect();
-    const triggerRect = ctx.triggerRef.current?.getBoundingClientRect();
+  const getStyle = (): PopupPlacementStyle => {
     const gapRem = getSpacingCss(gap) ?? "0";
+    const triggerRect = ctx.triggerRef.current?.getBoundingClientRect();
+    if (!triggerRect) return {}; // triggerがない場合はPopupを表示しません
+    const bodyRect = props.bodyElement?.getBoundingClientRect();
+    const contentRect = contentRef.current?.getBoundingClientRect();
+    if (!bodyRect || !contentRect)
+      // Popupの表示領域の指定がない場合、回り込みロジックは適用されません。(!bodyRect)
+      // レンダリング前にgetStyleが呼ばれると、contentRectを取得できないため、回り込みロジックは適用されません。(!contentRect)
+      return getPlacementStyle[direction](triggerRect, gapRem);
+    const dir = adjustDirection[direction](bodyRect, contentRect, triggerRect);
+    return getPlacementStyle[dir](triggerRect, gapRem);
+  };
 
-    if (!bodyRect || !triggerRect) return;
-    if (!contentRect) return getPlacementStyle[direction](triggerRect, gapRem);
-
-    const f = shift[direction];
-    const shifted = bodyRect
-      ? f(bodyRect, contentRect, triggerRect)
-      : direction;
-    return getPlacementStyle[shifted](triggerRect, gapRem);
-  })();
-
+  const [placementStyle, setPlacementStyle] = useState<PopupPlacementStyle>({});
   const [isActuallyOpen, setIsActuallyOpen] = useState(false);
   useEffect(() => {
+    setPlacementStyle(getStyle());
     if (!animation || !contentRef.current) {
       setIsActuallyOpen(ctx.isOpen);
       return;
@@ -288,7 +286,7 @@ export const WizPopupContent = ({
       fadeAnimation.open(contentRef.current, () => setIsActuallyOpen(true));
     else
       fadeAnimation.close(contentRef.current, () => setIsActuallyOpen(false));
-  }, [ctx.isOpen, isActuallyOpen]);
+  }, [ctx.isOpen, isActuallyOpen, direction]);
   if (!isActuallyOpen) return null;
   return (
     <WizPortal>
