2181149d0e5b885dfcfcb27cb617eff96fbe9dd6
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-04-18 16:17:43 +0900

    refactor(avatar): forwardRef, ComponentProps

--- packages/wiz-ui-react/src/components/base/avatar/components/avatar.tsx
+++ packages/wiz-ui-react/src/components/base/avatar/components/avatar.tsx
@@ -6,7 +6,13 @@ import {
   colorStyle,
 } from "@wizleap-inc/wiz-ui-styles/commons";
 import clsx from "clsx";
-import { memo, useState } from "react";
+import {
+  ComponentProps,
+  ForwardedRef,
+  forwardRef,
+  memo,
+  useState,
+} from "react";
 
 type Props = {
   src: string;
@@ -16,50 +22,58 @@ type Props = {
   alt?: string;
   fallback?: string;
   clickable?: boolean;
-  onClick?: () => void;
-};
+} & ComponentProps<"div">;
 
-const _Avatar = ({
-  src,
-  size = "xl3",
-  color = "gray.900",
-  bgColor = "gray.400",
-  alt,
-  fallback,
-  clickable,
-  ...props
-}: Props) => {
-  const [isImgLoadSuccess, setIsLoadSuccess] = useState(true);
+const _Avatar = forwardRef(
+  (
+    {
+      src,
+      size = "xl3",
+      color = "gray.900",
+      bgColor = "gray.400",
+      alt,
+      fallback,
+      clickable,
+      ...props
+    }: Props,
+    ref: ForwardedRef<HTMLDivElement>
+  ) => {
+    const [isImgLoadSuccess, setIsLoadSuccess] = useState(true);
 
-  const onClick = () => clickable && props.onClick?.();
-  const onError = () => setIsLoadSuccess(false);
+    const onError = () => setIsLoadSuccess(false);
 
-  return (
-    <div
-      className={clsx(
-        styles.avatarStyle,
-        sizeStyle[size],
-        colorStyle[color],
-        clickable && styles.avatarClickableStyle
-      )}
-      onClick={onClick}
-    >
-      {isImgLoadSuccess ? (
-        <img
-          className={styles.avatarImageStyle}
-          src={src}
-          alt={alt}
-          onError={onError}
-        />
-      ) : (
-        <div
-          className={clsx(styles.avatarFallbackStyle, backgroundStyle[bgColor])}
-        >
-          {fallback}
-        </div>
-      )}
-    </div>
-  );
-};
+    return (
+      <div
+        ref={ref}
+        className={clsx(
+          styles.avatarStyle,
+          sizeStyle[size],
+          colorStyle[color],
+          clickable && styles.avatarClickableStyle
+        )}
+        {...props}
+        onClick={(e) => clickable && props.onClick?.(e)}
+      >
+        {isImgLoadSuccess ? (
+          <img
+            className={styles.avatarImageStyle}
+            src={src}
+            alt={alt}
+            onError={onError}
+          />
+        ) : (
+          <div
+            className={clsx(
+              styles.avatarFallbackStyle,
+              backgroundStyle[bgColor]
+            )}
+          >
+            {fallback}
+          </div>
+        )}
+      </div>
+    );
+  }
+);
 
 export const WizAvatar = memo(_Avatar);
