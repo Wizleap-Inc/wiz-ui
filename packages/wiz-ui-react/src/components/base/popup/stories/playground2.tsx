4ef44b992c6ff667dab51e4f2b9aeab2807e2698
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-05-17 15:43:02 +0900

    docs(popup): refactor

--- packages/wiz-ui-react/src/components/base/popup/stories/playground2.tsx
+++ packages/wiz-ui-react/src/components/base/popup/stories/playground2.tsx
@@ -1,9 +1,75 @@
 import { StoryObj } from "@storybook/react";
-import { useRef, useState } from "react";
+import { CSSProperties, useRef, useState } from "react";
 
 import { WizPopup } from "../components";
 
 import { popupContentStyle } from "./common";
+
+const absolutePositions: CSSProperties[] = [
+  {
+    top: "0px",
+    left: "0px",
+    bottom: "initial",
+    right: "initial",
+    transform: "initial",
+  },
+  {
+    top: "0px",
+    left: "initial",
+    bottom: "initial",
+    right: "0px",
+    transform: "initial",
+  },
+  {
+    top: "initial",
+    left: "0px",
+    bottom: "0px",
+    right: "initial",
+    transform: "initial",
+  },
+  {
+    top: "initial",
+    left: "initial",
+    bottom: "0px",
+    right: "0px",
+    transform: "initial",
+  },
+  {
+    top: "50%",
+    left: "50%",
+    bottom: "initial",
+    right: "initial",
+    transform: "translate(-50%, -50%)",
+  },
+  {
+    top: "50%",
+    left: "0px",
+    bottom: "initial",
+    right: "initial",
+    transform: "translate(0%, -50%)",
+  },
+  {
+    top: "50%",
+    left: "initial",
+    bottom: "initial",
+    right: "0px",
+    transform: "translate(0%, -50%)",
+  },
+  {
+    top: "0px",
+    left: "50%",
+    bottom: "initial",
+    right: "initial",
+    transform: "translate(-50%, 0%)",
+  },
+  {
+    top: "initial",
+    left: "50%",
+    bottom: "0px",
+    right: "initial",
+    transform: "translate(-50%, 0%)",
+  },
+];
 
 export const Playground2Story: StoryObj<typeof WizPopup> = {
   args: {
@@ -11,71 +77,28 @@ export const Playground2Story: StoryObj<typeof WizPopup> = {
     isDirectionFixed: false,
   },
   render: (args) => {
-    const absolutePositions = [
-      {
-        top: "0px",
-        left: "0px",
-        bottom: "initial",
-        right: "initial",
-        transform: "initial",
-      },
-      {
-        top: "0px",
-        left: "initial",
-        bottom: "initial",
-        right: "0px",
-        transform: "initial",
-      },
-      {
-        top: "initial",
-        left: "0px",
-        bottom: "0px",
-        right: "initial",
-        transform: "initial",
-      },
-      {
-        top: "initial",
-        left: "initial",
-        bottom: "0px",
-        right: "0px",
-        transform: "initial",
-      },
-      {
-        top: "50%",
-        left: "50%",
-        bottom: "initial",
-        right: "initial",
-        transform: "translate(-50%, -50%)",
-      },
-      {
-        top: "50%",
-        left: "0px",
-        bottom: "initial",
-        right: "initial",
-        transform: "translate(0%, -50%)",
-      },
-      {
-        top: "50%",
-        left: "initial",
-        bottom: "initial",
-        right: "0px",
-        transform: "translate(0%, -50%)",
-      },
-      {
-        top: "0px",
-        left: "50%",
-        bottom: "initial",
-        right: "initial",
-        transform: "translate(-50%, 0%)",
-      },
-      {
-        top: "initial",
-        left: "50%",
-        bottom: "0px",
-        right: "initial",
-        transform: "translate(-50%, 0%)",
-      },
-    ];
+    const Popup = () => {
+      const [isOpen, setIsOpen] = useState(true);
+      const anchor = useRef<HTMLButtonElement | null>(null);
+      return (
+        <>
+          <button ref={anchor} onClick={() => setIsOpen((v) => !v)}>
+            Toggle
+          </button>
+          <WizPopup
+            {...args}
+            anchorElement={anchor}
+            isOpen={isOpen}
+            onClose={() => setIsOpen(false)}
+          >
+            <div style={popupContentStyle}>
+              <p>This is a popup content</p>
+              <button onClick={() => setIsOpen(false)}>Close</button>
+            </div>
+          </WizPopup>
+        </>
+      );
+    };
     return (
       <div
         style={{
@@ -84,31 +107,16 @@ export const Playground2Story: StoryObj<typeof WizPopup> = {
           position: "relative",
         }}
       >
-        {absolutePositions.map((pattern, i) => {
-          const [isOpen, setIsOpen] = useState(true);
-          const anchor = useRef<HTMLButtonElement | null>(null);
+        {absolutePositions.map((position, i) => {
           return (
             <div
               key={i}
               style={{
                 position: "absolute",
-                ...pattern,
+                ...position,
               }}
             >
-              <button ref={anchor} onClick={() => setIsOpen((v) => !v)}>
-                Toggle
-              </button>
-              <WizPopup
-                {...args}
-                anchorElement={anchor}
-                isOpen={isOpen}
-                onClose={() => setIsOpen(false)}
-              >
-                <div style={popupContentStyle}>
-                  <p>This is a popup content</p>
-                  <button onClick={() => setIsOpen(false)}>Close</button>
-                </div>
-              </WizPopup>
+              <Popup />
             </div>
           );
         })}
