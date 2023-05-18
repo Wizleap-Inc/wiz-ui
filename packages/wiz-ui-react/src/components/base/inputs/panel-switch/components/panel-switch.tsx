2e279f28da2a690545608b3bc373551fd3fc3b1e
wmoai <wmoaism@gmail.com>
2023-05-18 14:42:12 +0900

    fix: activeの判定方法を変更

--- packages/wiz-ui-react/src/components/base/inputs/panel-switch/components/panel-switch.tsx
+++ packages/wiz-ui-react/src/components/base/inputs/panel-switch/components/panel-switch.tsx
@@ -1,6 +1,6 @@
+import { ComponentName } from "@wizleap-inc/wiz-ui-constants";
 import * as styles from "@wizleap-inc/wiz-ui-styles/bases/panel-switch-input.css";
 import clsx from "clsx";
-import { memo } from "react";
 
 import { PanelItems } from "./type";
 
@@ -10,42 +10,48 @@ type Props = {
   setValue?: (value: number) => void;
 };
 
-const _PanelSwitch = ({ value, items, ...props }: Props) => {
-  const panelSwitchBlockColor = (key: number) =>
-    value && key === value - 1 ? "active" : "default";
+const PanelSwitch = ({ value, items, setValue }: Props) => {
   return (
     <div className={styles.panelSwitchStyle}>
-      {items.map((item, index) => (
-        <label
-          key={item.value}
-          className={clsx(
-            styles.panelSwitchBlockStyle,
-            index !== items.length - 1 && styles.panelSwitchBlockDividerStyle,
-            index === 0 && styles.panelSwitchBlockLeftStyle,
-            index === items.length - 1 && styles.panelSwitchBlockRightStyle,
-            value && index === value - 1 && styles.panelSwitchBlockActiveStyle,
-            styles.panelSwitchBlockColorStyle[panelSwitchBlockColor(index)]
-          )}
-          htmlFor={`panel${item.value}`}
-          tabIndex={0}
-          onKeyDown={(e) => {
-            if (e.key === "Enter" || e.key === " ")
-              props.setValue?.(item.value);
-          }}
-        >
-          {item.label}
-          <input
-            className={styles.panelSwitchInputStyle}
-            type="radio"
-            id={`panel${item.value}`}
-            name={`panel${item.value}`}
-            value={item.value}
-            onClick={() => props.setValue?.(item.value)}
-          />
-        </label>
-      ))}
+      {items.map((item, index) => {
+        const isFirstItem = index === 0;
+        const isLastItem = index === items.length - 1;
+        const isActiveItem = value && value === item.value;
+        return (
+          <label
+            key={item.value}
+            className={clsx(
+              styles.panelSwitchBlockStyle,
+              !isLastItem && styles.panelSwitchBlockDividerStyle,
+              isFirstItem && styles.panelSwitchBlockLeftStyle,
+              isLastItem && styles.panelSwitchBlockRightStyle,
+              isActiveItem && styles.panelSwitchBlockActiveStyle,
+              styles.panelSwitchBlockColorStyle[
+                isActiveItem ? "active" : "default"
+              ]
+            )}
+            htmlFor={`panel${item.value}`}
+            tabIndex={0}
+            onKeyDown={(e) => {
+              if (e.key === "Enter" || e.key === " ") setValue?.(item.value);
+            }}
+          >
+            {item.label}
+            <input
+              className={styles.panelSwitchInputStyle}
+              type="radio"
+              id={`panel${item.value}`}
+              name={`panel${item.value}`}
+              value={item.value}
+              onClick={() => setValue?.(item.value)}
+            />
+          </label>
+        );
+      })}
     </div>
   );
 };
 
-export const WizPanelSwitch = memo(_PanelSwitch);
+PanelSwitch.displayName = ComponentName.PanelSwitch;
+
+export const WizPanelSwitch = PanelSwitch;
