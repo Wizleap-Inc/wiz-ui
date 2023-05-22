25962e3ed1e22433c4b6a737d4ff05e1e05ac065
wmoai <wmoaism@gmail.com>
2023-05-22 15:23:48 +0900

    fix: vue版にあったpropsを追加

--- packages/wiz-ui-react/src/components/base/tables/flat/components/flat-td.tsx
+++ packages/wiz-ui-react/src/components/base/tables/flat/components/flat-td.tsx
@@ -1,16 +1,23 @@
-import { ComponentName } from "@wizleap-inc/wiz-ui-constants";
+import { ComponentName, THEME } from "@wizleap-inc/wiz-ui-constants";
 import * as styles from "@wizleap-inc/wiz-ui-styles/bases/flat-table.css";
 import clsx from "clsx";
 import { ComponentProps, ReactNode } from "react";
 
 type Props = {
   align?: "left" | "center" | "right";
+  fontSize?: "xs2" | "xs" | "sm" | "md";
   children?: ReactNode;
 } & ComponentProps<"td">;
 
-const FlatTd = ({ align = "center", children, ...props }: Props) => (
+const FlatTd = ({
+  align = "center",
+  fontSize = "sm",
+  children,
+  ...props
+}: Props) => (
   <td
     className={clsx(styles.flatTdStyle, styles.flatTdAlignStyle[align])}
+    style={{ fontSize: THEME.fontSize[fontSize] }}
     {...props}
   >
     {children}
