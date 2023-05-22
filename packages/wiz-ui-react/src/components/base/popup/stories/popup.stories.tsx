6d3ef840b0ee0f1f0216ac0fe54d099ac0143310
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-05-22 16:10:34 +0900

    docs(popup): remove playground2

--- packages/wiz-ui-react/src/components/base/popup/stories/popup.stories.tsx
+++ packages/wiz-ui-react/src/components/base/popup/stories/popup.stories.tsx
@@ -5,7 +5,6 @@ import { WizPopup } from "../components";
 import { DefaultStory } from "./default";
 import { GapStory } from "./gap";
 import { PlaygroundStory } from "./playground";
-import { Playground2Story } from "./playground2";
 
 const meta: Meta<typeof WizPopup> = {
   title: "Base/Popup",
@@ -21,5 +20,3 @@ export const Default: Story = DefaultStory;
 export const Gap: Story = GapStory;
 
 export const Playground: Story = PlaygroundStory;
-
-export const Playground2: Story = Playground2Story;
