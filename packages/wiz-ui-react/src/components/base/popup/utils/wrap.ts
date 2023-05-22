9869e1a0f6ae9db4bdeebdeeaca920f822f9320f
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-05-22 15:01:27 +0900

    refactor(popup): 回り込み条件の切り出し

--- packages/wiz-ui-react/src/components/base/popup/utils/wrap.ts
+++ packages/wiz-ui-react/src/components/base/popup/utils/wrap.ts
@@ -1,115 +1,100 @@
 import { DirectionValues } from "../types/direction";
 
+const isOutOfBound = {
+  top: (_: DOMRect, content: DOMRect, anchor: DOMRect) =>
+    anchor.y - content.height + window.scrollY < 0,
+  bottom: (bound: DOMRect, content: DOMRect, anchor: DOMRect) =>
+    bound.height < anchor.y + anchor.height + content.height + window.scrollY,
+  left: (_: DOMRect, content: DOMRect, anchor: DOMRect) =>
+    anchor.x - content.width + window.scrollX < 0,
+  right: (bound: DOMRect, content: DOMRect, anchor: DOMRect) =>
+    bound.width < anchor.x + anchor.width + content.width + window.scrollX,
+  rightOnV: (bound: DOMRect, content: DOMRect, anchor: DOMRect) =>
+    bound.width < anchor.x + content.width + window.scrollX,
+  leftOnV: (_: DOMRect, content: DOMRect, anchor: DOMRect) =>
+    anchor.x + anchor.width - content.width + window.scrollX < 0,
+  topOnH: (_: DOMRect, content: DOMRect, anchor: DOMRect) =>
+    anchor.y + anchor.height - content.height + window.scrollY < 0,
+  bottomOnH: (bound: DOMRect, content: DOMRect, anchor: DOMRect) =>
+    bound.height < anchor.y + anchor.height + content.height + window.scrollY,
+  leftOnHCenter: (_: DOMRect, content: DOMRect, anchor: DOMRect) =>
+    anchor.x - (anchor.width + content.width) / 2 + window.scrollX < 0,
+  rightOnHCenter: (bound: DOMRect, content: DOMRect, anchor: DOMRect) =>
+    bound.width <
+    anchor.x + (anchor.width + content.width) / 2 + window.scrollX,
+  topOnVCenter: (_: DOMRect, content: DOMRect, anchor: DOMRect) =>
+    anchor.y - (anchor.width + content.height) / 2 + window.scrollY < 0,
+  bottomOnVCenter: (bound: DOMRect, content: DOMRect, anchor: DOMRect) =>
+    bound.height <
+    anchor.y + (anchor.width + content.height) / 2 + window.scrollY,
+};
+
 export const wrapDirection: Record<
   DirectionValues,
   (bound: DOMRect, content: DOMRect, anchor: DOMRect) => DirectionValues
 > = {
   bl: (bound: DOMRect, content: DOMRect, anchor: DOMRect) => {
-    const y =
-      bound.height < anchor.y + anchor.height + content.height + window.scrollY
-        ? "t"
-        : "b";
-    const x =
-      bound.width < anchor.x + content.width + window.scrollX ? "r" : "l";
+    const y = isOutOfBound.bottom(bound, content, anchor) ? "t" : "b";
+    const x = isOutOfBound.rightOnV(bound, content, anchor) ? "r" : "l";
     return `${y}${x}` as DirectionValues;
   },
   bc: (bound: DOMRect, content: DOMRect, anchor: DOMRect) => {
-    const y =
-      bound.height < anchor.y + anchor.height + content.height + window.scrollY
-        ? "t"
-        : "b";
-    const r =
-      bound.width <
-        anchor.x + (anchor.width + content.width) / 2 + window.scrollX && "r";
-    const l =
-      anchor.x - (anchor.width + content.width) / 2 + window.scrollX < 0 && "l";
+    const y = isOutOfBound.bottom(bound, content, anchor) ? "t" : "b";
+    const r = isOutOfBound.rightOnHCenter(bound, content, anchor) && "r";
+    const l = isOutOfBound.leftOnHCenter(bound, content, anchor) && "l";
     return `${y}${l || r || "c"}` as DirectionValues;
   },
   br: (bound: DOMRect, content: DOMRect, anchor: DOMRect) => {
-    const y =
-      bound.height < anchor.y + anchor.height + content.height + window.scrollY
-        ? "t"
-        : "b";
-    const x =
-      anchor.x + anchor.width - content.width + window.scrollX < 0 ? "l" : "r";
+    const y = isOutOfBound.bottom(bound, content, anchor) ? "t" : "b";
+    const x = isOutOfBound.leftOnV(bound, content, anchor) ? "l" : "r";
     return `${y}${x}` as DirectionValues;
   },
   tl: (bound: DOMRect, content: DOMRect, anchor: DOMRect) => {
-    const y = anchor.y - content.height + window.scrollY < 0 ? "b" : "t";
-    const x =
-      bound.width < anchor.x + content.width + window.scrollX ? "r" : "l";
+    const y = isOutOfBound.top(bound, content, anchor) ? "b" : "t";
+    const x = isOutOfBound.rightOnV(bound, content, anchor) ? "r" : "l";
     return `${y}${x}` as DirectionValues;
   },
   tc: (bound: DOMRect, content: DOMRect, anchor: DOMRect) => {
-    const y = anchor.y - content.height + window.scrollY < 0 ? "b" : "t";
-    const r =
-      bound.width <
-        anchor.x + (anchor.width + content.width) / 2 + window.scrollX && "r";
-    const l =
-      anchor.x - (anchor.width + content.width) / 2 + window.scrollX < 0 && "l";
+    const y = isOutOfBound.top(bound, content, anchor) ? "b" : "t";
+    const r = isOutOfBound.rightOnHCenter(bound, content, anchor) && "r";
+    const l = isOutOfBound.leftOnHCenter(bound, content, anchor) && "l";
     return `${y}${l || r || "c"}` as DirectionValues;
   },
-  tr: (_: DOMRect, content: DOMRect, anchor: DOMRect) => {
-    const y = anchor.y - content.height + window.scrollY < 0 ? "b" : "t";
-    const x =
-      anchor.x + anchor.width - content.width + window.scrollX < 0 ? "l" : "r";
+  tr: (bound: DOMRect, content: DOMRect, anchor: DOMRect) => {
+    const y = isOutOfBound.top(bound, content, anchor) ? "b" : "t";
+    const x = isOutOfBound.leftOnV(bound, content, anchor) ? "l" : "r";
     return `${y}${x}` as DirectionValues;
   },
   rt: (bound: DOMRect, content: DOMRect, anchor: DOMRect) => {
-    const x =
-      bound.width < anchor.x + anchor.width + content.width + window.scrollX
-        ? "l"
-        : "r";
-    const y =
-      bound.height < anchor.y + content.height + window.scrollY ? "b" : "t";
+    const x = isOutOfBound.right(bound, content, anchor) ? "l" : "r";
+    const y = isOutOfBound.bottomOnH(bound, content, anchor) ? "b" : "t";
     return `${x}${y}` as DirectionValues;
   },
   rc: (bound: DOMRect, content: DOMRect, anchor: DOMRect) => {
-    const x =
-      bound.width < anchor.x + anchor.width + content.width + window.scrollX
-        ? "l"
-        : "r";
-    const b =
-      bound.height <
-        anchor.y + (anchor.width + content.height) / 2 + window.scrollY && "b";
-    const t =
-      anchor.y - (anchor.width + content.height) / 2 + window.scrollY < 0 &&
-      "t";
+    const x = isOutOfBound.right(bound, content, anchor) ? "l" : "r";
+    const b = isOutOfBound.bottomOnVCenter(bound, content, anchor) && "b";
+    const t = isOutOfBound.topOnVCenter(bound, content, anchor) && "t";
     return `${x}${t || b || "c"}` as DirectionValues;
   },
   rb: (bound: DOMRect, content: DOMRect, anchor: DOMRect) => {
-    const x =
-      bound.width < anchor.x + anchor.width + content.width + window.scrollX
-        ? "l"
-        : "r";
-    const y =
-      anchor.y + anchor.height - content.height + window.scrollY < 0
-        ? "t"
-        : "b";
+    const x = isOutOfBound.right(bound, content, anchor) ? "l" : "r";
+    const y = isOutOfBound.topOnH(bound, content, anchor) ? "t" : "b";
     return `${x}${y}` as DirectionValues;
   },
   lt: (bound: DOMRect, content: DOMRect, anchor: DOMRect) => {
-    const x = anchor.x - content.width + window.scrollX < 0 ? "r" : "l";
-    const y =
-      bound.height < anchor.y + content.height + window.scrollY ? "b" : "t";
+    const x = isOutOfBound.left(bound, content, anchor) ? "r" : "l";
+    const y = isOutOfBound.bottomOnH(bound, content, anchor) ? "b" : "t";
     return `${x}${y}` as DirectionValues;
   },
   lc: (bound: DOMRect, content: DOMRect, anchor: DOMRect) => {
-    const x = anchor.x - content.width + window.scrollX < 0 ? "r" : "l";
-    const b =
-      bound.height <
-        anchor.y + (anchor.width + content.height) / 2 + window.scrollY && "b";
-    const t =
-      anchor.y - (anchor.width + content.height) / 2 + window.scrollY < 0 &&
-      "t";
+    const x = isOutOfBound.left(bound, content, anchor) ? "r" : "l";
+    const b = isOutOfBound.bottomOnVCenter(bound, content, anchor) && "b";
+    const t = isOutOfBound.topOnVCenter(bound, content, anchor) && "t";
     return `${x}${t || b || "c"}` as DirectionValues;
   },
-  lb: (_: DOMRect, content: DOMRect, anchor: DOMRect) => {
-    const x = anchor.x - content.width + window.scrollX < 0 ? "r" : "l";
-    const y =
-      anchor.y + anchor.height - content.height + window.scrollY < 0
-        ? "t"
-        : "b";
+  lb: (bound: DOMRect, content: DOMRect, anchor: DOMRect) => {
+    const x = isOutOfBound.left(bound, content, anchor) ? "r" : "l";
+    const y = isOutOfBound.topOnH(bound, content, anchor) ? "t" : "b";
     return `${x}${y}` as DirectionValues;
   },
 };
