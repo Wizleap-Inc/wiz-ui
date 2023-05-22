8264bcd5fe524691077d5da18b8af7906fc76ed9
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-05-22 15:29:54 +0900

    refactor(popup): 反射ロジックの切り出し

--- packages/wiz-ui-react/src/components/base/popup/utils/wrap.ts
+++ packages/wiz-ui-react/src/components/base/popup/utils/wrap.ts
@@ -29,72 +29,97 @@ const isOutOfBound = {
     anchor.y + (anchor.width + content.height) / 2 + window.scrollY,
 };
 
+const reflection = {
+  t_: (bound: DOMRect, content: DOMRect, anchor: DOMRect) =>
+    isOutOfBound.top(bound, content, anchor) ? "b" : "t",
+  b_: (bound: DOMRect, content: DOMRect, anchor: DOMRect) =>
+    isOutOfBound.bottom(bound, content, anchor) ? "t" : "b",
+  r_: (bound: DOMRect, content: DOMRect, anchor: DOMRect) =>
+    isOutOfBound.right(bound, content, anchor) ? "l" : "r",
+  l_: (bound: DOMRect, content: DOMRect, anchor: DOMRect) =>
+    isOutOfBound.left(bound, content, anchor) ? "r" : "l",
+  _l: (bound: DOMRect, content: DOMRect, anchor: DOMRect) =>
+    isOutOfBound.rightOnV(bound, content, anchor) ? "r" : "l",
+  _r: (bound: DOMRect, content: DOMRect, anchor: DOMRect) =>
+    isOutOfBound.leftOnV(bound, content, anchor) ? "l" : "r",
+  _t: (bound: DOMRect, content: DOMRect, anchor: DOMRect) =>
+    isOutOfBound.bottomOnH(bound, content, anchor) ? "b" : "t",
+  _b: (bound: DOMRect, content: DOMRect, anchor: DOMRect) =>
+    isOutOfBound.topOnH(bound, content, anchor) ? "t" : "b",
+  vc: (bound: DOMRect, content: DOMRect, anchor: DOMRect) => {
+    const r = isOutOfBound.rightOnHCenter(bound, content, anchor) && "r";
+    const l = isOutOfBound.leftOnHCenter(bound, content, anchor) && "l";
+    return l || r || "c";
+  },
+  hc: (bound: DOMRect, content: DOMRect, anchor: DOMRect) => {
+    const b = isOutOfBound.bottomOnVCenter(bound, content, anchor) && "b";
+    const t = isOutOfBound.topOnVCenter(bound, content, anchor) && "t";
+    return t || b || "c";
+  },
+};
+
 export const wrapDirection: Record<
   DirectionValues,
   (bound: DOMRect, content: DOMRect, anchor: DOMRect) => DirectionValues
 > = {
   bl: (bound: DOMRect, content: DOMRect, anchor: DOMRect) => {
-    const y = isOutOfBound.bottom(bound, content, anchor) ? "t" : "b";
-    const x = isOutOfBound.rightOnV(bound, content, anchor) ? "r" : "l";
+    const y = reflection.b_(bound, content, anchor);
+    const x = reflection._l(bound, content, anchor);
     return `${y}${x}` as DirectionValues;
   },
   bc: (bound: DOMRect, content: DOMRect, anchor: DOMRect) => {
-    const y = isOutOfBound.bottom(bound, content, anchor) ? "t" : "b";
-    const r = isOutOfBound.rightOnHCenter(bound, content, anchor) && "r";
-    const l = isOutOfBound.leftOnHCenter(bound, content, anchor) && "l";
-    return `${y}${l || r || "c"}` as DirectionValues;
+    const y = reflection.b_(bound, content, anchor);
+    const x = reflection.vc(bound, content, anchor);
+    return `${y}${x}` as DirectionValues;
   },
   br: (bound: DOMRect, content: DOMRect, anchor: DOMRect) => {
-    const y = isOutOfBound.bottom(bound, content, anchor) ? "t" : "b";
-    const x = isOutOfBound.leftOnV(bound, content, anchor) ? "l" : "r";
+    const y = reflection.b_(bound, content, anchor);
+    const x = reflection._r(bound, content, anchor);
     return `${y}${x}` as DirectionValues;
   },
   tl: (bound: DOMRect, content: DOMRect, anchor: DOMRect) => {
-    const y = isOutOfBound.top(bound, content, anchor) ? "b" : "t";
-    const x = isOutOfBound.rightOnV(bound, content, anchor) ? "r" : "l";
+    const y = reflection.t_(bound, content, anchor);
+    const x = reflection._l(bound, content, anchor);
     return `${y}${x}` as DirectionValues;
   },
   tc: (bound: DOMRect, content: DOMRect, anchor: DOMRect) => {
-    const y = isOutOfBound.top(bound, content, anchor) ? "b" : "t";
-    const r = isOutOfBound.rightOnHCenter(bound, content, anchor) && "r";
-    const l = isOutOfBound.leftOnHCenter(bound, content, anchor) && "l";
-    return `${y}${l || r || "c"}` as DirectionValues;
+    const y = reflection.t_(bound, content, anchor);
+    const x = reflection.vc(bound, content, anchor);
+    return `${y}${x}` as DirectionValues;
   },
   tr: (bound: DOMRect, content: DOMRect, anchor: DOMRect) => {
-    const y = isOutOfBound.top(bound, content, anchor) ? "b" : "t";
-    const x = isOutOfBound.leftOnV(bound, content, anchor) ? "l" : "r";
+    const y = reflection.t_(bound, content, anchor);
+    const x = reflection._r(bound, content, anchor);
     return `${y}${x}` as DirectionValues;
   },
   rt: (bound: DOMRect, content: DOMRect, anchor: DOMRect) => {
-    const x = isOutOfBound.right(bound, content, anchor) ? "l" : "r";
-    const y = isOutOfBound.bottomOnH(bound, content, anchor) ? "b" : "t";
+    const x = reflection.r_(bound, content, anchor);
+    const y = reflection._t(bound, content, anchor);
     return `${x}${y}` as DirectionValues;
   },
   rc: (bound: DOMRect, content: DOMRect, anchor: DOMRect) => {
-    const x = isOutOfBound.right(bound, content, anchor) ? "l" : "r";
-    const b = isOutOfBound.bottomOnVCenter(bound, content, anchor) && "b";
-    const t = isOutOfBound.topOnVCenter(bound, content, anchor) && "t";
-    return `${x}${t || b || "c"}` as DirectionValues;
+    const x = reflection.r_(bound, content, anchor);
+    const y = reflection.hc(bound, content, anchor);
+    return `${x}${y}` as DirectionValues;
   },
   rb: (bound: DOMRect, content: DOMRect, anchor: DOMRect) => {
-    const x = isOutOfBound.right(bound, content, anchor) ? "l" : "r";
-    const y = isOutOfBound.topOnH(bound, content, anchor) ? "t" : "b";
+    const x = reflection.r_(bound, content, anchor);
+    const y = reflection._b(bound, content, anchor);
     return `${x}${y}` as DirectionValues;
   },
   lt: (bound: DOMRect, content: DOMRect, anchor: DOMRect) => {
-    const x = isOutOfBound.left(bound, content, anchor) ? "r" : "l";
-    const y = isOutOfBound.bottomOnH(bound, content, anchor) ? "b" : "t";
+    const x = reflection.l_(bound, content, anchor);
+    const y = reflection._t(bound, content, anchor);
     return `${x}${y}` as DirectionValues;
   },
   lc: (bound: DOMRect, content: DOMRect, anchor: DOMRect) => {
-    const x = isOutOfBound.left(bound, content, anchor) ? "r" : "l";
-    const b = isOutOfBound.bottomOnVCenter(bound, content, anchor) && "b";
-    const t = isOutOfBound.topOnVCenter(bound, content, anchor) && "t";
-    return `${x}${t || b || "c"}` as DirectionValues;
+    const x = reflection.l_(bound, content, anchor);
+    const y = reflection.hc(bound, content, anchor);
+    return `${x}${y}` as DirectionValues;
   },
   lb: (bound: DOMRect, content: DOMRect, anchor: DOMRect) => {
-    const x = isOutOfBound.left(bound, content, anchor) ? "r" : "l";
-    const y = isOutOfBound.topOnH(bound, content, anchor) ? "t" : "b";
+    const x = reflection.l_(bound, content, anchor);
+    const y = reflection._b(bound, content, anchor);
     return `${x}${y}` as DirectionValues;
   },
 };
