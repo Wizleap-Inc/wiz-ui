1ad6d1761cedadb3d697b7a5d9fda2e70eff0aa2
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-05-24 03:15:48 +0900

    docs(gap): add story

--- packages/wiz-ui-react/src/components/base/popup/stories/popup.stories.tsx
+++ packages/wiz-ui-react/src/components/base/popup/stories/popup.stories.tsx
@@ -4,6 +4,7 @@ import { WizPopup } from "../components";
 
 import { DefaultStory } from "./default";
 import { GapStory } from "./gap";
+import { GapDirectionStory } from "./gap-direction";
 import { PlaygroundStory } from "./playground";
 
 const meta: Meta<typeof WizPopup> = {
@@ -17,6 +18,8 @@ type Story = StoryObj<typeof WizPopup>;
 
 export const Default: Story = DefaultStory;
 
-export const Gap: Story = GapStory;
+export const GapSize: Story = GapStory;
+
+export const GapDirection: Story = GapDirectionStory;
 
 export const Playground: Story = PlaygroundStory;
