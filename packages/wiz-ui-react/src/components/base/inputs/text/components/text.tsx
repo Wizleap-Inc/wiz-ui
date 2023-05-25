3e00a3c99c6a738cca17a6b2b0e86456b23e14e8
wmoai <wmoaism@gmail.com>
2023-05-25 14:27:32 +0900

    refactor: memoを外してforwardRef化

--- packages/wiz-ui-react/src/components/base/inputs/text/components/text.tsx
+++ packages/wiz-ui-react/src/components/base/inputs/text/components/text.tsx
@@ -1,6 +1,13 @@
+import { ComponentName } from "@wizleap-inc/wiz-ui-constants";
 import * as styles from "@wizleap-inc/wiz-ui-styles/bases/text-input.css";
 import clsx from "clsx";
-import { ChangeEventHandler, ComponentProps, memo, useContext } from "react";
+import {
+  ChangeEventHandler,
+  ComponentProps,
+  ForwardedRef,
+  forwardRef,
+  useContext,
+} from "react";
 
 import { TIcon } from "@/components";
 import { PrivateBaseInput } from "@/components/base/inputs/base";
@@ -13,44 +20,47 @@ export type Props = {
   onChangeValue?: (value: string) => void;
 } & Omit<PrivateBaseInputProps, "type">;
 
-const _TextInput = ({
-  icon: Icon,
-  onChangeValue,
-  onChange,
-  ...props
-}: Props) => {
-  const formControl = useContext(FormControlContext);
+const TextInput = forwardRef(
+  (
+    { icon: Icon, onChangeValue, onChange, ...props }: Props,
+    ref: ForwardedRef<HTMLInputElement>
+  ) => {
+    const formControl = useContext(FormControlContext);
 
-  const computedExpand = props.expand ? "expand" : "default";
-  const error = props.error ?? formControl.error;
+    const computedExpand = props.expand ? "expand" : "default";
+    const error = props.error ?? formControl.error;
 
-  const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-    onChange?.(e);
-    onChangeValue?.(e.target.value);
-  };
+    const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+      onChange?.(e);
+      onChangeValue?.(e.target.value);
+    };
 
-  return (
-    <div
-      className={clsx(
-        styles.textInputStyle,
-        styles.textInputExpandStyle[computedExpand]
-      )}
-    >
-      {Icon && (
-        <div className={styles.textInputIconStyle}>
-          <Icon />
-        </div>
-      )}
-      <PrivateBaseInput
-        {...props}
-        error={error}
-        type="text"
-        onChange={handleChange}
-        spaceType={Icon ? "left" : "none"}
-        autoComplete="autocomplete"
-      />
-    </div>
-  );
-};
+    return (
+      <div
+        className={clsx(
+          styles.textInputStyle,
+          styles.textInputExpandStyle[computedExpand]
+        )}
+      >
+        {Icon && (
+          <div className={styles.textInputIconStyle}>
+            <Icon />
+          </div>
+        )}
+        <PrivateBaseInput
+          {...props}
+          ref={ref}
+          error={error}
+          type="text"
+          onChange={handleChange}
+          spaceType={Icon ? "left" : "none"}
+          autoComplete="autocomplete"
+        />
+      </div>
+    );
+  }
+);
 
-export const WizTextInput = memo(_TextInput);
+TextInput.displayName = ComponentName.TextInput;
+
+export const WizTextInput = TextInput;
