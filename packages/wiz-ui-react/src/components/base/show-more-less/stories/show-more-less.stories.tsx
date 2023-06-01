0f73705c2416c0ec14c9fede4f72f702af74b146
shuhei.tachikawa <shuhei.tusx@gmail.com>
2023-06-01 10:02:08 +0900

    feat(show-more-less): add storybook

--- packages/wiz-ui-react/src/components/base/show-more-less/stories/show-more-less.stories.tsx
+++ packages/wiz-ui-react/src/components/base/show-more-less/stories/show-more-less.stories.tsx
@@ -1,21 +1,44 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+
 import { Meta, StoryObj } from "@storybook/react";
-// import { useState } from "react";
+import { COLOR_MAP_ACCESSORS } from "@wizleap-inc/wiz-ui-constants";
+import { useState } from "react";
 
 import { WizShowMoreLess } from "../components";
 
 const meta: Meta<typeof WizShowMoreLess> = {
   title: "Base/ShowMoreLess",
   component: WizShowMoreLess,
-  argTypes: {},
+  argTypes: {
+    isOpen: {
+      control: { type: "boolean" },
+    },
+    openMessage: {
+      control: { type: "text" },
+    },
+    closeMessage: {
+      control: { type: "text" },
+    },
+    width: {
+      control: { type: "text" },
+    },
+    bgColor: {
+      control: { type: "select" },
+      options: COLOR_MAP_ACCESSORS,
+    },
+    fontColor: {
+      control: { type: "select" },
+      options: COLOR_MAP_ACCESSORS,
+    },
+    onToggle: {
+      action: "onClick",
+    },
+  },
   parameters: {
     docs: {
       description: {
-        component: `
-### WizShowMoreLess
-
-Length分のページネーションを表示するコンポーネントです。
-currentPageが現在のページの番号を表します。
-初期値は1にするとよいです。`,
+        component:
+          "childrenに渡したDOMを開閉し、表示-非表示を制御するためのコンポーネントです。",
       },
     },
   },
@@ -24,13 +47,91 @@ currentPageが現在のページの番号を表します。
 export default meta;
 type Story = StoryObj<typeof WizShowMoreLess>;
 
+const children = (
+  <div>
+    <p>折りたたまれている部分です．</p>
+    <p>折りたたまれている部分です．</p>
+    <p>折りたたまれている部分です．</p>
+  </div>
+);
+
 export const Default: Story = {
   args: {
-    length: 10,
+    children,
   },
   render: (args) => {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    // const [currentPage, setCurrentPage] = useState(1);
-    return <WizShowMoreLess {...args} />;
+    const [isOpen, setIsOpen] = useState(false);
+    return (
+      <WizShowMoreLess
+        {...args}
+        isOpen={isOpen}
+        onToggle={() => setIsOpen((prev) => !prev)}
+      />
+    );
+  },
+};
+
+export const Color: Story = {
+  args: {
+    children,
+    bgColor: "gray.300",
+    fontColor: "green.800",
+  },
+  render: (args) => {
+    const [isOpen, setIsOpen] = useState(false);
+    return (
+      <WizShowMoreLess
+        {...args}
+        isOpen={isOpen}
+        onToggle={() => setIsOpen((prev) => !prev)}
+      />
+    );
+  },
+};
+
+export const Open: Story = {
+  args: {
+    children,
+    isOpen: true,
+  },
+  render: (args) => {
+    const [isOpen, setIsOpen] = useState(args.isOpen);
+    return (
+      <WizShowMoreLess
+        {...args}
+        isOpen={isOpen}
+        onToggle={() => setIsOpen((prev) => !prev)}
+      />
+    );
+  },
+};
+
+export const Multiple: Story = {
+  args: {
+    children,
+  },
+  render: (args) => {
+    const [isOpen1, setIsOpen1] = useState(true);
+    const [isOpen2, setIsOpen2] = useState(false);
+    const [isOpen3, setIsOpen3] = useState(false);
+    return (
+      <>
+        <WizShowMoreLess
+          {...args}
+          isOpen={isOpen1}
+          onToggle={() => setIsOpen1((prev) => !prev)}
+        />
+        <WizShowMoreLess
+          {...args}
+          isOpen={isOpen2}
+          onToggle={() => setIsOpen2((prev) => !prev)}
+        />
+        <WizShowMoreLess
+          {...args}
+          isOpen={isOpen3}
+          onToggle={() => setIsOpen3((prev) => !prev)}
+        />
+      </>
+    );
   },
 };
