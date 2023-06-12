19eef41d2a2194105263470b8c3d70b506129484
shuhei.tachikawa <shuhei.tusx@gmail.com>
2023-06-12 11:28:59 +0900

    Feat(WizNavigation): コメント追加

--- packages/wiz-ui-react/src/components/base/navigation/components/item.tsx
+++ packages/wiz-ui-react/src/components/base/navigation/components/item.tsx
@@ -2,6 +2,7 @@ import { ComponentName } from "@wizleap-inc/wiz-ui-constants";
 import { FC } from "react";
 
 const Item: FC = () => {
+  // WizToolTip実装後に対応
   return <div className=""></div>;
 };
 
