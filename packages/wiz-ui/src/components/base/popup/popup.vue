4d118f3929a7be4fc96c64f85f3a60f77468176c
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-06-13 19:03:07 +0900

    fix(popup): null checkではない

--- packages/wiz-ui/src/components/base/popup/popup.vue
+++ packages/wiz-ui/src/components/base/popup/popup.vue
@@ -348,7 +348,9 @@ const isFixed = computed(() => {
 });
 
 const inset = computed(() => {
-  const { scrollX, scrollY } = isFixed ? { scrollX: 0, scrollY: 0 } : window;
+  const { scrollX, scrollY } = isFixed.value
+    ? { scrollX: 0, scrollY: 0 }
+    : window;
   const firstBTop = bodyPxInfo.top + scrollY + bodyPxInfo.height;
   const secondBTop =
     bodyPxInfo.top + scrollY - popupRect.value.height + bodyPxInfo.height;
