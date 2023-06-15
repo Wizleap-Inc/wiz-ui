b6470dac2b0b07bac89191969becdd19d9578749
shuhei.tachikawa <shuhei.tusx@gmail.com>
2023-06-16 08:26:45 +0900

    feat(WizNavigation): Itemのベース実装

--- packages/wiz-ui-react/src/components/base/navigation/components/item.tsx
+++ packages/wiz-ui-react/src/components/base/navigation/components/item.tsx
@@ -1,8 +1,66 @@
 import { ComponentName } from "@wizleap-inc/wiz-ui-constants";
-import { FC } from "react";
+import { FC, useRef } from "react";
 
-const Item: FC = () => {
-  return <div className=""></div>;
+import { TIcon, WizTooltip } from "@/components";
+
+import { ButtonGroupItem } from "../../popup-button-group/types";
+
+interface Props {
+  icon: TIcon;
+  label: string;
+  active: boolean;
+  href: string;
+  disabled?: boolean;
+  tooltipText?: string;
+  lockingPopup?: boolean;
+  buttons?: ButtonGroupItem[];
+  isOpen?: boolean;
+  onToggle: (isOpen: boolean) => void;
+  setLock: (lock: boolean) => void;
+}
+
+const Item: FC<Props> = ({
+  icon,
+  label,
+  active,
+  href,
+  disabled = false,
+  tooltipText = null,
+  lockingPopup = true,
+  buttons,
+  isOpen,
+  onToggle,
+  setLock,
+}) => {
+  const navItemRef = useRef<HTMLDivElement>(null);
+
+  const existPopup = buttons && buttons.length > 0;
+
+  const navItemOnClick = () => {
+    onToggle(true);
+    if (existPopup) setLock(true);
+  };
+
+  const navItemMouseEnter = () => {
+    if (!lockingPopup) onToggle(true);
+  };
+
+  const navItemMouseLeave = () => {
+    if (!lockingPopup) onToggle(false);
+  };
+
+  return (
+    <WizTooltip content={<div>{tooltipText}</div>}>
+      <div
+        ref={navItemRef}
+        onClick={navItemOnClick}
+        onMouseEnter={navItemMouseEnter}
+        onMouseLeave={navItemMouseLeave}
+      >
+        <a href={href}></a>
+      </div>
+    </WizTooltip>
+  );
 };
 Item.displayName = ComponentName.NavigationItem;
 
