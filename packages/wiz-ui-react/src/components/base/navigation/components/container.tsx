6bed8727c0f1563f520cf2b42ca9087484be3427
shuhei.tachikawa <shuhei.tusx@gmail.com>
2023-06-18 08:07:26 +0900

    feat(WizNavigation): itemのcontainer追加

--- packages/wiz-ui-react/src/components/base/navigation/components/container.tsx
+++ packages/wiz-ui-react/src/components/base/navigation/components/container.tsx
@@ -1,8 +1,54 @@
-import { ComponentName } from "@wizleap-inc/wiz-ui-constants";
-import { FC } from "react";
+import { ComponentName, THEME } from "@wizleap-inc/wiz-ui-constants";
+import {
+  navigationContainerFooterStyle,
+  navigationContainerItemsStyle,
+  navigationContainerStyle,
+} from "@wizleap-inc/wiz-ui-styles/bases/navigation.css";
+import { CSSProperties, FC, ReactNode } from "react";
 
-const Container: FC = () => {
-  return <div className=""></div>;
+interface Props {
+  isOpen: boolean;
+  width?: string;
+  sticky?: boolean;
+  children: ReactNode;
+  footer?: ReactNode;
+}
+
+const Container: FC<Props> = ({
+  isOpen = true,
+  width,
+  sticky = false,
+  children,
+  footer,
+}) => {
+  const widthStyle = () => {
+    if (width) return width;
+    if (isOpen) return "180px";
+    return `calc(${THEME.spacing.xl} * 2 + ${THEME.spacing.sm})`;
+  };
+
+  const stickyStyle = () => {
+    const styles: CSSProperties = {};
+    if (sticky) styles.position = "sticky";
+    if (sticky) styles.top = `calc(${THEME.share.HEADER_HEIGHT} + 1px)`; // +1px for border of header
+    if (sticky) styles.left = 0;
+    if (sticky) styles.borderRight = `1px solid ${THEME.color.gray[400]}`;
+    if (sticky)
+      styles.height = `calc(100vh - ${THEME.share.HEADER_HEIGHT} - 1px)`;
+    return styles;
+  };
+
+  return (
+    <div
+      className={navigationContainerStyle}
+      style={{ ...stickyStyle(), width: widthStyle() }}
+    >
+      <div className={navigationContainerItemsStyle}>{children}</div>
+      {isOpen && footer && (
+        <div className={navigationContainerFooterStyle}>{footer}</div>
+      )}
+    </div>
+  );
 };
 Container.displayName = ComponentName.NavigationContainer;
 
--- packages/wiz-ui-react/src/components/base/navigation/stories/container.stories.tsx
+++ packages/wiz-ui-react/src/components/base/navigation/stories/container.stories.tsx
@@ -1,6 +1,16 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import { Meta, StoryObj } from "@storybook/react";
+import { useState } from "react";
 
-import { WizNavigationContainer } from "../components";
+import {
+  WizIAssignment,
+  WizIBusinessCenter,
+  WizIDashboard,
+  WizIHelp,
+} from "@/components/icons";
+
+import { ButtonGroupItem } from "../../popup-button-group/types";
+import { WizNavigationContainer, WizNavigationItem } from "../components";
 
 const meta: Meta<typeof WizNavigationContainer> = {
   title: "Base/Navigation/Container",
@@ -11,7 +21,159 @@ const meta: Meta<typeof WizNavigationContainer> = {
 export default meta;
 type Story = StoryObj<typeof WizNavigationContainer>;
 
-export const Default: Story = {
+const handleToggle = () => void 0;
+const setLock = () => void 0;
+
+const paths = [
+  {
+    icon: WizIDashboard,
+    label: "Home",
+    href: "/",
+  },
+  {
+    icon: WizIAssignment,
+    label: "Page1",
+    href: "/page1",
+  },
+  {
+    icon: WizIBusinessCenter,
+    label: "Page2",
+    href: "/page2",
+  },
+  {
+    icon: WizIHelp,
+    label: "Page3",
+    href: "/page3",
+  },
+];
+
+const Template: Story = {
+  render: (args) => (
+    <div style={{ display: "flex", height: "100vh" }}>
+      <WizNavigationContainer {...args}>
+        {paths.map((path) => (
+          <WizNavigationItem
+            key={`path-${path.label}`}
+            icon={path.icon}
+            label={path.label}
+            href={path.href}
+            active={false}
+            onToggle={handleToggle}
+            setLock={setLock}
+          />
+        ))}
+      </WizNavigationContainer>
+    </div>
+  ),
+};
+
+export const Overview: Story = {
+  ...Template,
   args: {},
-  render: (args) => <WizNavigationContainer {...args} />,
+};
+
+export const WithFooter: Story = {
+  ...Template,
+  args: {
+    footer: (
+      <div>
+        <h4>Footer</h4>
+        <h5>Footer</h5>
+        <h6>Footer</h6>
+      </div>
+    ),
+  },
+};
+
+export const Close: Story = {
+  ...Template,
+  args: {
+    isOpen: false,
+  },
+};
+
+export const Playground: Story = {
+  args: {},
+  render: (args) => {
+    const [isMenuOpen, setIsMenuOpen] = useState(args.isOpen);
+    const handleToggle = () => setIsMenuOpen((prev) => !prev);
+    return (
+      <div style={{ display: "flex", height: "100vh" }}>
+        <WizNavigationContainer {...args} isOpen={isMenuOpen}>
+          {paths.map((path) => (
+            <WizNavigationItem
+              key={`path-${path.label}`}
+              icon={path.icon}
+              label={path.label}
+              href={path.href}
+              active={false}
+              onToggle={handleToggle}
+              setLock={setLock}
+            />
+          ))}
+        </WizNavigationContainer>
+        <div>
+          デバッグ用
+          <button onClick={handleToggle}>
+            {isMenuOpen ? "Close" : "Open"}
+          </button>
+        </div>
+      </div>
+    );
+  },
+};
+
+export const Fixed: Story = {
+  ...Template,
+  args: {
+    sticky: true,
+    footer: (
+      <div>
+        <h4>Footer</h4>
+        <h5>Footer</h5>
+        <h6>Footer</h6>
+      </div>
+    ),
+  },
+};
+
+const createButton = (n: number): ButtonGroupItem => ({
+  kind: "button",
+  option: {
+    label: `label ${n}`,
+    value: n,
+    onClick: () => void 0,
+  },
+});
+
+const PopupTemplate: Story = {
+  render: (args) => {
+    const [isPopupOpen, setIsPopupOpen] = useState(true);
+    return (
+      <div style={{ display: "flex", height: "100vh" }}>
+        <WizNavigationContainer {...args}>
+          {paths.map((path, i) => (
+            <WizNavigationItem
+              key={`path-${path.label}`}
+              icon={path.icon}
+              label={path.label}
+              href={path.href}
+              active={false}
+              onToggle={handleToggle}
+              setLock={setLock}
+              buttons={[createButton(i + 1)]}
+              isOpen={isPopupOpen}
+            />
+          ))}
+        </WizNavigationContainer>
+      </div>
+    );
+  },
+};
+
+export const Popup: Story = {
+  ...PopupTemplate,
+  args: {
+    isOpen: true,
+  },
 };
