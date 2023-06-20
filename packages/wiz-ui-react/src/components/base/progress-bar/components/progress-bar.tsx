eb4b931b14a6992bdc475272491f380607e4a03e
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-06-20 16:26:51 +0900

    feat(progress-bar): tooltip対応

--- packages/wiz-ui-react/src/components/base/progress-bar/components/progress-bar.tsx
+++ packages/wiz-ui-react/src/components/base/progress-bar/components/progress-bar.tsx
@@ -2,6 +2,8 @@ import { ComponentName } from "@wizleap-inc/wiz-ui-constants";
 import * as styles from "@wizleap-inc/wiz-ui-styles/bases/progress-bar.css";
 import { ReactNode } from "react";
 
+import { WizText, WizTooltip } from "@/components";
+
 import { WizProgressPoint } from "./progress-point";
 import { ProgressItem } from "./types";
 
@@ -11,28 +13,40 @@ type Props = {
   contents: ProgressItem[];
   children?: ReactNode;
 };
+
+const Progress = (content: ProgressItem) => (
+  <div className={styles.progressBarPointWrapperStyle}>
+    <span className={styles.progressBarItemAnnotationStyle}>
+      <WizText fontSize="xs2" color="red.800" whiteSpace="nowrap">
+        {content.annotation}
+      </WizText>
+    </span>
+    <WizProgressPoint status={content.status} value={content.value} />
+    <span className={styles.progressBarItemLabelStyle}>
+      <WizText fontSize="xs2" color="gray.600" whiteSpace="nowrap">
+        {content.label}
+      </WizText>
+    </span>
+  </div>
+);
+
 const ProgressBar = ({ ...props }: Props) => {
   return (
     <div className={styles.progressBarStyle}>
       {props.contents.map((content, index) => (
-        // WizTooltip
         <div
           className={
             styles.progressBarItemStyle[index === 0 ? "first" : "default"]
           }
           key={content.id}
         >
-          <div className={styles.progressBarPointWrapperStyle}>
-            <span className={styles.progressBarItemAnnotationStyle}>
-              {/*<WizText fontSize="xs2" color="red.800" whiteSpace="nowrap"> */}
-              {content.annotation}
-            </span>
-            <WizProgressPoint status={content.status} value={content.value} />
-            <span className={styles.progressBarItemLabelStyle}>
-              {/* <WizText fontSize="xs2" color="gray.600" whiteSpace="nowrap"> */}
-              {content.label}
-            </span>
-          </div>
+          {content.tooltip ? (
+            <WizTooltip content={<>{content.tooltip}</>}>
+              {Progress(content)}
+            </WizTooltip>
+          ) : (
+            Progress(content)
+          )}
           <WizProgressLine active={content.progress} isFirst={index === 0} />
         </div>
       ))}
