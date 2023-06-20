cbae6f7de8d90c21cb7818d1482f0f218e4caab6
shuhei.tachikawa <shuhei.tusx@gmail.com>
2023-06-20 11:22:36 +0900

    fix(WizNavigation): containerリファクタ

--- packages/wiz-ui-react/src/components/base/navigation/components/container.tsx
+++ packages/wiz-ui-react/src/components/base/navigation/components/container.tsx
@@ -21,27 +21,24 @@ const Container: FC<Props> = ({
   children,
   footer,
 }) => {
-  const widthStyle = () => {
-    if (width) return width;
-    if (isOpen) return "180px";
-    return `calc(${THEME.spacing.xl} * 2 + ${THEME.spacing.sm})`;
-  };
+  const widthStyle =
+    width ??
+    (isOpen ? "180px" : `calc(${THEME.spacing.xl} * 2 + ${THEME.spacing.sm})`);
 
-  const stickyStyle = () => {
-    const styles: CSSProperties = {};
-    if (sticky) styles.position = "sticky";
-    if (sticky) styles.top = `calc(${THEME.share.HEADER_HEIGHT} + 1px)`; // +1px for border of header
-    if (sticky) styles.left = 0;
-    if (sticky) styles.borderRight = `1px solid ${THEME.color.gray[400]}`;
-    if (sticky)
-      styles.height = `calc(100vh - ${THEME.share.HEADER_HEIGHT} - 1px)`;
-    return styles;
-  };
+  const stickyStyle: CSSProperties = sticky
+    ? {
+        position: "sticky",
+        top: `calc(${THEME.share.HEADER_HEIGHT} + 1px)`,
+        left: 0,
+        borderRight: `1px solid ${THEME.color.gray[400]}`,
+        height: `calc(100vh - ${THEME.share.HEADER_HEIGHT} - 1px)`,
+      }
+    : {};
 
   return (
     <div
       className={navigationContainerStyle}
-      style={{ ...stickyStyle(), width: widthStyle() }}
+      style={{ ...stickyStyle, width: widthStyle }}
     >
       <div className={navigationContainerItemsStyle}>{children}</div>
       {isOpen && footer && (
