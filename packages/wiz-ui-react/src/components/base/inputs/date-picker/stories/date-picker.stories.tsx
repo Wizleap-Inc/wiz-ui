a4c2754ca437dc4f60309943d2931d2ce2fad12e
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-06-27 11:35:30 +0900

    docs(date-picker): add stories

--- packages/wiz-ui-react/src/components/base/inputs/date-picker/stories/date-picker.stories.tsx
+++ packages/wiz-ui-react/src/components/base/inputs/date-picker/stories/date-picker.stories.tsx
@@ -1,5 +1,7 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import { Meta, StoryObj } from "@storybook/react";
+import { userEvent, within } from "@storybook/testing-library";
+import { ARIA_LABELS } from "@wizleap-inc/wiz-ui-constants";
 import { useState } from "react";
 
 import { WizDatePicker } from "../components";
@@ -14,10 +16,66 @@ const meta: Meta<typeof WizDatePicker> = {
 export default meta;
 type Story = StoryObj<typeof WizDatePicker>;
 
+export const Default: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    canvas.getByLabelText(ARIA_LABELS.DATE_PICKER_INPUT).click();
+  },
+};
+
+export const Placeholder: Story = {
+  args: {
+    placeholder: "(例) 2000/1/1",
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    canvas.getByLabelText(ARIA_LABELS.DATE_PICKER_INPUT).click();
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    canvas.getByLabelText(ARIA_LABELS.DATE_PICKER_INPUT).click();
+  },
+};
+
+export const WithValue: Story = {
+  args: {
+    date: new Date(2023, 0, 1),
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    canvas.getByLabelText(ARIA_LABELS.DATE_PICKER_INPUT).click();
+  },
+};
+
+export const Hover: Story = {
+  args: {
+    date: new Date(2023, 0, 1),
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    userEvent.hover(canvas.getByLabelText(ARIA_LABELS.DATE_PICKER_INPUT));
+  },
+};
+
+export const IsDirectionFixed: Story = {
+  args: {
+    isDirectionFixed: true,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    canvas.getByLabelText(ARIA_LABELS.DATE_PICKER_INPUT).click();
+  },
+};
+
 export const Playground: Story = {
   render: (args) => {
     const [date, setDate] = useState<Date | null>(null);
-
     return <WizDatePicker {...args} date={date} onClickDate={setDate} />;
   },
 };
