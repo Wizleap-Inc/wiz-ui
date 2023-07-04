e40be929c433112c6c998b05bdd983fa2d905456
sor4chi <sorakawa0218@gmail.com>
2023-07-04 12:55:18 +0900

    feat(example-vue3): vue3のexampleにbreakpointの上書きの仕様を説明するページを作成

---
+++ examples/vue3/src/pages/breakpoint/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <WizHStack p="md">
+    <WizBox bg-color="blue.500" width="500px" height="100px">
+      BREAKPOINT = {{ bp }}
+      <DisplayBreakpoint />
+      <ReProvideBreakpoint />
+    </WizBox>
+  </WizHStack>
+</template>
+
+<script setup lang="ts">
+import { provideBreakpoint, WizBox, WizHStack } from "@wizleap-inc/wiz-ui-next";
+
+import DisplayBreakpoint from "./_components/DisplayBreakpoint.vue";
+import ReProvideBreakpoint from "./_components/ReProvideBreakpoint.vue";
+
+const bp = {
+  sm: 10000,
+  md: 10000,
+  lg: 10000,
+};
+
+provideBreakpoint(bp);
+</script>
