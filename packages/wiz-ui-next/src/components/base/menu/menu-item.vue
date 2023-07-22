b90f8cdaa8ff90c2ae0a45ff8af6e1e96d93194e
okakyo <0622okakyo@gmail.com>
2023-07-23 01:01:57 +0900

    [#935] feat: change color

--- packages/wiz-ui-next/src/components/base/menu/menu-item.vue
+++ packages/wiz-ui-next/src/components/base/menu/menu-item.vue
@@ -92,6 +92,7 @@ const componentStatus = computed(() => {
   if (props.selected) return "selected";
   if (props.active || isPressed.value) return "active";
   if (isHover.value) return "hover";
+  if (props.selected) return "selected";
   return "default";
 });
 </script>
