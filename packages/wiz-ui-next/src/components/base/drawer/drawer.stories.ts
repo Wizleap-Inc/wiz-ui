8c38d7c4efe9af8409a0b1c405317bece3d1e477
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-08-01 11:19:48 +0900

    docs(drawer): add with-header story

--- packages/wiz-ui-next/src/components/base/drawer/drawer.stories.ts
+++ packages/wiz-ui-next/src/components/base/drawer/drawer.stories.ts
@@ -1,4 +1,6 @@
 import { Meta, StoryFn } from "@storybook/vue3";
+import { THEME } from "@wizleap-inc/wiz-ui-constants";
+import { provide } from "vue";
 
 import { WizDrawer, WizHeader, WizIconButton, WizNavItem } from "@/components";
 import {
@@ -9,11 +11,20 @@ import {
   WizIHelp,
   WizIMenu,
 } from "@/components/icons";
+import { globalKey, useGlobalProvider } from "@/hooks/use-global-provider";
 
 export default {
   title: "Base/Drawer",
   component: WizDrawer,
   argTypes: {},
+  decorators: [
+    () => ({
+      setup() {
+        provide(globalKey, useGlobalProvider());
+      },
+      template: `<story />`,
+    }),
+  ],
   parameters: {
     layout: "fullscreen",
   },
@@ -37,8 +48,47 @@ const Template: StoryFn<typeof WizDrawer> = (args) => ({
       WizICancel,
     };
   },
+  template: ` 
+    <WizDrawer v-bind="args">      
+      <WizNavItem :icon="WizIDashboard" label="Home" to="/" :active="true" />
+      <WizNavItem :icon="WizIAssignment" label="Page1" to="/page1" :active="false" />
+      <WizNavItem :icon="WizIBusinessCenter" label="Page2" to="/page2" :active="false" />
+      <WizNavItem :icon="WizIHelp" label="Page3" to="/page3" :active="false" />
+      <template #footer>
+        <h4>Footer</h4>
+        <h5>Footer</h5>
+        <h6>Footer</h6>
+      </template>
+    </WizDrawer>  
+  `,
+});
+
+export const Default = Template.bind({});
+Default.args = {
+  isOpen: true,
+};
+
+export const WithHeader: StoryFn<typeof WizDrawer> = (args) => ({
+  components: {
+    WizDrawer,
+    WizNavItem,
+    WizHeader,
+  },
+  setup: () => {
+    return {
+      args,
+      WizIDashboard,
+      WizIAssignment,
+      WizIBusinessCenter,
+      WizIHelp,
+      WizIconButton,
+      WizIMenu,
+      WizICancel,
+    };
+  },
   template: `
-    <div> 
+    <div style="height: 100vh"> 
+      <WizHeader />
       <WizDrawer v-bind="args">      
         <WizNavItem :icon="WizIDashboard" label="Home" to="/" :active="true" />
         <WizNavItem :icon="WizIAssignment" label="Page1" to="/page1" :active="false" />
@@ -49,17 +99,12 @@ const Template: StoryFn<typeof WizDrawer> = (args) => ({
           <h5>Footer</h5>
           <h6>Footer</h6>
         </template>
-      </WizDrawer>
-        <WizIconButton 
-          :icon="WizIMenu "
-          size="lg"
-          variant="transparent" 
-        /> 
+      </WizDrawer> 
     </div>
   `,
 });
 
-export const Default = Template.bind({});
-Default.args = {
+WithHeader.args = {
   isOpen: true,
+  offsetHeight: THEME.share.HEADER_HEIGHT,
 };
--- packages/wiz-ui/src/components/base/drawer/drawer.stories.ts
+++ packages/wiz-ui/src/components/base/drawer/drawer.stories.ts
@@ -1,17 +1,28 @@
 import { Meta, StoryFn } from "@storybook/vue";
+import { THEME } from "@wizleap-inc/wiz-ui-constants";
+import { provide } from "vue";
 
-import { WizDrawer, WizNavItem } from "@/components";
+import { WizDrawer, WizHeader, WizNavItem } from "@/components";
 import {
   WizIAssignment,
   WizIBusinessCenter,
   WizIDashboard,
   WizIHelp,
 } from "@/components/icons";
+import { globalKey, useGlobalProvider } from "@/hooks/use-global-provider";
 
 export default {
   title: "Base/Drawer",
   component: WizDrawer,
   argTypes: {},
+  decorators: [
+    () => ({
+      setup() {
+        provide(globalKey, useGlobalProvider());
+      },
+      template: `<story />`,
+    }),
+  ],
   parameters: {
     layout: "fullscreen",
   },
@@ -22,6 +33,42 @@ const Template: StoryFn<typeof WizDrawer> = (_, { argTypes }) => ({
   components: {
     WizDrawer,
     WizNavItem,
+    WizHeader,
+  },
+  setup: () => {
+    return {
+      WizIDashboard,
+      WizIAssignment,
+      WizIBusinessCenter,
+      WizIHelp,
+    };
+  },
+  template: `  
+    <WizDrawer v-bind="$props">      
+      <WizNavItem :icon="WizIDashboard" label="Home" to="/" :active="true" />
+      <WizNavItem :icon="WizIAssignment" label="Page1" to="/page1" :active="false" />
+      <WizNavItem :icon="WizIBusinessCenter" label="Page2" to="/page2" :active="false" />
+      <WizNavItem :icon="WizIHelp" label="Page3" to="/page3" :active="false" />
+      <template #footer>
+        <h4>Footer</h4>
+        <h5>Footer</h5>
+        <h6>Footer</h6>
+      </template>
+    </WizDrawer> 
+  `,
+});
+
+export const Default = Template.bind({});
+Default.args = {
+  isOpen: true,
+};
+
+export const WithHeader: StoryFn<typeof WizDrawer> = (_, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: {
+    WizDrawer,
+    WizNavItem,
+    WizHeader,
   },
   setup: () => {
     return {
@@ -32,7 +79,8 @@ const Template: StoryFn<typeof WizDrawer> = (_, { argTypes }) => ({
     };
   },
   template: ` 
-   <div>
+   <div style="height: 100vh"> 
+      <WizHeader />
       <WizDrawer v-bind="$props">      
         <WizNavItem :icon="WizIDashboard" label="Home" to="/" :active="true" />
         <WizNavItem :icon="WizIAssignment" label="Page1" to="/page1" :active="false" />
@@ -48,7 +96,7 @@ const Template: StoryFn<typeof WizDrawer> = (_, { argTypes }) => ({
   `,
 });
 
-export const Default = Template.bind({});
-Default.args = {
+WithHeader.args = {
   isOpen: true,
+  offsetHeight: THEME.share.HEADER_HEIGHT,
 };
