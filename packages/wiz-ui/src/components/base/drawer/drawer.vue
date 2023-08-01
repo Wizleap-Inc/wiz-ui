6aaec038ccee11b763131117fc9a4bdb10930fba
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-08-01 11:19:13 +0900

    feat(drawer): offset height

--- packages/wiz-ui/src/components/base/drawer/drawer.vue
+++ packages/wiz-ui/src/components/base/drawer/drawer.vue
@@ -5,6 +5,7 @@
     :style="{
       position: 'absolute',
       width: '100%',
+      height: `calc(${height}px - ${offsetHeight})`,
       display: isActuallyOpen ? undefined : 'none',
     }"
   >
@@ -20,7 +21,7 @@
 <script setup lang="ts">
 import { ComponentName } from "@wizleap-inc/wiz-ui-constants";
 import * as styles from "@wizleap-inc/wiz-ui-styles/bases/drawer.css";
-import { ref, useSlots, watch } from "vue";
+import { onMounted, onUnmounted, ref, useSlots, watch } from "vue";
 
 defineOptions({
   name: ComponentName.Drawer,
@@ -31,10 +32,32 @@ const props = defineProps({
     type: Boolean,
     required: true,
   },
+  /**
+   * オフセットを指定してdrawerの高さを調整します。
+   * @type {string}
+   * @default 0px
+   * @example THEME.share.HEADER_HEIGHT
+   */
+  offsetHeight: {
+    type: String,
+    required: false,
+    default: "0px",
+  },
 });
 
 const slots = useSlots();
 const containerRef = ref<HTMLElement | null>(null);
+const height = ref(document.documentElement.clientHeight);
+const updateHeight = () => {
+  height.value = document.documentElement.clientHeight;
+};
+onMounted(() => {
+  window.addEventListener("resize", updateHeight);
+});
+onUnmounted(() => {
+  window.removeEventListener("resize", updateHeight);
+});
+
 const isActuallyOpen = ref<boolean>(props.isOpen);
 watch(props, () => {
   if (props.isOpen) {
