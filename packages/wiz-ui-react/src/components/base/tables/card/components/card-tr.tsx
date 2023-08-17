be8203935b0a3fedc4df8c24bd608dd76590835b
ichi-h <85932406+ichi-h@users.noreply.github.com>
2023-08-17 11:39:44 +0900

    feat: wiz-ui-reactのsafari対応

--- packages/wiz-ui-react/src/components/base/tables/card/components/card-tr.tsx
+++ packages/wiz-ui-react/src/components/base/tables/card/components/card-tr.tsx
@@ -2,15 +2,23 @@ import { ComponentName } from "@wizleap-inc/wiz-ui-constants";
 import * as styles from "@wizleap-inc/wiz-ui-styles/bases/card-table.css";
 import { ComponentProps, ReactNode } from "react";
 
+import { useIsSafari } from "../hooks/use-is-safari";
+
 type Props = {
   children?: ReactNode;
 } & ComponentProps<"tr">;
 
-const CardTr = ({ children, ...props }: Props) => (
-  <tr className={styles.cardTrStyle} {...props}>
-    {children}
-  </tr>
-);
+const CardTr = ({ children, ...props }: Props) => {
+  const isSafari = useIsSafari();
+  return (
+    <tr
+      className={isSafari ? styles.cardTrOnSafariStyle : styles.cardTrStyle}
+      {...props}
+    >
+      {children}
+    </tr>
+  );
+};
 
 CardTr.displayName = ComponentName.CardTr;
 
