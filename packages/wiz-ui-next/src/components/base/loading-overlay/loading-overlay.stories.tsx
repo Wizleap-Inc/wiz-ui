e3830a8bde14fa8d813dfd4c6807851564c86d98
sor4chi <sorakawa0218@gmail.com>
2023-08-25 01:51:06 +0900

    feat(vue3): loading overlayコンポーネントを作成

---
+++ packages/wiz-ui-next/src/components/base/loading-overlay/loading-overlay.stories.tsx
@@ -0,0 +1,43 @@
+import { StoryFn } from "@storybook/vue3";
+
+import { WizLoadingOverlay } from ".";
+
+export default {
+  title: "Base/LoadingOverlay",
+  component: WizLoadingOverlay,
+  argTypes: {
+    isLoading: {
+      control: {
+        type: "boolean",
+      },
+    },
+  },
+};
+const Template: StoryFn = (_, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { WizLoadingOverlay },
+  template: `<WizLoadingOverlay v-bind="$props" />`,
+});
+
+export const Default = Template.bind({});
+Default.args = {
+  isLoading: true,
+};
+Default.parameters = {
+  docs: {
+    description: {
+      story: `
+LoadingOverlay は、画面のロード中に表示されるオーバーレイです。
+
+position: absoluteで実装されているので、直近のrelativeな要素に対して表示されます。そのためbody直下に配置すれば、画面全体を覆うことができます。
+      `.trim(),
+    },
+    source: {
+      code: `
+<template>
+  <WizLoadingOverlay isLoading />
+</template>
+      `,
+    },
+  },
+};
