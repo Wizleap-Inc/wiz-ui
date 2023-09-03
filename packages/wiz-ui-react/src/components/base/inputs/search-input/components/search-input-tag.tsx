742da4b9a038116f5c1101e71bd51608480a122e
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-09-03 23:55:44 +0900

    fix(search-input): remove search-input-tag

--- packages/wiz-ui-react/src/components/base/inputs/search-input/components/search-input-tag.tsx
+++
@@ -1,8 +0,0 @@
-import * as styles from "@wizleap-inc/wiz-ui-styles/bases/search-input.css";
-import { FC } from "react";
-
-import { Tag } from "./types";
-
-export const SearchInputTag: FC<Tag> = ({ label }) => {
-  return <div className={styles.searchInputTagStyle}>{label}</div>;
-};
