6c26f188d58dd53ec195e46d190ddcf497f9c653
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-11-07 15:53:11 +0900

    docs(toggle-switch): update default story

--- packages/wiz-ui-react/src/components/base/inputs/toggle-switch/stories/toggle-switch.stories.tsx
+++ packages/wiz-ui-react/src/components/base/inputs/toggle-switch/stories/toggle-switch.stories.tsx
@@ -24,11 +24,14 @@ export const Default: Story = {
   args: {},
   render: (args) => {
     // eslint-disable-next-line react-hooks/rules-of-hooks
-    const [value, setValue] = useState(false);
+    const [value1, setValue1] = useState(false);
+    const [value2, setValue2] = useState(true);
     return (
       <>
-        <p>入力値: {value ? "true" : "false"}</p>
-        <WizToggleSwitch {...args} value={value} setValue={setValue} />
+        <p>入力値: {value1 ? "true" : "false"}</p>
+        <WizToggleSwitch {...args} value={value1} setValue={setValue1} />
+        <p>入力値: {value2 ? "true" : "false"}</p>
+        <WizToggleSwitch {...args} value={value2} setValue={setValue2} />
       </>
     );
   },
