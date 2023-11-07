ebb4caea54b687c98b00ecac3292291bd558b38b
RyushiAok <55625375+RyushiAok@users.noreply.github.com>
2023-11-07 16:16:31 +0900

    docs(toggle-switch): add disabled story

--- packages/wiz-ui-next/src/components/base/inputs/toggle-switch/toggle-switch.stories.ts
+++ packages/wiz-ui-next/src/components/base/inputs/toggle-switch/toggle-switch.stories.ts
@@ -32,3 +32,7 @@ const Template: StoryFn<typeof WizToggleSwitch> = (args) => ({
 });
 
 export const Default = Template.bind({});
+export const Disabled = Template.bind({});
+Disabled.args = {
+  disabled: true,
+};
--- packages/wiz-ui-react/src/components/base/inputs/toggle-switch/stories/toggle-switch.stories.tsx
+++ packages/wiz-ui-react/src/components/base/inputs/toggle-switch/stories/toggle-switch.stories.tsx
@@ -36,3 +36,22 @@ export const Default: Story = {
     );
   },
 };
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+  render: (args) => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [value1, setValue1] = useState(false);
+    const [value2, setValue2] = useState(true);
+    return (
+      <>
+        <p>入力値: {value1 ? "true" : "false"}</p>
+        <WizToggleSwitch {...args} value={value1} setValue={setValue1} />
+        <p>入力値: {value2 ? "true" : "false"}</p>
+        <WizToggleSwitch {...args} value={value2} setValue={setValue2} />
+      </>
+    );
+  },
+};
