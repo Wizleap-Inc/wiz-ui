7418aed3d6b99b090c45499e26cda39f80774bd2
AOKI Takashi <55625375+RyushiAok@users.noreply.github.com>
2024-04-15 14:36:58 +0900

    chore(example): import path

--- examples/react/vite.config.ts
+++ examples/react/vite.config.ts
@@ -1,7 +1,14 @@
+import { resolve } from "path";
+
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
 });
