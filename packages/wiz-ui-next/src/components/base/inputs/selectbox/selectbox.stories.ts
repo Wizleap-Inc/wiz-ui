2418757f85ab23ecad2a7bdae80586937cdce169
AOKI Takashi <55625375+RyushiAok@users.noreply.github.com>
2024-09-02 06:09:43 +0900

    fix(selectbox): story

--- packages/wiz-ui-next/src/components/base/inputs/selectbox/selectbox.stories.ts
+++ packages/wiz-ui-next/src/components/base/inputs/selectbox/selectbox.stories.ts
@@ -1,5 +1,4 @@
 import { Meta, StoryFn } from "@storybook/vue3";
-import { ref } from "vue";
 
 import { WizHStack } from "@/components";
 
@@ -37,12 +36,11 @@ export default {
 const Template: StoryFn<typeof WizSelectBox> = (args) => ({
   components: { WizSelectBox, WizHStack },
   setup() {
-    const modelValue = ref(args.modelValue);
-    return { modelValue, args };
+    return { args };
   },
   template: `
     <WizHStack>
-      <WizSelectBox v-bind="args" v-model="value" @update:modelValue="args.input"/>
+      <WizSelectBox v-bind="args" v-model="args.modelValue"/>
     </WizHStack>
   `,
 });
