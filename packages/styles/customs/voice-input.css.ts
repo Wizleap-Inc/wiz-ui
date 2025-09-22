247ac68f2165c505acd3a2910bc01099ac5858ab
BestTempuraJP <degutech@gmail.com>
2025-09-22 11:51:56 +0900

    fix: voice-inputのアイコンをボタンに変更

---
+++ packages/styles/customs/voice-input.css.ts
@@ -0,0 +1,58 @@
+import { style } from "@vanilla-extract/css";
+import { THEME } from "@wizleap-inc/wiz-ui-constants";
+
+export const voiceInputStyle = style({
+  borderRadius: THEME.spacing.xs2,
+  boxSizing: "border-box",
+  padding: THEME.spacing.xs,
+  fontSize: THEME.fontSize.sm,
+  lineHeight: 1.5,
+  color: THEME.color.gray[700],
+  display: "inline-flex",
+  justifyContent: "space-between",
+  alignItems: "end",
+  gap: THEME.spacing.xs,
+});
+
+export const voiceInputExpandStyle = style({
+  width: "100%",
+});
+
+export const voiceInputDisabledStyle = style({
+  backgroundColor: THEME.color.gray[300],
+  cursor: "not-allowed",
+});
+
+export const voiceInputTextAreaStyle = style({
+  width: "100%",
+  height: "100%",
+  boxSizing: "border-box",
+  border: "none",
+  outline: "none",
+  "::placeholder": {
+    color: THEME.color.gray[500],
+  },
+  ":disabled": {
+    opacity: 0.5,
+    color: THEME.color.gray[700],
+    backgroundColor: THEME.color.gray[300],
+    cursor: "not-allowed",
+  },
+  selectors: {
+    "&:disabled::placeholder": {
+      color: THEME.color.gray[700],
+    },
+  },
+});
+
+export const voiceInputButtonStyle = style({
+  background: "transparent",
+  border: "none",
+  padding: 0,
+  cursor: "pointer",
+  display: "flex",
+  alignItems: "center",
+  ":disabled": {
+    cursor: "not-allowed",
+  },
+});
