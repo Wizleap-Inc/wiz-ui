4f8a8880d1d38e33ef72ea772c882d81c707372b
BestTempuraJP <degutech@gmail.com>
2025-10-14 08:51:45 +0900

    feat: add icons build

---
+++ packages/wiz-ui-next/src/components/icons/location-on-bold.vue
@@ -0,0 +1,20 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="1em"
+    viewBox="0 -960 960 960"
+    width="1em"
+  >
+    <path
+      d="M480-301q99-80 149.5-154T680-594q0-90-56-148t-144-58q-88 0-144 58t-56 148q0 65 50.5 139T480-301Zm0 101Q339-304 269.5-402T200-594q0-125 78-205.5T480-880q124 0 202 80.5T760-594q0 94-69.5 192T480-200Zm0-320q33 0 56.5-23.5T560-600q0-33-23.5-56.5T480-680q-33 0-56.5 23.5T400-600q0 33 23.5 56.5T480-520ZM200-80v-80h560v80H200Zm280-520Z"
+    />
+  </svg>
+</template>
+
+<script setup lang="ts">
+import { ComponentName } from "@wizleap-inc/wiz-ui-constants";
+
+defineOptions({
+  name: ComponentName.ILocationOnBold,
+});
+</script>
---
+++ packages/wiz-ui-next/src/components/icons/rotate-right-bold.vue
@@ -0,0 +1,20 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="1em"
+    viewBox="0 -960 960 960"
+    width="1em"
+  >
+    <path
+      d="M522-80v-80q70-14 119-68.5T690-353q0-83-58.5-141.5T490-553h-20l60 60-56 57-160-160 160-160 56 57-60 59h20q117 0 198.5 81.5T770-360q0 104-68 182T522-80ZM320-183 160-343l160-160 56 57-62 63h16q26 0 51-5.5t48-16.5l62 64q-33 20-70.5 30.5T344-300h-14l46 46-56 71Z"
+    />
+  </svg>
+</template>
+
+<script setup lang="ts">
+import { ComponentName } from "@wizleap-inc/wiz-ui-constants";
+
+defineOptions({
+  name: ComponentName.IRotateRightBold,
+});
+</script>
